00506a3dbd93d600b9c379c78fd22886a20be7fe
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-08-02 07:58:29 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -10,9 +10,6 @@ PRINTSFILE=/sdcard/printslist
 
 # Certified fingerprints
 PRINTSLIST="
-Huawei Mate 9 (8.0.0)=HUAWEI/MHA-L29/HWMHA:8.0.0/HUAWEIMHA-L29/373(C636):user/release-keys
-Huawei P8 Lite 2017 (8.0.0)=HUAWEI/PRA-LX1/HWPRA-H:8.0.0/HUAWEIPRA-LX1/367(C432):user/release-keys
-Xiaomi Mi 5S Plus (7.0)=Xiaomi/natrium/natrium:7.0/NRD90M/8.7.26:user/release-keys
 "
 
 if [ -f "$PRINTSFILE" ]; then
