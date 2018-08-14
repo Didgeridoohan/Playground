5ac66d4847084a53b867bfb04be6ea746823d9c0
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-08-14 21:31:48 +0200

    testing

--- prints.sh
+++ prints.sh
@@ -10,11 +10,11 @@ PRINTSFILE=/sdcard/printslist
 
 # Certified fingerprints
 PRINTSLIST="
+Google Pixel (9)=google/sailfish/sailfish:9/PPR1.180610.009/4898911:user/release-keys
+Google Pixel XL (9)=google/marlin/marlin:9/PPR1.180610.009/4898911:user/release-keys
+Google Pixel 2 (9)=google/walleye/walleye:9/PPR1.180610.009/4898911:user/release-keys
+Google Pixel 2 XL (9)=google/taimen/taimen:9/PPR1.180610.009/4898911:user/release-keys
 Motorola Moto Z2 Play (8.0.0)=motorola/albus/albus:8.0.0/OPSS27.76-12-25-3/4:user/release-keys
-Google Pixel (9.0)=google/sailfish/sailfish:9/PPR1.180610.009/4898911:user/release-keys
-Google Pixel XL (9.0)=google/marlin/marlin:9/PPR1.180610.009/4898911:user/release-keys
-Google Pixel 2 (9.0)=google/walleye/walleye:9/PPR1.180610.009/4898911:user/release-keys
-Google Pixel 2 XL (9.0)=google/taimen/taimen:9/PPR1.180610.009/4898911:user/release-keys
 "
 
 if [ -f "$PRINTSFILE" ]; then
