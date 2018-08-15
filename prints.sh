618c49a0833247fb0c4cd28b687d3f4cb779945c
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-08-15 20:10:30 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -10,7 +10,6 @@ PRINTSFILE=/sdcard/printslist
 
 # Certified fingerprints
 PRINTSLIST="
-Motorola Moto Z2 Play (8.0.0)=motorola/albus/albus:8.0.0/OPS27.76-12-25/26:user/release-keys
 "
 
 if [ -f "$PRINTSFILE" ]; then
