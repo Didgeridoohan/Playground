e2637576413d7eea83a7aba41a8699858cdff551
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-10-23 06:41:11 +0200

    testing

--- prints.sh
+++ prints.sh
@@ -10,6 +10,12 @@ PRINTSFILE=/sdcard/printslist
 
 # Certified fingerprints
 PRINTSLIST="
+Huawei Mate 10 (8.0.0)=HUAWEI/ALP-L29/HWALP:8.0.0/HUAWEIALP-L29S/143(C636):user/release-keys
+OnePlus 3 (8.0.0)=OnePlus/OnePlus3/OnePlus3:8.0.0/OPR1.170623.032/1809142047:user/release-keys§2018-09-01
+OnePlus 3T (8.0.0)=OnePlus/OnePlus3/OnePlus3T:8.0.0/OPR1.170623.032/1809150114:user/release-keys§2018-09-01
+OnePlus 5 (8.1.0)=OnePlus/OnePlus5/OnePlus5:8.1.0/OPM1.171019.011/1809241512:user/release-keys§2018-09-01
+OnePlus 5T (8.1.0)=OnePlus/OnePlus5T/OnePlus5T:8.1.0/OPM1.171019.011/1809241512:user/release-keys§2018-09-01
+ZTE Axon 7 (8.0.0)=ZTE/P996A01_O/ailsa_ii:8.0.0/OPR1.170623.032/20180815.171922:user/release-keys§2018-07-01
 "
 
 if [ -f "$PRINTSFILE" ]; then
