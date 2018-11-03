7d33cbf9aa465be2bf509c2972554af0d35e3aaf
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-11-03 22:55:25 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -10,5 +10,6 @@ PRINTSTRANSF=1
 PRINTSLIST="
 Asus Zenfone 2 Laser ZE500KL=asus/WW_Phone/ASUS_Z00E_2:6.0.1/MMB29P/13.1010.1612.53-20170202:user/release-keys
 Huawei P9 Lite VNS-L31 (7.0)=HUAWEI/VNS-L31/HWVNS-H:7.0/HUAWEIVNS-L31/C432B414:user/release-keys__2018-06-01
+Xiaomi Pocophone F1 (8.1.0)=Xiaomi/beryllium/beryllium:8.1.0/OPM1.171019.011/V9.6.22.0.OEJMIFD:user/release-keys__2018-08-01
 Xiaomi Redmi 3/3 Pro (5.1.1)=Xiaomi/ido/ido:5.1.1/LMY47V/V9.6.2.0.LAIMIFD:user/release-keys
 "
