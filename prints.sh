6318519a0a5812f2fa70e180d635f59330c58c6a
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-11-03 07:02:23 +0100

    testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Asus Zenfone 2 Laser ZE500KL=asus/WW_Phone/ASUS_Z00E_2:6.0.1/MMB29P/13.1010.1612.53-20170202:user/release-keys
+Huawei P9 Lite VNS-L31 (7.0)=HUAWEI/VNS-L31/HWVNS-H:7.0/HUAWEIVNS-L31/C432B414:user/release-keys__2018-06-01
+Xiaomi Redmi 3/3 Pro (5.1.1)=Xiaomi/ido/ido:5.1.1/LMY47V/V9.6.2.0.LAIMIFD:user/release-keys
 "
