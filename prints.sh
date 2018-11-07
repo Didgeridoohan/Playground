acd45b165ef14322d7bf8ab55d0fff73e3b45ba5
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-11-07 23:31:17 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,13 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Pixel 2 (9)=google/walleye/walleye:9/PQ1A.181105.017.A1/5081125:user/release-keys__2018-11-05
-Essential PH-1 (9)=essential/mata/mata:9/PPR1.181005.076/390:user/release-keys__2018-11-05
-Google Nexus 5x (8.1.0)=google/bullhead/bullhead:8.1.0/OPM7.181105.004/5038062:user/release-keys__2018-11-05
-Google Nexus 6P (8.1.0)=google/angler/angler:8.1.0/OPM7.181105.004/5038062:user/release-keys__2018-11-05
-Google Pixel 2 XL (9)=google/taimen/taimen:9/PQ1A.181105.017.A1/5081125:user/release-keys__2018-11-05
-Google Pixel 3 (9)=google/blueline/blueline:9/PQ1A.181105.017.A1/5081125:user/release-keys__2018-11-05
-Google Pixel 3 XL (9)=google/crosshatch/crosshatch:9/PQ1A.181105.017.A1/5081125:user/release-keys__2018-11-05
-Huawei P8 Lite 2017 (8.0.0)=HUAWEI/PRA-LX1/HWPRA-H:8.0.0/HUAWEIPRA-LX1/386(C432):user/release-keys__2018-09-01
-Samsung Galaxy Note 3 (5.0)=samsung/hltezh/hlte:5.0/LRX21V/N9005ZSUGPJ2:user/release-keys
 "
