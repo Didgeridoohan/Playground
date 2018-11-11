686b48559e3a082197c30b2666e4f909ce7e291a
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-11-11 09:32:42 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Nexus 10 (5.1.1)=google/mantaray/manta:5.1.1/LMY49J/2640980:user/release-keys
-LG G5 H850 (8.0.0)=lge/h1_global_com/h1:8.0.0/OPR1.170623.032/182532053d3a3:user/release-keys__2018-09-01
 "
