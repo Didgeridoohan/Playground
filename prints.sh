e6a5a95923daa2ef96aca271bf31eda0f1668d5d
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-12-01 18:35:03 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,5 +8,9 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-
+Huawei G8 (5.1)=HUAWEI/RIO-L01/hwRIO-L01:5.1/HuaweiRIO-L01/C150B130:user/release-keys
+Samsung Galaxy Tab A 2016 (7.0)=samsung/gtaxlltexx/gtaxllte:7.0/NRD90M/T585XXU2BQI9:user/release-keys
+Sony Xperia Tab Z (5.1.1)=Sony/SGP321/SGP321:5.1.1/10.7.A.0.228/2020576680:user/release-keys
+Sony Xperia XZ2 Compact (9)=Sony/H8324/H8324:9/52.0.A.3.84/2936176301:user/release-keys__2018-11-01
+Xiaomi Mi 8 (8.1.0)=Xiaomi/dipper/dipper:8.1.0/OPM1.171019.026/V10.0.3.0.OEAMIFH:user/release-keys__2018-10-01
 "
