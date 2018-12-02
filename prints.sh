2d4ce8c512dd7a483b83aa08ffe3dcd046e65456
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-12-02 07:22:54 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,5 +8,13 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Google Nexus 7 2012 WiFI (5.1.1)=google/nakasi/grouper:5.1.1/LMY47V/1836172:user/release-keys
+Google Nexus 7 2012 LTE (5.1.1)=google/nakasig/tilapia:5.1.1/LMY47V/1836172:user/release-keys
+Google Nexus 7 2013 WiFi (6.0.1)=google/razor/flo:6.0.1/MOB30X/3036618:user/release-keys
+Google Nexus 7 2013 LTE (6.0.1)=google/razorg/deb:6.0.1/MOB30X/3036618:user/release-keys
+Google Nexus 9 WiFi ()=google/volantis/flounder:7.1.1/N9F27M/4333998:user/release-keys
+Google Nexus 9 LTE (7.1.1)=google/volantisg/flounder_lte:7.1.1/N4F27P/4333386:user/release-keys
+Google Pixel C (8.1.0)=google/ryu/dragon:8.1.0/OPM8.181105.002/5038063:user/release-keys__2018-11-05
+OnePlus 6T (9)=OnePlus/OnePlus6T/OnePlus6T:9/PKQ1.180716.001/1811170214:user/release-keys__2018-11-01
 Samsung Galaxy J7 Prime (6.0.1)=samsung/on7xeltedd/on7xelte:6.0.1/MMB29K/G610FDDU1AQG2:user/release-keys
 "
