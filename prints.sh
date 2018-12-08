7a1000cc331b89dba6467121b28965a53c8886ff
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-12-08 07:18:15 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,5 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Sony Xperia XZ1 Compact (9)=Sony/G8441/G8441:9/47.2.A.2.33/3676206253:user/release-keys__2018-11-01
-Sony Xperia XZ1 Compact (8.0.0)=Sony/G8441/G8441:8.0.0/47.1.A.16.20/623594567:user/release-keys__2018-09-01
-Sony Xperia Z4 Tablet LTE=Sony/SGP771/SGP771:7.1.1/32.4.A.1.54/219395100:user/release-keys
+Xiaomi Redmi Y1 (7.1.2)=xiaomi/ugg/ugg:7.1.2/N2G47H/V9.6.2.0.NDKMIFD:user/release-keys__2018-07-01
 "
