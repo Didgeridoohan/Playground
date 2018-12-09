65a96e9cc0548d1c4787f321594de5109e1ab9cc
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-12-09 07:58:23 +0100

    test

--- prints.sh
+++ prints.sh
@@ -8,5 +8,18 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Essential PH1 (9)=essential/mata/mata:9/PPR1.181005.099/413:user/release-keys__2018-12-05
+Google Nexus 5x (8.1.0)=google/bullhead/bullhead:8.1.0/OPM7.181205.001/5080180:user/release-keys__2018-12-05
+Google Nexus 6p (8.1.0)=google/angler/angler:8.1.0/OPM7.181205.001/5080180:user/release-keys__2018-12-05
+Google Pixel (9)=google/sailfish/sailfish:9/PQ1A.181205.002.A1/5129870:user/release-keys__2018-12-05
+Google Pixel XL (9)=google/marlin/marlin:9/PQ1A.181205.002.A1/5129870:user/release-keys__2018-12-05
+Google Pixel 2 (9)=google/walleye/walleye:9/PQ1A.181205.002/5086253:user/release-keys__2018-12-05
+Google Pixel 2 XL (9)=google/taimen/taimen:9/PQ1A.181205.002/5086253:user/release-keys__2018-12-05
+Google Pixel 3 (9)=google/blueline/blueline:9/PQ1A.181205.006/5108886:user/release-keys__2018-12-05
+Google Pixel 3 XL (9)=google/crosshatch/crosshatch:9/PQ1A.181205.006/5108886:user/release-keys__2018-12-05
+Google Pixel C (8.1.0)=google/ryu/dragon:8.1.0/OPM8.181205.001/5080184:user/release-keys__2018-12-05
+Sony Xperia XZ1 Compact (9)=Sony/G8441/G8441:9/47.2.A.2.33/3676206253:user/release-keys__2018-11-01
+Sony Xperia Z4 Tablet LTE (7.1.1)=Sony/SGP771/SGP771:7.1.1/32.4.A.1.54/219395100:user/release-keys
+Xiaomi Mi A2 (8.1.0)=xiaomi/jasmine/jasmine_sprout:8.1.0/OPM1.171019.011/V9.6.16.0.ODIMIFE:user/release-keys__2018-11-05
 Xiaomi Redmi Y1 (7.1.2)=xiaomi/ugg/ugg:7.1.2/N2G47H/V9.6.2.0.NDKMIFD:user/release-keys__2018-07-01
 "
