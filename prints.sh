74bd29459815384319136e5ecba5135e4da10a3f
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-12-14 02:46:17 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Samsung Galaxy Note 9 (9)=samsung/crownltexx/crownlte:9/PPR1.180610.011/N960FXXU2ZRKQ:user/release-keys
-Xiaomi Mi 8 (9)=Xiaomi/dipper/dipper:9/PKQ1.180729.001/V10.1.1.0.PEAMIFI:user/release-keys
+Samsung Galaxy Note 9 (9)=samsung/crownltexx/crownlte:9/PPR1.180610.011/N960FXXU2ZRKQ:user/release-keys__2018-11-01
+Xiaomi Mi A2 (9.0.0)=xiaomi/jasmine/jasmine_sprout:9/PKQ1.180904.001/V10.0.2.0.PDIMIFJ:user/release-keys__2018-12-05
+Xiaomi Mi 8 (9)=Xiaomi/dipper/dipper:9/PKQ1.180729.001/V10.1.1.0.PEAMIFI:user/release-keys__2018-11-01
 Xiaomi Redmi Note 5 (8.1.0)=xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V10.0.6.0.OEIMIFH:user/release-keys
 "
