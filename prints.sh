9553e57b0cfc2530c081d414f498f917d789aa54
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-12-22 12:01:36 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,12 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Razer Phone 1 (8.1.0)=razer/cheryl/cheryl:8.1.0/OPM1.171019.011-RZR-180803/6033:user/release-keys__2018-07-05
-Razer Phone 2 (8.1.0)=razer/cheryl2/aura:8.1.0/O-MR2-RC009-RZR-181124/2009:user/release-keys__2018-10-05
-Samsung Galaxy Note 9 (9)=samsung/crownltexx/crownlte:9/PPR1.180610.011/N960FXXU2ZRKQ:user/release-keys__2018-11-01
-Xiaomi Mi A2 (9)=xiaomi/jasmine/jasmine_sprout:9/PKQ1.180904.001/V10.0.1.0.PDIMIFJ:user/release-keys__2018-11-05
-Xiaomi Mi A2 (9)=xiaomi/jasmine/jasmine_sprout:9/PKQ1.180904.001/V10.0.2.0.PDIMIFJ:user/release-keys__2018-12-05
-Xiaomi Mi 8 (9)=Xiaomi/dipper/dipper:9/PKQ1.180729.001/V10.1.1.0.PEAMIFI:user/release-keys__2018-11-01
-Xiaomi Mi Pad 4 (8.1.0)=ro.build.fingerprint=Xiaomi/clover/clover:8.1.0/OPM1.171019.019/V10.1.1.0.ODJCNFI:user/release-keys__2018-10-01
-Xiaomi Pocophone F1 (9)=Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.1.3.0.PEJMIFI:user/release-keys__2018-11-01
 "
