763442a075f0314708e7f6bbc0b3a7fdb7188d1c
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-01-07 08:31:31 +0100

    testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,20 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Test Asus Zenfone 2 Laser (6.0.1)=asus/WW_Z00L/ASUS_Z00L_63:6.0.1/MMB29P/WW_user_21.40.1220.2196_20180308:user/release-keys
+Test Essential PH-1 (9)=essential/mata/mata:9/PPR1.181005.099/413:user/release-keys__2018-12-05
+Test Google Pixel XL (9)=google/marlin/marlin:9/PQ1A.181205.002.A1/5129870:user/release-keys__2018-12-05
+Test Huawei P8 Lite (8.0.0)=HUAWEI/PRA-LX1/HWPRA-H:8.0.0/HUAWEIPRA-LX1/399(C432):user/release-keys__2019-01-01
+Test LG G4 H812 (6.0)=lge/p1_rgs_ca/p1:6.0/MRA58K/1601215569825:user/release-keys
+Test Motorola Moto E5 Play (8.0.0)=motorola/james_sprint/james:8.0.0/OCPS27.91-140-1/1:user/release-keys__2018-10-01
+Test Nvidia Shield K1 (7.0)=nvidia/sb_na_wf/shieldtablet:7.0/NRD90M/1928188_1065.2559:user/release-keys
+Test Nvidia Shield Tablet (7.0)=nvidia/wx_na_wf/shieldtablet:7.0/NRD90M/1928188_904.2811:user/release-keys
+Test OnePlus 2 (6.0.1)=OnePlus/OnePlus2/OnePlus2:6.0.1/MMB29M/1447858500:user/release-keys
+Test Samsung Galaxy A5 2017 (8.0.0)=samsung/a5y17ltexx/a5y17lte:8.0.0/R16NW/A520FXXU4CRE8:user/release-keys__2018-05-01
+Test Samsung Galaxy Note 4 (6.0.1)=samsung/trltexx/trlte:6.0.1/MMB29M/N910FXXS1DQH9:user/release-keys
+Test Xiaomi Mi 8 (9)=Xiaomi/dipper/dipper:9/PKQ1.180729.001/V10.1.2.0.PEAMIFI:user/release-keys__2018-11-01
+Test Xiaomi Mi A2 Lite (9)=xiaomi/daisy/daisy_sprout:9/PKQ1.180917.001/V10.0.1.0.PDLMIFJ:user/release-keys__2018-11-05
+Test Xiaomi Mi Mix 2S (9)=Xiaomi/polaris/polaris:9/PKQ1.180729.001/V10.0.9.0.PDGMIFH:user/release-keys__2018-11-01
+Test Xiaomi Redmi 6 Pro (8.1.0)=xiaomi/sakura/sakura:8.1.0/OPM1.171019.019/8.12.6:user/release-keys__2018-12-01
+Test Xiaomi Redmi Note 6 Pro (8.1.0)=xiaomi/tulip/tulip:8.1.0/OPM1.171019.011/V10.0.5.0.OEKMIFH:user/release-keys__2018-10-01
 "
