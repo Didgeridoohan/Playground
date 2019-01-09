7d5d71df40345204f4122893c7ba3d4cce26d70d
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-01-09 07:37:49 +0100

    testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,15 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Essential PH-1 (9)=essential/mata/mata:9/PPR1.181005.116/430:user/release-keys__2019-01-05
+OnePlus 3T (8.0.0)=OnePlus/OnePlus3/OnePlus3T:8.0.0/OPR1.170623.032/1812060016:user/release-keys__2018-11-01
+OnePlus 5T (9)=OnePlus/OnePlus5T/OnePlus5T:9/PKQ1.180716.001/1812290005:user/release-keys__2018-12-01
+Pixel (9)=google/sailfish/sailfish:9/PQ1A.190105.004/5148680:user/release-keys__2019-01-05
+Pixel XL (9)=google/marlin/marlin:9/PQ1A.190105.004/5148680:user/release-keys__2019-01-05
+Pixel 2 (9)=google/walleye/walleye:9/PQ1A.190105.004/5148680:user/release-keys__2019-01-05
+Pixel 2 XL (9)=google/taimen/taimen:9/PQ1A.190105.004/5148680:user/release-keys__2019-01-05
+Pixel 3 (9)=google/blueline/blueline:9/PQ1A.190105.004/5148680:user/release-keys__2019-01-05
+Pixel 3 XL (9)=google/crosshatch/crosshatch:9/PQ1A.190105.004/5148680:user/release-keys__2019-01-05
+Pixel C (8.1.0)=google/ryu/dragon:8.1.0/OPM8.190105.002/5148684:user/release-keys__2019-01-05
+Samsung J7 2017 (8.1.0)=samsung/j7y17ltexx/j7y17lte:8.1.0/M1AJQ/J730FXXU3BRK2:user/release-keys__2018-11-01
 "
