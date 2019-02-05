a96eb2d19b1d766c3e8651887c90bcd0782d7232
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-02-05 20:16:28 +0100

    testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,23 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+ASUS ZenFone 2 (6.0.1)=asus/WW_Z008/Z008:6.0.1/MMB29P/4.21.40.311_20170609_7906_user:user/release-keys
+Essential PH-1 (9)=essential/mata/mata:9/PQ1A.190105.014/451:user/release-keys__2019-02-05
+Google Pixel (9)=google/sailfish/sailfish:9/PQ2A.190205.003/5180536:user/release-keys__2019-02-05
+Google Pixel XL (9)=google/marlin/marlin:9/PQ2A.190205.003/5180536:user/release-keys__2019-02-05
+Google Pixel 2 (9)=google/walleye/walleye:9/PQ2A.190205.002/5164942:user/release-keys__2019-02-05
+Google Pixel 2 XL (9)=google/taimen/taimen:9/PQ2A.190205.002/5164942:user/release-keys__2019-02-05
+Google Pixel 3 (9)=google/blueline/blueline:9/PQ2A.190205.001/5163636:user/release-keys__2019-02-05
+Google Pixel 3 XL (9)=google/crosshatch/crosshatch:9/PQ2A.190205.001/5163636:user/release-keys__2019-02-05
+Google Pixel C (8.1.0)=google/ryu/dragon:8.1.0/OPM8.190205.001/5175807:user/release-keys__2019-02-05
+Huawei P20 (9)=HUAWEI/EML-L29/HWEML:9/HUAWEIEML-L29/160C432R1:user/release-keys__2018-11-01
+Huawei Mate 20 Pro (9)=HUAWEI/LYA-L29/HWLYA:9/HUAWEILYA-L29/183AUSC636R1:user/release-keys
+LG G6 H870 (7.0)=lge/lucye_global_com/lucye:7.0/NRD90U/171732341982c:user/release-keys
+Motorola Moto G5S (8.1.0)=motorola/montana/montana:8.1.0/OPP28.65-37/c13e:user/release-keys
+OnePlus 5T (9)=OnePlus/OnePlus5T/OnePlus5T:9/PKQ1.180716.001/1901182129:user/release-keys__2018-12-01
+Samsung Galaxy A5 2015 (6.0.1)=samsung/a5ultexx/a5ulte:6.0.1/MMB29M/A500FUXXU1CPH3:user/release-keys
+Xiaomi Mi A1 (9)=xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.4.0.PDHMIXM:user/release-keys_2019-01-05
+Xiaomi Mi A2 (9)=xiaomi/jasmine/jasmine_sprout:9/PKQ1.180904.001/V10.0.3.0.PDIMIXM:user/release-keys__2019-01-05
+Xiaomi Mi Mix 2S (9)=Xiaomi/polaris/polaris:9/PKQ1.180729.001/V10.2.1.0.PDGMIXM:user/release-keys__2018-11-01
+Xiaomi Poco F1 (9)=Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.2.2.0.PEJMIXM:user/release-keys__2018-12-01
 "
