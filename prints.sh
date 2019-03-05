ceb89c5c07304e557ceec4a42bf9e8c6f9b16250
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-03-05 09:53:21 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,17 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Essential PH-1 (9)=essential/mata/mata:9/PQ1A.190105.024/462:user/release-keys__2019-03-05
+Google Pixel (9)=google/sailfish/sailfish:9/PQ2A.190305.002/5240760:user/release-keys__2019-03-05
+Google Pixel XL (9)=google/marlin/marlin:9/PQ2A.190305.002/5240760:user/release-keys__2019-03-05
+Google Pixel 2 (9)=google/walleye/walleye:9/PQ2A.190305.002/5240760:user/release-keys__2019-03-05
+Google Pixel 2 XL (9)=google/taimen/taimen:9/PQ2A.190305.002/5240760:user/release-keys__2019-03-05
+Google Pixel 3 (9)=google/blueline/blueline:9/PQ2A.190305.002/5240760:user/release-keys__2019-03-05
+Google Pixel 3 XL (9)=google/crosshatch/crosshatch:9/PQ2A.190305.002/5240760:user/release-keys__2019-03-05
+Google Pixel C (8.1.0)=google/ryu/dragon:8.1.0/OPM8.190305.001/5238107:user/release-keys__2019-03-05
+OnePlus 5T (9)=OnePlus/OnePlus5T/OnePlus5T:9/PKQ1.180716.001/1902221914:user/release-keys__2019-01-01
+OnePlus 6T (9)=OnePlus/OnePlus6T/OnePlus6T:9/PKQ1.180716.001/1901231231:user/release-keys__2019-01-01
+Xiaomi Mi A2 (9)=xiaomi/jasmine/jasmine_sprout:9/PKQ1.180904.001/V10.0.4.0.PDIMIXM:user/release-keys__2019-02-05
+Xiaomi Mi Mix 2 (8.0.0)=Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V10.2.2.0.ODEMIXM:user/release-keys__2018-12-01
+Xiaomi Pocophone F1 (9)=Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.2.2.0.PEJMIXM:user/release-keys__2018-12-01
 "
