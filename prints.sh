b4b4a23411d6715e2cb98912eb8b3c7a008996cf
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-03-10 06:47:04 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,6 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Sony Xperia X (8.0.0)=Sony/F5121/F5121:8.0.0/34.4.A.2.118/1631208121:user/release-key__2018-09-01
+Sony Xperia X Dual (8.0.0)=Sony/F5122/F5122:8.0.0/34.4.A.2.118/1631208121:user/release-keys__2018-09-01
 "
