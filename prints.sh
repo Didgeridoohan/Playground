a76a3e8c3652b404fac24a611c78ecdc9d2c5334
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-04-08 20:02:01 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Oppo Neo7 A33w (5.1)=OPPO/A33w/A33w:5.1/LMY47I/1440568801:user/release-keys
-Xiaomi Mi 9 (9)=Xiaomi/cepheus_eea/cepheus:9/PKQ1.181121.001/V10.2.17.0.PFAEUXM:user/release-keys__2019-03-01
 "
