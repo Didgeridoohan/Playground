b0a3622773617107bb035b45696b40950fff3eef
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-04-27 16:04:13 +0200

    testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,17 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Lenovo K6 Note (7.0)=Lenovo/karatep/K53a48:7.0/NRD90N/K53_S228_171114_ROW:user/release-keys
+LG G5 H850 (8.0.0)=lge/h1_global_com/h1:8.0.0/OPR1.170623.032/190380127377a:user/release-keys__2018-12-01
+LG G5 H830 (8.0.0)=lge/h1_tmo_us/h1:8.0.0/OPR1.170623.032/182251913485f:user/release-keys__2018-08-01
+LG G5 RS988 (7.0)=lge/h1_lra_us/h1:7.0/NRD90U/1707917149a1b:user/release-keys
+LG G6 H870 (8.0.0)=lge/lucye_global_com/lucye:8.0.0/OPR1.170623.032/183301757502c:user/release-keys__2018-11-01
+LG G6 H872 (8.0.0)=lge/lucye_tmo_us/lucye:8.0.0/OPR1.170623.032/183621644ce05:user/release-keys__2019_01-01
+LG G6 US997 (8.0.0)=lge/lucye_nao_us/lucye:8.0.0/OPR1.170623.032/181521705197f:user/release-keys__2018_06_01
+LG V20 H918 (8.0.0)=lge/elsa_tmo_us/elsa:8.0.0/OPR1.170623.032/1831917495b33:user/release-keys__2018-11-01
+LG V20 H910 (8.0.0)=lge/elsa_att_us/elsa:8.0.0/OPR1.170623.032/181871124f959:user/release-keys__2018-06-01
+LG V20 VS995 (8.0.0)=lge/elsa_vzw/elsa:8.0.0/OPR1.170623.032/183141449275f:user/release-keys__2018-10-01
+Oppo Neo7 A33w (5.1)=OPPO/A33w/A33w:5.1/LMY47I/1440568801:user/release-keys
+Oppo Neo7 A1603 (5.1)=OPPO/A1603/A1603:5.1/LMY47I/1489407760:user/release-keys
+Samsung Galaxy S7 Edge (8.0.0)=samsung/hero2ltexx/hero2lte:8.0.0/R16NW/G935FXXS4ESC3:user/release-keys__2019-03-01
 "
