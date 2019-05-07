c8d746870acd2b1afccf0757a39b118f097c815f
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-05-07 09:58:24 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,17 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Essential PH-1 (9)=essential/mata/mata:9/PQ1A.190105.058/496:user/release-keys__2019-05-05
-OnePlus 5T (9)=OnePlus/OnePlus5T/OnePlus5T:9/PKQ1.180716.001/1904191514:user/release-keys__2019-04-01
-OnePlus 5T Beta 29 (9)=OnePlus/OnePlus5T/OnePlus5T:9/PKQ1.180716.001/1904150000:user/release-keys__2019-04-01
-Google Pixel (9)=google/sailfish/sailfish:9/PQ3A.190505.001/5373320:user/release-keys__2019-05-05
-Google Pixel XL (9)=google/marlin/marlin:9/PQ3A.190505.001/5373320:user/release-keys__2019-05-05
-Google Pixel 2 (9)=google/walleye/walleye:9/PQ3A.190505.001/5373320:user/release-keys__2019-05-05
-Google Pixel 2 XL (9)=google/taimen/taimen:9/PQ3A.190505.001/5373320:user/release-keys__2019-05-05
-Google Pixel 3 (9)=google/blueline/blueline:9/PQ3A.190505.002/5450365:user/release-keys__2019-05-05
-Google Pixel 3 XL (9)=google/crosshatch/crosshatch:9/PQ3A.190505.002/5450365:user/release-keys__2019-05-05
-Google Pixel C (8.1.0)=google/ryu/dragon:8.1.0/OPM8.190505.001/5373745:user/release-keys__2019-05-05
-Samsung Galaxy S10 Plus (9)=samsung/beyond2qlteue/beyond2q:9/PPR1.180610.011/G975U1UEU1ASD3:user/release-keys__2019-03-01
-Xiaomi Mi Max 3 (9)=Xiaomi/nitrogen/nitrogen:9/PKQ1.181007.001/V10.3.1.0.PEDMIXM:user/release-keys__2019-03-01
-Xiaomi Mi Mix 3 (9)=Xiaomi/perseus/perseus:9/PKQ1.180729.001/V10.3.1.0.PEEMIXM:user/release-keys__2019-03-01
 "
