8bb3d3dc7be66308e1a286b2609f30e33baeaee3
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-05-27 14:02:43 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -11,5 +11,4 @@ PRINTSLIST="
 Google Nexus Player (6.0.1)=google/fugu/fugu:6.0.1/MMB29U/2495654:user/release-keys
 OnePlus 3 (9)=OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1905102347:user/release-keys__2019-04-01
 OnePlus 3T (9)=OnePlus/OnePlus3/OnePlus3T:9/PKQ1.181203.001/1905102346:user/release-keys__2019-04-01
-Xiaomi Mi S (8.1.0)=Xiaomi/oneday/oneday:8.1.0/OPM1.171019.011/xxx:user/release-keys__2018-08-05
 "
