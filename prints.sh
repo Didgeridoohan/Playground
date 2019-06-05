0fee2e783602852b3b75fd393941080ccec1dc14
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-06-05 12:28:44 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,18 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-BLU R1 HD 2018 (7.0)=BLU/BLU_S1/S0320WW:7.0/NRD90M/1514191903:user/release-keys
-Essential PH-1 (9)=essential/mata/mata:9/PQ1A.190105.077/515:user/release-keys__2019-06-05
-OnePlus 7 Pro (9)=OnePlus/OnePlus7Pro/OnePlus7Pro:9/PKQ1.190110.001/1905251651:user/release-keys__2019-04-01
-Google Pixel (9)=google/sailfish/sailfish:9/PQ3A.190605.003/5524043:user/release-keys__2019-06-05
-Google Pixel XL (9)=google/marlin/marlin:9/PQ3A.190605.003/5524043:user/release-keys__2019-06-05
-Google Pixel 2 (9)=google/walleye/walleye:9/PQ3A.190605.003/5524043:user/release-keys__2019-06-05
-Google Pixel 2 XL (9)=google/taimen/taimen:9/PQ3A.190605.003/5524043:user/release-keys__2019-06-05
-Google Pixel 3 (9)=google/blueline/blueline:9/PQ3A.190605.003/5524043:user/release-keys__2019-06-05
-Google Pixel 3 XL (9)=google/crosshatch/crosshatch:9/PQ3A.190605.003/5524043:user/release-keys__2019-06-05
-Google Pixel 3a (9)=google/sargo/sargo:9/PQ3B.190605.006/5560048:user/release-keys__2019-06-05
-Google Pixel 3a XL (9)=google/bonito/bonito:9/PQ3B.190605.006/5560048:user/release-keys__2019-06-05
-Google Pixel C (8.1.0)=google/ryu/dragon:8.1.0/OPM8.190605.003/5524042:user/release-keys__2019-06-05
-Mecool KM8 (8.0.0 & 9)=MECOOL/KM8/KM8:8.0.0/OPR6.170623.013/V0412.201806271453:user/release-keys__2018-05-05;MECOOL/KM8/KM8:9/PI/20190315:user/release-keys__2019-02-05
-Xiaomi Mi9 (9)=Xiaomi/cepheus_eea/cepheus:9/PKQ1.181121.001/V10.2.24.0.PFAEUXM:user/release-keys__2019-04-01
 "
