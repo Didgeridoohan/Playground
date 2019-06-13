4d7c39dae00b505e45bcaba9ac09f0ce3c167c35
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-06-13 14:22:37 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,14 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Asus Zenfone 6 (9.0)=asus/WW_I01WD/ASUS_I01WD:9/PPR2.181005.003/16.1210.1904.115-0:user/release-keys__2019-04-01
-LG G6 US997 (8.0.0)=lge/lucye_nao_us/lucye:8.0.0/OPR1.170623.032/181521705197f:user/release-keys__2018_06_01
-LG V20 LS997 (7.0)=lge/elsa_spr_us/elsa:8.0.0/OPR1.170623.032/1819814314eb7:user/release-keys__2018-07-01
-LG V20 US996 (8.0.0)=lge/elsa_nao_us/elsa:8.0.0/OPR1.170623.032/183241436cd40:user/release-keys__2018-11-01
-Mecool KM8 (8.0.0 & 9)=MECOOL/KM8/KM8:8.0.0/OPR6.170623.013/V0412.201806271453:user/release-keys__2018-05-05;MECOOL/KM8/KM8:9/PI/20190315:user/release-keys__2019-02-05
-Nokia 7 Plus (9)=Nokia/Onyx_00WW/B2N_sprout:9/PPR1.180610.011/00WW_3_53B:user/release-keys__2019-05-01
-Sony Xperia XA2 Dual (8.0.0)=Sony/H4113/H4113:8.0.0/50.1.A.13.123/2558067933:user/release-keys
-Xiaomi Mi 9 (9)=Xiaomi/cepheus_eea/cepheus:9/PKQ1.181121.001/V10.2.26.0.PFAEUXM:user/release-keys__2019-04-01
-Xiaomi Redmi Go (8.1.0)=Xiaomi/tiare/tiare:8.1.0/OPM1.171019.026/V10.2.6.0.OCLMIXM:user/release-keys__2019-01-01
-Xiaomi Redmi Note 5 Pro (9)=xiaomi/whyred/whyred:9/PKQ1.180904.001/V10.3.1.0.PEIMIXM:user/release-keys__2019-05-05
 "
