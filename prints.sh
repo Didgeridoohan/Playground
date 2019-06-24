fb7412417b832ae1c9917d9eed182bfeead34809
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-06-24 12:30:54 +0200

    testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Elephone P9000 (7.0)=alps/full_c239v55_dx/c239v55_dx:7.0/NRD90M/1504503914:user/release-keys
+Elephone P9000E (7.0)=alps/full_c239v55_dx_e/c239v55_dx_e:7.0/NRD90M/1491037216:user/release-keys
+Motorola Moto G6 (9)=motorola/ali/ali:9/PPS29.55-34/0c5d:user/release-keys__2019-02-01
+Xiaomi Mi 9 (10 Beta)=Xiaomi/cepheus/cepheus:10/QKQ1.190406.002/9.04.30.2.native.google.user:user/release-keys__2019-05-05
 "
