915bd84165d8c51d46d2ab376ce467a7f8126a1a
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-07-03 21:22:08 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -9,8 +9,6 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 Asus Zenfone 6 (9.0)=asus/WW_I01WD/ASUS_I01WD:9/PPR2.181005.003/16.1210.1906.156-0:user/release-keys__2019-06-01
-Elephone P9000 (7.0)=alps/full_c239v55_dx/c239v55_dx:7.0/NRD90M/1504503914:user/release-keys
-Elephone P9000E (7.0)=alps/full_c239v55_dx_e/c239v55_dx_e:7.0/NRD90M/1491037216:user/release-keys
 Essential PH-1 (9)=essential/mata/mata:9/PQ1A.190105.090/528:user/release-keys__2019-07-05
 Google Pixel (9)=google/sailfish/sailfish:9/PQ3A.190705.001/5565753:user/release-keys__2019-07-05
 Google Pixel XL (9)=google/marlin/marlin:9/PQ3A.190705.001/5565753:user/release-keys__2019-07-05
@@ -23,9 +21,9 @@ Google Pixel 3a XL (9)=google/bonito/bonito:9/PQ3B.190705.003/5622519:user/relea
 Huawei P20 Lite (8.0.0 & 9)=HUAWEI/ANE-LX1/HWANE:8.0.0/HUAWEIANE-LX1/180(C432):user/release-keys__2019-04-05;HUAWEI/ANE-LX1/HWANE:9/HUAWEIANE-L21/132C432R1:user/release-keys__2019-05-05
 Motorola Moto G4 (8.1.0)=motorola/athene/athene:8.1.0/OPJ28.111-22/532da:user/release-keys__2018-12-01
 Motorola Moto G6 (9)=motorola/ali/ali:9/PPS29.55-34/0c5d:user/release-keys__2019-02-01
+Motorola Moto G6 Plus (9)=motorola/evert_amx_n/evert_n:9/PPWS29.116-16-6/5cbfe:user/release-keys__2019-05-01
 OnePlus 5T (9)=OnePlus/OnePlus5T/OnePlus5T:9/PKQ1.180716.001/1905271747:user/release-keys__2019-05-01
 OnePlus 7 Pro (9)=OnePlus/OnePlus7Pro/OnePlus7Pro:9/PKQ1.190110.001/1906160616:user/release-keys__2019-05-01
 Sony Xperia XA2 Dual (8.0.0)=Sony/H4113/H4113:8.0.0/50.1.A.4.76/2209478692:user/release-keys
-Xiaomi Mi 9 (10 Beta)=Xiaomi/cepheus/cepheus:10/QKQ1.190406.002/9.04.30.2.native.google.user:user/release-keys__2019-05-05
 Xiaomi Pocophone F1 (9)=Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.3.5.0.PEJMIXM:user/release-keys__2019-05-01
 "
