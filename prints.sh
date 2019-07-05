94e598b9f1efc96aca7d591fe8c3f80395407a5c
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-07-05 12:50:01 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,22 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Asus Zenfone 6 (9.0)=asus/WW_I01WD/ASUS_I01WD:9/PPR2.181005.003/16.1210.1906.156-0:user/release-keys__2019-06-01
-Essential PH-1 (9)=essential/mata/mata:9/PQ1A.190105.090/528:user/release-keys__2019-07-05
-Google Pixel (9)=google/sailfish/sailfish:9/PQ3A.190705.001/5565753:user/release-keys__2019-07-05
-Google Pixel XL (9)=google/marlin/marlin:9/PQ3A.190705.001/5565753:user/release-keys__2019-07-05
-Google Pixel 2 (9)=google/walleye/walleye:9/PQ3A.190705.001/5565753:user/release-keys__2019-07-05
-Google Pixel 2 XL (9)=google/taimen/taimen:9/PQ3A.190705.001/5565753:user/release-keys__2019-07-05
-Google Pixel 3 (9)=google/blueline/blueline:9/PQ3A.190705.003/5600800:user/release-keys__2019-07-05
-Google Pixel 3 XL (9)=google/crosshatch/crosshatch:9/PQ3A.190705.003/5600800:user/release-keys__2019-07-05
-Google Pixel 3a (9)=google/sargo/sargo:9/PQ3B.190705.003/5622519:user/release-keys__2019-07-05
-Google Pixel 3a XL (9)=google/bonito/bonito:9/PQ3B.190705.003/5622519:user/release-keys__2019-07-05
-Huawei P20 Lite (8.0.0 & 9)=HUAWEI/ANE-LX1/HWANE:8.0.0/HUAWEIANE-LX1/180(C432):user/release-keys__2019-04-05;HUAWEI/ANE-LX1/HWANE:9/HUAWEIANE-L21/132C432R1:user/release-keys__2019-05-05
-Motorola Moto G4 (8.1.0)=motorola/athene/athene:8.1.0/OPJ28.111-22/532da:user/release-keys__2018-12-01
-Motorola Moto G6 (9)=motorola/ali/ali:9/PPS29.55-34/0c5d:user/release-keys__2019-02-01
-Motorola Moto G6 Plus (9)=motorola/evert_amx_n/evert_n:9/PPWS29.116-16-6/5cbfe:user/release-keys__2019-05-01
-OnePlus 5T (9)=OnePlus/OnePlus5T/OnePlus5T:9/PKQ1.180716.001/1905271747:user/release-keys__2019-05-01
-OnePlus 7 Pro (9)=OnePlus/OnePlus7Pro/OnePlus7Pro:9/PKQ1.190110.001/1906160616:user/release-keys__2019-05-01
-Sony Xperia XA2 Dual (8.0.0)=Sony/H4113/H4113:8.0.0/50.1.A.4.76/2209478692:user/release-keys
-Xiaomi Pocophone F1 (9)=Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.3.5.0.PEJMIXM:user/release-keys__2019-05-01
 "
