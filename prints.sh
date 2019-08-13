5e7b5a5c47b3c52f17f61dd8a03feedb07e07a29
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-08-13 15:18:51 +0200

    testing

--- prints.sh
+++ prints.sh
@@ -9,4 +9,5 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 Xiaomi Mi A3 (9)=Xiaomi/laurel_sprout_eea/laurel_sprout:9/PKQ1.190416.001/V10.3.5.0.PFQEUXM:user/release-keys__2019-07-05
+ZTE Nubia Red Magic 3 (9)=nubia/NX629J/NX629J:9/PKQ1.190321.001/eng.nubia.20190715.220013:user/release-keys__2019-03-05
 "
