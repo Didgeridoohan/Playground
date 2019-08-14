62c77c4693ad37d94162be081ba258943298192f
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-08-14 17:16:55 +0200

    testing

--- prints.sh
+++ prints.sh
@@ -10,6 +10,11 @@ PRINTSTRANSF=1
 PRINTSLIST="
 OnePlus 3 (9)=OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys__2019-08-01
 OnePlus 3T (9)=OnePlus/OnePlus3/OnePlus3T:9/PKQ1.181203.001/1907311932:user/release-keys__2019-08-01
+OnePlus 5 (9)=OnePlus/OnePlus5/OnePlus5:9/PKQ1.180716.001/1907311824:user/release-keys__2019-08-01
+OnePlus 5T (9)=OnePlus/OnePlus5T/OnePlus5T:9/PKQ1.180716.001/1907311828:user/release-keys__2019-08-01
+OnePlus 6 (9)=OnePlus/OnePlus6/OnePlus6:9/PKQ1.180716.001/1908012000:user/release-keys__2019-08-01
+OnePlus 6T (9)=OnePlus/OnePlus6T/OnePlus6T:9/PKQ1.180716.001/1908012000:user/release-keys__2019-08-01
+OnePlus 7 (9)=OnePlus/OnePlus7/OnePlus7:9/PKQ1.190110.001/1907280700:user/release-keys__2019-08-01
 Xiaomi Mi A3 (9)=Xiaomi/laurel_sprout_eea/laurel_sprout:9/PKQ1.190416.001/V10.3.5.0.PFQEUXM:user/release-keys__2019-07-05
 ZTE Nubia Red Magic 3 (9)=nubia/NX629J/NX629J:9/PKQ1.190321.001/eng.nubia.20190715.220013:user/release-keys__2019-03-05
 "
