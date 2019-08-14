5efad0935cb4acb854760432419e612d553ca95b
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-08-14 12:12:32 +0200

    testing

--- prints.sh
+++ prints.sh
@@ -8,6 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 3 (9)=OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys__2019-08-01
+OnePlus 3T (9)=OnePlus/OnePlus3/OnePlus3T:9/PKQ1.181203.001/1907311932:user/release-keys__2019-08-01
 Xiaomi Mi A3 (9)=Xiaomi/laurel_sprout_eea/laurel_sprout:9/PKQ1.190416.001/V10.3.5.0.PFQEUXM:user/release-keys__2019-07-05
 ZTE Nubia Red Magic 3 (9)=nubia/NX629J/NX629J:9/PKQ1.190321.001/eng.nubia.20190715.220013:user/release-keys__2019-03-05
 "
