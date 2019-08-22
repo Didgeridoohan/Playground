bac1e85fbe0381399f46721a966ff60d654b8aef
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-08-22 06:19:29 +0200

    testing

--- prints.sh
+++ prints.sh
@@ -8,5 +8,5 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Xiaomi Mi A3 (9)=Xiaomi/laurel_sprout_eea/laurel_sprout:9/PKQ1.190416.001/V10.3.5.0.PFQEUXM:user/release-keys__2019-07-05
+Xiaomi Mi A3 (9)=Xiaomi/laurel_sprout_eea/laurel_sprout:9/PKQ1.190416.001/V10.3.7.0.PFQEUXM:user/release-keys__2019-08-05
 "
