3c8855d1ad7baec839d4cb50cf978a856f278ebe
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-11-10 13:46:48 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Fairphone 2 (6.0.1)=Fairphone/FP2/FP2:6.0.1/FP2-gms-17.09.3/FP2-gms-17.09.3:user/release-keys
-Samsung Galaxt Tab A WiFi (9)=samsung/gta2xlwifixx/gta2xlwifi:9/PPR1.180610.011/T590XXU3BSJ1:user/release-keys__2019-10-01
 "
