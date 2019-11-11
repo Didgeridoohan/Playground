2c172f915fa25d140d9dbc6ccf1f6982eb1dca49
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-11-11 21:02:45 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,19 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Essential PH-1 (10)=essential/mata/mata:10/QP1A.191005.014/478:user/release-keys__2019-11-05
+OnePlus 6 (10)=OnePlus/OnePlus6/OnePlus6:10/QKQ1.190716.003/1910270526:user/release-keys__2019-10-01
+OnePlus 6T (10)=OnePlus/OnePlus6T/OnePlus6T:10/QKQ1.190716.003/1910270420:user/release-keys__2019-10-01
+OnePlus 7 Pro (10)=OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/1910071200:user/release-keys__2019-09-05
+OnePlus 7 Pro NR (9)=OnePlus/OnePlus7ProNR_EEA/OnePlus7ProNR:9/PKQ1.190223.001/1909251702:user/release-keys__2019-10-01
+OnePlus 7T (10)=OnePlus/OnePlus7T/OnePlus7T:10/QKQ1.190716.003/1909250940:user/release-keys__2019-09-05
+Google Pixel 2 (10)=google/walleye/walleye:10/QP1A.191105.004/5908170:user/release-keys__2019-11-05
+Google Pixel 2 XL (10)=google/taimen/taimen:10/QP1A.191105.004/5908170:user/release-keys__2019-11-05
+Google Pixel 3 (10)=google/blueline/blueline:10/QP1A.191105.003/5899767:user/release-keys__2019-11-05
+Google Pixel 3 XL (10)=google/crosshatch/crosshatch:10/QP1A.191105.003/5899767:user/release-keys__2019-11-05
+Google Pixel 3a (10)=google/sargo/sargo:10/QP1A.191105.003/5899767:user/release-keys__2019-11-05
+Google Pixel 3a XL (10)=google/bonito/bonito:10/QP1A.191105.003/5899767:user/release-keys__2019-11-05
+Google Pixel 4 (10)=google/flame/flame:10/QD1A.190821.007.A3/5891392:user/release-keys__2019-11-05
+Google Pixel 4 XL (10)=google/coral/coral:10/QD1A.190821.007.A3/5891392:user/release-keys__2019-11-05
+Xiaomi Mi 9 Lite (9)=Xiaomi/pyxis/pyxis:9/PKQ1.181121.001/V11.3.3.0.PFCMIXM:user/release-keys__2019-10-01
 "
