80be4fafff2d9dcf33f96b0bb8f5b3775db0df85
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-11-12 06:20:55 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -23,4 +23,5 @@ Google Pixel 3a XL (10)=google/bonito/bonito:10/QP1A.191105.003/5899767:user/rel
 Google Pixel 4 (10)=google/flame/flame:10/QD1A.190821.007.A3/5891392:user/release-keys__2019-11-05
 Google Pixel 4 XL (10)=google/coral/coral:10/QD1A.190821.007.A3/5891392:user/release-keys__2019-11-05
 Xiaomi Mi 9 Lite (9)=Xiaomi/pyxis/pyxis:9/PKQ1.181121.001/V11.3.3.0.PFCMIXM:user/release-keys__2019-10-01
+Xiaomi Redmi Note 7 Pro (9)=xiaomi/violet/violet:9/PKQ1.181203.001/V10.3.13.0.PFHINXM:user/release-keys__2019-10-01
 "
