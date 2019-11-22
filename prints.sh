771083c2ea634c34e0d6ad226d00ab316e55f4c0
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-11-22 20:00:36 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Samsung Galaxy A5 2017 (8.0.0)=samsung/a5y17ltexx/a5y17lte:8.0.0/R16NW/A520FXXUCCSK1:user/release-keys__2019-11-01
-Xiaomi Redmi K20 Pro/Mi 9T Pro (10)=Xiaomi/raphael/raphael:10/QKQ1.190825.002/V11.0.1.0.QFKMIXM:user/release-keys__2019-10-01
 "
