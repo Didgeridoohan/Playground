9687a411a3d1961e166363dee7dc7d787dd9164c
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-11-23 19:29:02 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Galaxy S10 Plus (10)=samsung/beyond2qltesq/beyond2q:10/QP1A.190711.020/G975USQU2ZSJF:user/release-keys__2019-11-01
+Nvidia Shield TV 2017 (8 & 9)=NVIDIA/darcy/darcy:8.0.0/OPR6.170623.010/3507953_1441.7411:user/release-keys__2018-12-05;NVIDIA/darcy/darcy:9/PPR1.180610.011/4086636_1604.6430:userdebug/test-keys__2019-07-05
+Nvidia Shield TV Pro (8 & 9)=NVIDIA/foster_e_hdd/foster:8.0.0/OPR6.170623.010/3507953_1441.7411:user/release-keys__2018-12-05;NVIDIA/foster_e_hdd/foster:9/PPR1.180610.011/4086636_1604.6430:userdebug/test-keys__2019-07-05
 "
