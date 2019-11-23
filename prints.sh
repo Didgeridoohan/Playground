118ee24e8fce0a9605c4434b21e99352ab5c13f7
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-11-23 19:30:17 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -11,4 +11,5 @@ PRINTSLIST="
 Galaxy S10 Plus (10)=samsung/beyond2qltesq/beyond2q:10/QP1A.190711.020/G975USQU2ZSJF:user/release-keys__2019-11-01
 Nvidia Shield TV 2017 (8 & 9)=NVIDIA/darcy/darcy:8.0.0/OPR6.170623.010/3507953_1441.7411:user/release-keys__2018-12-05;NVIDIA/darcy/darcy:9/PPR1.180610.011/4086636_1604.6430:userdebug/test-keys__2019-07-05
 Nvidia Shield TV Pro (8 & 9)=NVIDIA/foster_e_hdd/foster:8.0.0/OPR6.170623.010/3507953_1441.7411:user/release-keys__2018-12-05;NVIDIA/foster_e_hdd/foster:9/PPR1.180610.011/4086636_1604.6430:userdebug/test-keys__2019-07-05
+Xiaomi Redmi Note 5 Pro (9)=xiaomi/whyred/whyred:9/PKQ1.180904.001/V11.0.3.0.PEIMIXM:user/release-keys__2019-11-05
 "
