1f5158c7c50229fdb26617e7d742f4ec56b1f0fb
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-11-26 07:12:40 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,8 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Samsung Galaxt Tab A SM-T595 (9)=samsung/gta2xlltexx/gta2xllte:9/PPR1.180610.011/T595XXU3BSF3:user/release-keys__2019-06-01
-Samsung Galaxt Tab A SM-T597 (9)=samsung/gta2xlltespr/gta2xlltespr:9/PPR1.180610.011/T597PVPU2BSH7:user/release-keys__2019-08-01
+Samsung Galaxt Tab A LTE SM-T595 (9)=samsung/gta2xlltexx/gta2xllte:9/PPR1.180610.011/T595XXU3BSF3:user/release-keys__2019-06-01
+Samsung Galaxt Tab A LTE SM-T597 (9)=samsung/gta2xlltespr/gta2xlltespr:9/PPR1.180610.011/T597PVPU2BSH7:user/release-keys__2019-08-01
 Nvidia Shield TV 2017 (9)=NVIDIA/darcy/darcy:9/PPR1.180610.011/4086637_1604.6430:user/release-keys__2019-07-05
 Nvidia Shield TV Pro (9)=NVIDIA/foster_e_hdd/foster:9/PPR1.180610.011/4086637_1604.6430:user/release-keys__2019-07-05
 "
