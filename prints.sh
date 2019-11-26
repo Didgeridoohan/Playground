240a98658306cb4f9c72aa370e4691a773a86e76
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-11-26 13:30:06 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -12,4 +12,5 @@ Samsung Galaxt Tab A LTE SM-T595 (9)=samsung/gta2xlltexx/gta2xllte:9/PPR1.180610
 Samsung Galaxt Tab A LTE SM-T597 (9)=samsung/gta2xlltespr/gta2xlltespr:9/PPR1.180610.011/T597PVPU2BSH7:user/release-keys__2019-08-01
 Nvidia Shield TV 2017 (9)=NVIDIA/darcy/darcy:9/PPR1.180610.011/4086637_1604.6430:user/release-keys__2019-07-05
 Nvidia Shield TV Pro (9)=NVIDIA/foster_e_hdd/foster:9/PPR1.180610.011/4086637_1604.6430:user/release-keys__2019-07-05
+Xiaomi Redmi Note 8T (9)=xiaomi/willow_eea/willow:9/PKQ1.190616.001/V10.3.5.0.PCXEUXM:user/release-keys__2019-10-01
 "
