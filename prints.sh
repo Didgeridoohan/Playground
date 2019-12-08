a374e9fa0cd82d55c5a48e088e9398e994ee8eb1
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-12-08 07:40:23 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Samsung A3 2015 (6.0.1)=samsung/a3ultexx/a3ulte:6.0.1/MMB29M/A300FUXXU1CRF1:user/release-keys
-Xiaomi Redmi Note 4 (7.0)=xiaomi/mido/mido:7.0/NRD90M/V9.6.1.0.NCFMIFD:user/release-keys
 "
