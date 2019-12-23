1745ee703f05a01435e9ac5b4c11b81330644e81
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-12-23 03:22:21 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,22 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Pixel (10)=google/sailfish/sailfish:10/QP1A.191005.007.A3/5972272:user/release-keys__2019-10-06
-Google Pixel XL (10)=google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys__2019-10-06
-Google Pixel 2 (10)=google/walleye/walleye:10/QQ1A.191205.008/5974827:user/release-keys__2019-12-05
-Google Pixel 2 XL (10)=google/taimen/taimen:10/QQ1A.191205.008/5974827:user/release-keys__2019-12-05
-Google Pixel 3 (10)=google/blueline/blueline:10/QQ1A.191205.008/5974827:user/release-keys__2019-12-05
-Google Pixel 3 XL (10)=google/crosshatch/crosshatch:10/QQ1A.191205.008/5974827:user/release-keys__2019-12-05
-Google Pixel 3a (10)=google/sargo/sargo:10/QQ1A.191205.011/6009058:user/release-keys__2019-12-05
-Google Pixel 3a XL (10)=google/bonito/bonito:10/QQ1A.191205.011/6009058:user/release-keys__2019-12-05
-Google Pixel 4 (10)=google/flame/flame:10/QQ1B.191205.012.A1/6015823:user/release-keys__2019-12-05
-Google Pixel 4 XL (10)=google/coral/coral:10/QQ1B.191205.012.A1/6015823:user/release-keys__2019-12-05
-OnePlus 3 (9)=OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1911042107:user/release-keys__2019-10-01
-OnePlus 3T (9)=OnePlus/OnePlus3/OnePlus3T:9/PKQ1.181203.001/1911042108:user/release-keys__2019-10-01
-OnePlus 6 (10)=OnePlus/OnePlus6/OnePlus6:10/QKQ1.190716.003/1911142022:user/release-keys__2019-10-01
-OnePlus 6T (10)=OnePlus/OnePlus6T/OnePlus6T:10/QKQ1.190716.003/1911142147:user/release-keys__2019-10-01
-Samsung Galaxy A51 (10)=samsung/a51nsxx/a51:10/QP1A.190711.020/A515FXXU1ASKJ:user/release-keys__2019-12-01
-Samsung Galaxy M20 (10)=samsung/m20ltedd/m20lte:10/QP1A.190711.020/M205FDDU3CSL4:user/release-keys__2019-12-01
-Samsung Galaxy S10 Plus Exynos (10)=samsung/beyond2ltexx/beyond2:10/QP1A.190711.020/G975FXXU3BSKO:user/release-keys__2019-12-01
-Xiaomi Redmi Note 8 Pro (9)=Redmi/begoniain/begoniain:9/PPR1.180610.011/V10.4.4.0.PGGINXM:user/release-keys__2019-09-01
 "
