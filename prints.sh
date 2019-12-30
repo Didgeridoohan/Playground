7b63043992d5ea0f2d0f1e7239863471bba4a324
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2019-12-30 19:39:00 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Lenovo K6 Power (7.0)=Lenovo/karate/K33a42:7.0/NRD90N/K33_S231_171114_ROW:user/release-keys
+Xiaomi Mi Mix 2 (9)=Xiaomi/chiron/chiron:9/PKQ1.190118.001/V11.0.3.0.PDEMIXM:user/release-keys__2019-09-01
+Xiaomi Redmi Note 4/4X (7.0)=xiaomi/mido/mido:7.0/NRD90M/V11.0.2.0.NCFMIXM:user/release-keys__2018-12-01
 "
