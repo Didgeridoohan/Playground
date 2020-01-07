21a6cdee9cba721f7ad3015f6803ff453400af3a
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-01-07 22:13:29 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -9,7 +9,7 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 Xiaomi Mi 9T European (10)=Xiaomi/davinci_eea/davinci:10/QKQ1.190825.002/V11.0.1.0.QFJEUXM:user/release-keys__2019-12-01
-Xiaomi Mi 9T International (10)=Xiaomi/davinci/davinci:10/QKQ1.190825.002/V11.0.1.0.QFJMIXM:user/release-keys__2019-12-01
+Xiaomi Mi 9T Global (10)=Xiaomi/davinci/davinci:10/QKQ1.190825.002/V11.0.1.0.QFJMIXM:user/release-keys__2019-12-01
 Xiaomi Mi Mix 2 (9)=Xiaomi/chiron/chiron:9/PKQ1.190118.001/V11.0.3.0.PDEMIXM:user/release-keys__2019-09-01
 Xiaomi Redmi Note 4/4X (7.0)=xiaomi/mido/mido:7.0/NRD90M/V11.0.2.0.NCFMIXM:user/release-keys__2018-12-01
 "
