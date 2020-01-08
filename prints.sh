80b2690216e301050fd6937244a6094ec887e63b
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-01-08 14:30:05 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Samsung Galaxy Note 10 Plus (10)=samsung/d2sxx/d2s:10/QP1A.190711.020/N975FXXU1BSL7:user/release-keys__2019-12-01
 Xiaomi Mi 9T European (10)=Xiaomi/davinci_eea/davinci:10/QKQ1.190825.002/V11.0.1.0.QFJEUXM:user/release-keys__2019-12-01
 Xiaomi Mi 9T Global (10)=Xiaomi/davinci/davinci:10/QKQ1.190825.002/V11.0.1.0.QFJMIXM:user/release-keys__2019-12-01
 Xiaomi Mi Mix 2 (9)=Xiaomi/chiron/chiron:9/PKQ1.190118.001/V11.0.3.0.PDEMIXM:user/release-keys__2019-09-01
