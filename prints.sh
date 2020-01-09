319c51547d518c7d4421101800b1d74f1f242b05
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-01-09 21:35:08 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,22 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Essential PH-1 (10)=essential/mata/mata:10/QQ1A.200105.007/515:user/release-keys__2020-01-05
-Google Pixel 2 (10)=google/walleye/walleye:10/QQ1A.200105.002/6031801:user/release-keys__2020-01-01
-Google Pixel 2 XL (10)=google/taimen/taimen:10/QQ1A.200105.002/6031801:user/release-keys__2020-01-01
-Google Pixel 3 (10)=google/blueline/blueline:10/QQ1A.200105.002/6031801:user/release-keys__2020-01-01
-Google Pixel 3 XL (10)=google/crosshatch/crosshatch:10/QQ1A.200105.002/6031801:user/release-keys__2020-01-01
-Google Pixel 3a (10)=google/sargo/sargo:10/QQ1A.200105.002/6031801:user/release-keys__2020-01-01
-Google Pixel 3a XL (10)=google/bonito/bonito:10/QQ1A.200105.002/6031801:user/release-keys__2020-01-01
-Google Pixel 4 (10)=google/flame/flame:10/QQ1B.200105.004/6031802:user/release-keys__2020-01-01
-Google Pixel 4 XL (10)=google/coral/coral:10/QQ1B.200105.004/6031802:user/release-keys__2020-01-01
-Motorola Moto G7 Power (9)=motorola/ocean/ocean:9/PPOS29.114-134-7/d9234:user/release-keys__2019-12-05
-OnePlus 5 (9)=OnePlus/OnePlus5/OnePlus5:9/PKQ1.180716.001/1912311102:user/release-keys__2019-12-01
-OnePlus 5T (9)=OnePlus/OnePlus5T/OnePlus5T:9/PKQ1.180716.001/1912311102:user/release-keys__2019-12-01
-OnePlus 6T (10)=OnePlus/OnePlus6T/OnePlus6T:10/QKQ1.190716.003/1912142009:user/release-keys__2019-11-01
-Samsung Galaxy Note 10 Plus (10)=samsung/d2sxx/d2s:10/QP1A.190711.020/N975FXXU1BSL7:user/release-keys__2019-12-01
-Xiaomi Mi 9T European (10)=Xiaomi/davinci_eea/davinci:10/QKQ1.190825.002/V11.0.1.0.QFJEUXM:user/release-keys__2019-12-01
-Xiaomi Mi 9T Global (10)=Xiaomi/davinci/davinci:10/QKQ1.190825.002/V11.0.1.0.QFJMIXM:user/release-keys__2019-12-01
-Xiaomi Mi Mix 2 (9)=Xiaomi/chiron/chiron:9/PKQ1.190118.001/V11.0.3.0.PDEMIXM:user/release-keys__2019-09-01
-Xiaomi Redmi Note 4/4X (7.0)=xiaomi/mido/mido:7.0/NRD90M/V11.0.2.0.NCFMIXM:user/release-keys__2018-12-01
 "
