38edea4149eb75054bfe5c5e8b15ab5e707184a8
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-01-11 22:00:14 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Motorola Moto G5S (8.1.0)=motorola/montana/montana:8.1.0/OPPS28.65-37-7-12/073f74:user/release-keys__2019-08-01
+Motorola Moto G7 (9)=motorola/river_retru_n/river_n:9/PPOS29.114-134-7/b6963:user/release-keys__2019-12-05
+Motorola Moto G7 Power (9)=motorola/ocean/ocean:9/PPOS29.114-134-7/d9234:user/release-keys__2019-12-01
+Razer Phone 2 (9)=razer/cheryl2/aura:9/P-MR2-RC001-RZR-190914/3123:user/release-keys__2019-09-05
 "
