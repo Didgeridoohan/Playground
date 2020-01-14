7a09e59bde66a72507a5954ca8cb6018bf108928
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-01-14 20:05:56 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -10,4 +10,6 @@ PRINTSTRANSF=1
 PRINTSLIST="
 Motorola Moto G7 Power (9)=motorola/ocean/ocean:9/PPOS29.114-134-7/d9234:user/release-keys__2019-12-01
 Razer Phone 2 (9)=razer/cheryl2/aura:9/P-MR2-RC001-RZR-190914/3123:user/release-keys__2019-09-05
+Xiaomi Mi 9 SE (9)=Xiaomi/grus/grus:9/PKQ1.181121.001/V10.3.4.0.PFBMIXM:user/release-keys__2019-08-01
+Xiaomi Mi 9 SE (9)=Xiaomi/grus/grus:9/PKQ1.181121.001/V11.0.4.0.PFBMIXM:user/release-keys__2019-10-01
 "
