ce601605e02b66649f599afb7b2cc5e11f7009c4
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-01-15 20:09:05 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -9,7 +9,9 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 Motorola Moto G7 Power (9)=motorola/ocean/ocean:9/PPOS29.114-134-7/d9234:user/release-keys__2019-12-01
+OnePlus 7 Pro NR GM1920 (9)=OnePlus/OnePlus7ProNR_EEA/OnePlus7ProNR:9/PKQ1.190223.001/1912171007:user/release-keys__2019-12-01
 Razer Phone 2 (9)=razer/cheryl2/aura:9/P-MR2-RC001-RZR-190914/3123:user/release-keys__2019-09-05
+Samsung Galaxy Core Prime (5.1.1)=samsung/coreprimeveltexx/coreprimevelte:5.1.1/LMY48B/G361FXXU1APB1:user/release-keys
 Xiaomi Mi 9 SE (9)=Xiaomi/grus/grus:9/PKQ1.181121.001/V10.3.4.0.PFBMIXM:user/release-keys__2019-08-01
 Xiaomi Mi 9 SE (9)=Xiaomi/grus/grus:9/PKQ1.181121.001/V11.0.4.0.PFBMIXM:user/release-keys__2019-10-01
 "
