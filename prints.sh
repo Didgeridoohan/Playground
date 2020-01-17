8cbc317af1838c665cc38687fb8f7b6ade6ed675
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-01-17 09:16:05 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Motorola Moto G5S (8.1.0)=motorola/montana/montana:8.1.0/OPPS28.65-37-7-12/073f74:user/release-keys__2019-08-01
-Motorola Moto G7 (9)=motorola/river_retru_n/river_n:9/PPOS29.114-134-7/b6963:user/release-keys__2019-12-05
 "
