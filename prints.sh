9465541f62c03030512f00a262f131aed2fe8f2b
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-01-18 17:47:43 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Motorola Moto G5S Plus (8.1.0)=motorola/sanders_n/sanders_n:8.1.0/OPS28.65-36-14/63857:user/release-keys__2019-08-01
+OnePlus 7T Pro NR HD1925 (10)=OnePlus/OnePlus7TProNR/OnePlus7TProNR:10/QKQ1.190716.003/2001101147:user/release-keys__2019-12-01
+Samsung Galaxy A20 (9)=samsung/a20cs/a20:9/PPR1.180610.011/A205WVLU1ASE7:user/release-keys__2019-08-01
 "
