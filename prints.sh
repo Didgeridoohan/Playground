6aef3b7a5ba805ac933f40ee7e34258f1fe80f76
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-01-20 19:57:11 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,21 +8,4 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Huawei Mate 20 Lite (9)=HUAWEI/SNE-LX1EEA/HWSNE:9/HUAWEISNE-L01/245EEAC782:user/release-keys__2019-09-01
-Motorola Moto G5S Plus (8.1.0)=motorola/sanders_n/sanders_n:8.1.0/OPS28.65-36-14/63857:user/release-keys__2019-08-01
-OnePlus 5T (7.1.1 & 8.0.0)=OnePlus/OnePlus5T/OnePlus5T:7.1.1/NMF26X/12152312:user/release-keys;OnePlus/OnePlus5T/OnePlus5T:8.0.0/OPR1.170623.032/02040656:user/release-keys
-OnePlus 6 (10)=OnePlus/OnePlus6/OnePlus6:10/QKQ1.190716.003/1912142009:user/release-keys__2019-11-01
-OnePlus 6T T-Mobile (9)=OnePlus/OnePlus6TSingle/OnePlus6TSingle:9/PKQ1.180716.001/1810291147:user/release-keys__2018-11-01
-OnePlus 7 GM1901/GM1905 (10)=OnePlus/OnePlus7/OnePlus7:10/QKQ1.190716.003/1912141900:user/release-keys__2019-11-01
-OnePlus 7 GM1903 (10)=OnePlus/OnePlus7_EEA/OnePlus7:10/QKQ1.190716.003/1912061900:user/release-keys__2019-11-01
-OnePlus 7 Pro GM1911/GM1917 (10)=OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/1912141900:user/release-keys__2019-11-01
-OnePlus 7 Pro GM1913 (10)=OnePlus/OnePlus7Pro_EEA/OnePlus7Pro:10/QKQ1.190716.003/1912061900:user/release-keys__2019-11-01
-OnePlus 7 Pro GM1915 (9)=OnePlus/OnePlus7ProTMO/OnePlus7ProTMO:9/PKQ1.190110.001/1905021004:user/release-keys__2019-03-01
-OnePlus 7 Pro NR Spr GM1925 (9)=OnePlus/OP7ProNRSpr/OP7ProNRSpr:9/PKQ1.190223.001/1908201041:user/release-keys__2019-08-01
-OnePlus 7T HD1901/HD1905 (10)=OnePlus/OnePlus7T/OnePlus7T:10/QKQ1.190716.003/1912060025:user/release-keys__2019-11-01
-OnePlus 7T HD1903 (10)=OnePlus/OnePlus7T_EEA/OnePlus7T:10/QKQ1.190716.003/1912060025:user/release-keys__2019-11-01
-OnePlus 7T Pro HD1911/HD1917 (10)=OnePlus/OnePlus7TPro/OnePlus7TPro:10/QKQ1.190716.003/1912060020:user/release-keys__2019-11-01
-OnePlus 7T Pro HD1913 (10)=OnePlus/OnePlus7TPro_EEA/OnePlus7TPro:10/QKQ1.190716.003/1912060020:user/release-keys__2019-11-01
-OnePlus 7T Pro NR HD1925 (10)=OnePlus/OnePlus7TProNR/OnePlus7TProNR:10/QKQ1.190716.003/2001101147:user/release-keys__2019-12-01
-Samsung Galaxy A7 (8.0.0)=samsung/a7y17ltexx/a7y17lte:8.0.0/R16NW/A720FXXU9CSK3:user/release-keys__2019-12-01
 "
