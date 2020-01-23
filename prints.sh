b41bbb515524d010505d3aecef0578a9893b7341
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-01-23 13:44:58 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,6 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Xiaomi Mi Note 10 European (9)=Xiaomi/tucana_eea/tucana:9/PKQ1.190302.001/V11.0.11.0.PFDEUXM:user/release-keys__2019-11-01
-Xiaomi Pocophone F1 (10)=Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.4.0.QEJMIXM:user/release-keys__2020-01-01
+Xiaomi Mi Note 10 European (9)=
+Xiaomi Pocophone F1 (10)=
 "
