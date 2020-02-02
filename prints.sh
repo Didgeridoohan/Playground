f7c24127e2f0f98a6f26bc9a94496af48eaf2450
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-02-02 13:07:04 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,4 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Nokia 7.1 (10)=Nokia/Crystal_00WW/CTL_sprout:10/QKQ1.190828.002/00WW_4_08B:user/release-keys__2019-11-01
 "
+
+# Contributors:
+# @Max1Truc
