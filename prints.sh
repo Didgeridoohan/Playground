1b3b08feab8304562413e87027104188fca82710
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-02-02 19:42:40 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,8 +8,15 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Nokia 7.1 (10)=Nokia/Crystal_00WW/CTL_sprout:10/QKQ1.190828.002/00WW_4_08B:user/release-keys__2019-11-01
+Nokia 6 TA-1039 (9)=Nokia/TA-1039_00WW/PLE:9/PKQ1.181105.001/00WW_6_19C:user/release-keys__2020-01-01
+Nokia 6 TA-1033 (9)=Nokia/TA-1033_00WW/PLE:9/PKQ1.181105.001/00WW_6_19C:user/release-keys__2020-01-01
+Nokia 6 TA-1021 (9)=Nokia/TA-1021_00WW/PLE:9/PKQ1.181105.001/00WW_6_19C:user/release-keys__2020-01-01
+Nokia 6 TA-1025 (9)=Nokia/TA-1025_00WW/PLE:9/PKQ1.181105.001/00WW_6_19C:user/release-keys__2020-01-01
+Nokia 6.2 (9)=Nokia/Starlord_00WW/SLD_sprout:9/PKQ1.190118.001/00WW_1_170:user/release-keys__2020-01-05
+Nokia 7.1 TA-1095 (10)=Nokia/Crystal_00WW/CTL_sprout:10/QKQ1.190828.002/00WW_4_08B:user/release-keys__2019-11-01
+OnePlus 6 (10)=OnePlus/OnePlus6/OnePlus6:10/QKQ1.190716.003/2001151848:user/release-keys__2019-12-01
+OnePlus 6T (10)=OnePlus/OnePlus6T/OnePlus6T:10/QKQ1.190716.003/2001151848:user/release-keys__2019-12-01
 "
 
 # Contributors:
-# @Max1Truc
+# @Max1Truc, @Some_Random_Username
