bfceb356c08ca4a38d3932a77a7c6fa938e73856
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-02-04 17:51:25 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,15 +8,25 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Essential PH-1 (10)=essential/mata/mata:10/QQ1A.200105.032/540:user/release-keys__2020-02-05
+Google Pixel 2 (10)=google/walleye/walleye:10/QQ1A.200205.002/6084386:user/release-keys__2020-02-05
+Google Pixel 2 XL (10)=google/taimen/taimen:10/QQ1A.200205.002/6084386:user/release-keys__2020-02-05
+Google Pixel 3 (10)=google/blueline/blueline:10/QQ1A.200205.002/6084386:user/release-keys__2020-02-05
+Google Pixel 3 XL (10)=google/crosshatch/crosshatch:10/QQ1A.200205.002/6084386:user/release-keys__2020-02-05
+Google Pixel 3a (10)=google/sargo/sargo:10/QQ1A.200205.002/6084386:user/release-keys__2020-02-05
+Google Pixel 3a XL (10)=google/bonito/bonito:10/QQ1A.200205.002/6084386:user/release-keys__2020-02-05
+Google Pixel 4 (10)=google/flame/flame:10/QQ1B.200205.002/6084387:user/release-keys__2020-02-05
+Google Pixel 4 XL (10)=google/coral/coral:10/QQ1B.200205.002/6084387:user/release-keys__2020-02-05
 Nokia 6 TA-1021 (9)=Nokia/TA-1021_00WW/PLE:9/PKQ1.181105.001/00WW_6_19C:user/release-keys__2020-01-01
 Nokia 6 TA-1025 (9)=Nokia/TA-1025_00WW/PLE:9/PKQ1.181105.001/00WW_6_19C:user/release-keys__2020-01-01
 Nokia 6 TA-1033 (9)=Nokia/TA-1033_00WW/PLE:9/PKQ1.181105.001/00WW_6_19C:user/release-keys__2020-01-01
 Nokia 6 TA-1039 (9)=Nokia/TA-1039_00WW/PLE:9/PKQ1.181105.001/00WW_6_19C:user/release-keys__2020-01-01
 Nokia 6.2 (9)=Nokia/Starlord_00WW/SLD_sprout:9/PKQ1.190118.001/00WW_1_170:user/release-keys__2020-01-05
 Nokia 7.1 TA-1095 (10)=Nokia/Crystal_00WW/CTL_sprout:10/QKQ1.190828.002/00WW_4_08B:user/release-keys__2019-11-01
+OnePlus 5T (9)=OnePlus/OnePlus5T/OnePlus5T:9/PKQ1.180716.001/1912311102:user/release-keys__2019-12-01
 OnePlus 6 (10)=OnePlus/OnePlus6/OnePlus6:10/QKQ1.190716.003/2001151848:user/release-keys__2019-12-01
 OnePlus 6T (10)=OnePlus/OnePlus6T/OnePlus6T:10/QKQ1.190716.003/2001151848:user/release-keys__2019-12-01
 "
 
 # Contributors:
-# @Max1Truc, @Some_Random_Username
+# @Max1Truc, @Some_Random_Username, @ipdev
