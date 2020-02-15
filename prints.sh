32c57d5cc248d51c1df012dbd6d6c54a8fc688b1
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-02-15 14:30:06 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 7 GM1901/GM1905 (10)=OnePlus/OnePlus7/OnePlus7:10/QKQ1.190716.003/2002021900:user/release-keys__2020-01-01
 OnePlus 7 Pro GM1911/1917 (10)=OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/2002021900:user/release-keys__2020-01-01
 OnePlus 7T Pro HD1911/1917 (10)=OnePlus/OnePlus7TPro/OnePlus7TPro:10/QKQ1.190716.003/2002022020:user/release-keys__2020-01-01
 OnePlus 7T Pro HD1913 (10)=OnePlus/OnePlus7TPro_EEA/OnePlus7TPro:10/QKQ1.190716.003/2002022020:user/release-keys__2020-01-01
