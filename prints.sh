5e57e0f4c4dab352a9b0c5056472321ebe94bbd7
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-02-18 21:19:18 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,9 +8,9 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Nokia 6.1 Plus (10)=Nokia/Dragon_00WW/DRG_sprout:10/QKQ1.190828.002/00WW_4_10D:user/release-keys__2020-01-01
-OnePlus 7 (GM1903) (10)=OnePlus/OnePlus7_EEA/OnePlus7:10/QKQ1.190716.003/2002021900:user/release-keys__2020-01-01
-OnePlus 7 Pro(GM1913) (10)=OnePlus/OnePlus7Pro_EEA/OnePlus7Pro:10/QKQ1.190716.003/2002021900:user/release-keys__2020-01-01
+Nokia 6.1 Plus (10)=
+OnePlus 7 (GM1903) (10)=
+OnePlus 7 Pro(GM1913) (10)=
 "
 
 # Contributors:
