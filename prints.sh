98df9ec1e41a607fb27ae9307f3336d8f58cc19e
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-02-21 09:09:03 +0100

    testing

--- prints.sh
+++ prints.sh
@@ -10,7 +10,8 @@ PRINTSTRANSF=1
 PRINTSLIST="
 OnePlus 7T HD1903 (10)=OnePlus/OnePlus7T_EEA/OnePlus7T:10/QKQ1.190716.003/2002022021:user/release-keys__2020-01-01
 OnePlus 7T HD1901/1905 (10)=OnePlus/OnePlus7T/OnePlus7T:10/QKQ1.190716.003/2002022020:user/release-keys__2020-01-01
+Xiaomi Redmi Note 8 (9)=Xiaomi/ginkgo/ginkgo:9/PKQ1.190616.001/V11.0.6.0.PCOMIXM:user/release-keys__2020-01-01
 "
 
 # Contributors:
-# @Some_Random_Username, @Sid127
+# @Some_Random_Username, @Sid127, @Hafitz Setya
