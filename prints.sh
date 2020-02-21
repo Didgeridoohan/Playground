bdf0eb00815430d0fd744ede8922176a363cf1b8
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-02-21 14:10:52 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,8 +8,6 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-OnePlus 7T HD1903 (10)=OnePlus/OnePlus7T_EEA/OnePlus7T:10/QKQ1.190716.003/2002022021:user/release-keys__2020-01-01
-OnePlus 7T HD1901/1905 (10)=OnePlus/OnePlus7T/OnePlus7T:10/QKQ1.190716.003/2002022020:user/release-keys__2020-01-01
 "
 
 # Contributors:
