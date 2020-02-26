2d0574084859ed2848581b452230c428f6d976b7
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-02-26 08:00:53 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,11 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+HTC U12 Plus (9)=htc/imeuhl_00617/htc_imeuhl:9/PQ2A.190205.003/1087121.1:user/release-keys__2019-07-01
+Samsung Galaxy Note 9 (10)=samsung/crownltexx/crownlte:10/QP1A.190711.020/N960FXXS4DTA5:user/release-keys__2020-02-01
+Vernee Mars Pro (7.0)=vernee/Mars_pro/Mars_pro:7.0/NRD90M/1506326000:user/release-keys
+Xiaomi Mi Box S (9)=Xiaomi/oneday/oneday:9/PI/2231:user/release-keys__2019-11-05
 "
 
 # Contributors:
-# @Some_Random_Username, @Sid127
+# @Some_Random_Username, @Sid127, @aerodown, @babaren, @jeanrivera
