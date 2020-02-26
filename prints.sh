171633736f7f2d7543627b9819cc458b9406fbd5
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-02-26 09:42:15 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,9 +8,6 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-HTC U12 Plus (9)=htc/imeuhl_00617/htc_imeuhl:9/PQ2A.190205.003/1087121.1:user/release-keys__2019-07-01
-Samsung Galaxy Note 9 (10)=samsung/crownltexx/crownlte:10/QP1A.190711.020/N960FXXS4DTA5:user/release-keys__2020-02-01
-Xiaomi Mi Box S (9)=Xiaomi/oneday/oneday:9/PI/2231:user/release-keys__2019-11-05
 "
 
 # Contributors:
