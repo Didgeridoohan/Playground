e20bb3ede2706d6042c997472c2f83a41fb707c7
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-02-29 11:35:29 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,10 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 6 (10)=OnePlus/OnePlus6/OnePlus6:10/QKQ1.190716.003/2002220019:user/release-keys__2020-02-01
+OnePlus 6T (10)=OnePlus/OnePlus6T/OnePlus6T:10/QKQ1.190716.003/2002220019:user/release-keys__2020-02-01
+Xiaomi Redmi 7 (9):Xiaomi:Redmi 7=xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys__2019-10-01
 "
 
 # Contributors:
-# 
+# @loco1911, @Some_Random_Username
