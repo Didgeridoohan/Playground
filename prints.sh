eee5a305ca8288fad0b167596ae34d3c23f6fe9f
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-03-02 11:45:45 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,8 +8,9 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Samsung Galaxy A5 2017 SM-A520F (8.0.0)=samsung/a5y17ltexx/a5y17lte:8.0.0/R16NW/A520FXXUCCTA5:user/release-keys__2020-01-01
 Xiaomi Redmi 7 (9):Xiaomi:Redmi 7=xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys__2019-10-01
 "
 
 # Contributors:
-# @loco1911
+# @loco1911, @swour
