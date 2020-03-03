3056e3f8ee5a1adaf217cfc6dc4d5d6b285abf1d
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-03-03 18:14:24 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -8,9 +8,10 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 7 Pro NR GM1920 (10)=OnePlus/OnePlus7ProNR_EEA/OnePlus7ProNR:10/QKQ1.190716.003/2002141334:user/release-keys__2020-01-01
 Samsung Galaxy A5 2017 SM-A520F (8.0.0)=samsung/a5y17ltexx/a5y17lte:8.0.0/R16NW/A520FXXUCCTA5:user/release-keys__2020-01-01
-Xiaomi Redmi 7 (9):Xiaomi:Redmi 7=xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys__2019-10-01
+Xiaomi Redmi 7 (9):Xiaomi:Redmi 7=xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys__2019-12-01
 "
 
 # Contributors:
-# @loco1911, @swour
+# @loco1911, @swour, @Some_Random_Username
