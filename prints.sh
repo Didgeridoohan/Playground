fb02953c706aac2fa6b18211cd90a89355053201
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-03-06 13:33:14 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,16 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Pixel 2 (10)=google/walleye/walleye:10/QQ2A.200305.002/6138846:user/release-keys__2020-03-05
-Google Pixel 2 XL (10)=google/taimen/taimen:10/QQ2A.200305.002/6138846:user/release-keys__2020-03-05
-Google Pixel 3 (10)=google/blueline/blueline:10/QQ2A.200305.002/6138846:user/release-keys__2020-03-05
-Google Pixel 3 XL (10)=google/crosshatch/crosshatch:10/QQ2A.200305.002/6138846:user/release-keys__2020-03-05
-Google Pixel 3a (10)=google/sargo/sargo:10/QQ2A.200305.002/6138846:user/release-keys__2020-03-05
-Google Pixel 3a XL (10)=google/bonito/bonito:10/QQ2A.200305.002/6138846:user/release-keys__2020-03-05
-Google Pixel 4 (10)=google/flame/flame:10/QQ2A.200305.003/6156912:user/release-keys__2020-03-05
-Google Pixel 4 XL (10)=google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys__2020-03-05
-Xiaomi Pocophone F1 (10)=Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.6.0.QEJMIXM:user/release-keys__2020-02-01
 "
 
 # Contributors:
-# @loco1911, @swour, @Some_Random_Username
+# @loco1911, @swour, @Some_Random_Username, @Sid220, @ipdev
