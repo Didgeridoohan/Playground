a2d7145a9102d5c5012ea12c0945fff2f2f50d36
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-03-06 11:05:17 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -16,10 +16,7 @@ Google Pixel 3a (10)=google/sargo/sargo:10/QQ2A.200305.002/6138846:user/release-
 Google Pixel 3a XL (10)=google/bonito/bonito:10/QQ2A.200305.002/6138846:user/release-keys__2020-03-05
 Google Pixel 4 (10)=google/flame/flame:10/QQ2A.200305.003/6156912:user/release-keys__2020-03-05
 Google Pixel 4 XL (10)=google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys__2020-03-05
-OnePlus 7 Pro NR GM1920 (10)=OnePlus/OnePlus7ProNR_EEA/OnePlus7ProNR:10/QKQ1.190716.003/2002141334:user/release-keys__2020-01-01
-Samsung Galaxy A5 2017 SM-A520F (8.0.0)=samsung/a5y17ltexx/a5y17lte:8.0.0/R16NW/A520FXXUCCTA5:user/release-keys__2020-01-01
 Xiaomi Pocophone F1 (10)=Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.6.0.QEJMIXM:user/release-keys__2020-02-01
-Xiaomi Redmi 7 (9):Xiaomi:Redmi 7=xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys__2019-12-01
 "
 
 # Contributors:
