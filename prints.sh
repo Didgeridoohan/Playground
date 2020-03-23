241c9f25672ba990b0affd4eb53479580a71d213
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-03-23 11:05:27 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,11 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Motorola Moto G7 (9):Motorola:moto g(7)=motorola/river/river:9/PPOS29.114-134-10/ca814:user/release-keys__2020-02-01
-Motorola Moto G7 Power (9):Motorola:moto g(7) power=motorola/ocean/ocean:9/PPOS29.114-134-10/21fefe:user/release-keys
-Motorola Moto G7 Channel (9):Motorola:moto g(7) channel=motorola/channel/channel:9/PPY29.105-160-4/56a11:user/release-keys__2019-12-01
-OnePlus 7 Pro 5G Sprint GM1925 (10)=OnePlus/OP7ProNRSpr/OP7ProNRSpr:10/QKQ1.190716.003/2002281825:user/release-keys__2020-02-01
 "
 
 # Contributors:
-# @Some_Random_Username, SyberHexen @ GitHub
+# 
