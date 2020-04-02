5e4c1848140f2fcc405f6dda08105f67e20436e5
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-04-02 06:53:24 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,6 +8,12 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+LG G7 ThinQ 1 (9):LGE:LG-G710=lge/judyln_lao_com/judyln:9/PKQ1.181105.001/1917215300e03.FGN:user/release-keys__2019-06-01
+LG G7 ThinQ 2 (9):LGE:LG-G710=lge/judyln_lao_com/judyln:9/PKQ1.181105.001/200350248e04d.FGN:user/release-keys__2020-01-01
+OnePlus 7 GM1901 and 1905 (10)=OnePlus/OnePlus7/OnePlus7:10/QKQ1.190716.003/2003131900:user/release-keys__2020-03-01
+OnePlus 7 GM1903 (10)=OnePlus/OnePlus7_EEA/OnePlus7:10/QKQ1.190716.003/2003131900:user/release-keys__2020-03-01
+OnePlus 7 Pro GM1911 and 1917 (10)=OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/2003131900:user/release-keys__2020-03-01
+OnePlus 7 Pro NR GM1920 (10)=OnePlus/OnePlus7ProNR_EEA/OnePlus7ProNR:10/QKQ1.190716.003/2003171325:user/release-keys__2020-03-01
 "
 
 # Contributors:
