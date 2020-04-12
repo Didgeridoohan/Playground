553a48d7a0e579a17f83785bf5f5f23b966be442
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-04-12 23:27:56 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,26 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Pixel 2 (10):Google:Pixel 2=google/walleye/walleye:10/QQ2A.200405.005/6254899:user/release-keys__2020-04-05
-Google Pixel 2 XL (10):Google:Pixel 2 XL=google/taimen/taimen:10/QQ2A.200405.005/6254899:user/release-keys__2020-04-05
-Google Pixel 3 (10):Google:Pixel 3=google/blueline/blueline:10/QQ2A.200405.005/6254899:user/release-keys__2020-04-05
-Google Pixel 3 XL (10):Google:Pixel 3 XL=google/crosshatch/crosshatch:10/QQ2A.200405.005/6254899:user/release-keys__2020-04-05
-Google Pixel 3a (10):Google:Pixel 3a=google/sargo/sargo:10/QQ2A.200405.005/6254899:user/release-keys__2020-04-05
-Google Pixel 3a XL (10):Google:Pixel 3a XL=google/bonito/bonito:10/QQ2A.200405.005/6254899:user/release-keys__2020-04-05
-Google Pixel 4 (10):Google:Pixel 4=google/flame/flame:10/QQ2A.200405.005/6254899:user/release-keys__2020-04-05
-Google Pixel 4 XL (10):Google:Pixel 4 XL=google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys__2020-04-05
-Huawei P20 EML-L09 (9)=HUAWEI/EML-L09/HWEML:9/HUAWEIEML-L09/9.1.0.381C109:user/release-keys__2020-01-01
-Meizu X8 (8.1.0):Meizu:meizu X8=Meizu/meizu_X8/X8:8.1.0/OPM1.171019.026/1556996821:user/release-keys__2019-04-01
-Motorola Moto G7 (9)=motorola/river/river:9/PPOS29.114-134-10/ca814:user/release-keys__2020-02-01
-OnePlus 5 (10)=OnePlus/OnePlus5/OnePlus5:10/QKQ1.191014.012/2003241826:user/release-keys__2020-02-05
-OnePlus 7 Pro GM1913 (10)=OnePlus/OnePlus7Pro_EEA/OnePlus7Pro:10/QKQ1.190716.003/2003131900:user/release-keys__2020-03-01
-OnePlus 7T HD1901 and 1905 (10)=OnePlus/OnePlus7T/OnePlus7T:10/QKQ1.190716.003/2003271900:user/release-keys__2020-03-01
-OnePlus 7T HD1903 (10)=OnePlus/OnePlus7T_EEA/OnePlus7T:10/QKQ1.190716.003/2003271900:user/release-keys__2020-03-01
-OnePlus 7T Pro HD1911 and HD1917 (10)=OnePlus/OnePlus7TPro/OnePlus7TPro:10/QKQ1.190716.003/2003271900:user/release-keys__2020-03-01
-OnePlus 7T Pro HD1913 (10)=OnePlus/OnePlus7TPro_EEA/OnePlus7TPro:10/QKQ1.190716.003/2003271900:user/release-keys__2020-03-01
-POCO X2 (10):POCO:POCO X2=POCO/phoenixin/phoenixin:10/QKQ1.190825.002/V11.0.6.0.QGHINXM:user/release-keys__2020-03-01
-Xiaomi Mi 6 (9):Xiaomi:MI 6=Xiaomi/sagit/sagit:9/PKQ1.190118.001/V11.0.6.0.PCAMIXM:user/release-keys__2019-09-01
 "
 
 # Contributors:
-# @Some_Random_Username, @ipdev, @ricaoalberto, @xiangnan
+# 
