e9e1111969096499461be6bdf5f531e54d761cfd
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-04-20 09:20:22 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,12 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Infinix Note 5 (8.1.0 & 9 & 10):Infinix:Infinix X604=Infinix/H633/Infinix-X604_sprout:8.1.0/O11019/H-181114V256:user/release-keys;Infinix/H633/Infinix-X604_sprout:9/PPR1.180610.011/H-190617V240:user/release-keys__2019-06-05;Infinix/H633/Infinix-X604_sprout:10/QP1A.190711.020/H-191227V306:user/release-keys__2020-01-05
+OnePlus 8 IN2013 (10):OnePlus:IN2013=OnePlus/OnePlus8_EEA/OnePlus8:10/QKQ1.191222.002/2004050031:user/release-keys__2020-03-01
+OnePlus 8 Pro IN2023 (10):OnePlus:IN2023=OnePlus/OnePlus8Pro_EEA/OnePlus8Pro:10/QKQ1.191222.002/2004111413:user/release-keys__2020-03-01
+OnePlus 8 Pro IN2025 (10):OnePlus:IN2025=OnePlus/OnePlus8Pro/OnePlus8Pro:10/QKQ1.191222.002/2004111414:user/release-keys__2020-03-01
+Sony Xperia Z5 E6653 (7.1.1):Sony:=Sony/E6653/E6653:7.1.1/32.4.A.1.54/3761073091:user/release-keys
 "
 
 # Contributors:
-# 
+# @Some_Random_Username, @Klaus N., @
