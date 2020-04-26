5007db3938339d1ac05e003b288d21e279a19fcb
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-04-26 22:24:54 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Samsung Galaxy Note 10 Plus SM-N9750 (10):Samsung:SM-N9750=samsung/d2qzh/d2q:10/QP1A.190711.020/N9750ZSU2CTD1:user/release-keys
+Samsung Galaxy Note 10 Plus SM-N9750 (10):Samsung:SM-N9750=samsung/d2qzh/d2q:10/QP1A.190711.020/N9750ZSU2CTD1:user/release-keys__2020-04-01
 Samsung Galaxy Note 10 Plus SM-N975U (10):Samsung:SM-N975U=samsung/d2qsq/d2q:10/QP1A.190711.020/N975USQU2BTA1:user/release-keys__2020-01-01
 Samsung Galaxy S20 Ultra (10)=samsung/z3sxxx/z3s:10/QP1A.190711.020/G988BXXU1ATCT:user/release-keys__2020-04-01
 Xiaomi Mi A1 (7.1.2 & 8.0.0 & 8.1.0 & 9):Xiaomi:Mi A1=xiaomi/tissot/tissot_sprout:7.1.2/N2G47H/7.12.19:user/release-keys;xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/V.9.5.10.0.ODHMIFA:user/release-keys__2018-04-01;xiaomi/tissot/tissot_sprout:8.1.0/OPM1.171019.026/V9.6.8.0.ODHMIFE:user/release-keys__2018-11-01;xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.20.0.PDHMIXM:user/release-keys__2020-04-01
