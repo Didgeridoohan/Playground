e4f4c94d4d2781a174dbac91726578ad81164c6e
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-04-29 14:39:47 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,6 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Mi 8 (10):Xiaomi:MI 8=Xiaomi/dipper/dipper:10/QKQ1.190828.002/V11.0.3.0.QEAMIXM:user/release-keys__2020-01-01
 "
 
 # Contributors:
