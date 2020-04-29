a28284c79fb0ceadaa38602d54d0eec79d71c876
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-04-29 11:58:47 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,16 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Nexus Player (6.0.1 & 7.0 & 7.1.1 & 7.1.2 & 8.0.0):Asus:Nexus Player=google/fugu/fugu:6.0.1/MOB30W/3031100:user/release-keys;google/fugu/fugu:7.0/NRD91N/3318108:user/release-keys;google/fugu/fugu:7.1.1/NMF27D/3783773:user/release-keys;google/fugu/fugu:7.1.2/N2G48C/4104010:user/release-keys;google/fugu/fugu:8.0.0/OPR2.170623.027/4397545:user/release-keys
-Motorola Moto X4 (9):motorola:moto x4:=motorola/payton_fi/payton_sprout:9/PPWS29.69-39-6-6/8c843:user/release-keys__2020-03-01
-Samsung Galaxy J2 SM-S260DL (8.1.0):samsung:SM-S260DL:=samsung/j2corepltetfntmo/j2corepltetfntmo:8.1.0/M1AJQ/S260DLUDU1ASC7:user/release-keys__2019-03-01
-Samsung Galaxy Note 10 Plus SM-N9750 (10):Samsung:SM-N9750=samsung/d2qzh/d2q:10/QP1A.190711.020/N9750ZSU2CTD1:user/release-keys__2020-04-01
-Samsung Galaxy Note 10 Plus SM-N975U (10):Samsung:SM-N975U=samsung/d2qsq/d2q:10/QP1A.190711.020/N975USQU2BTA1:user/release-keys__2020-01-01
-Samsung Galaxy S10e SM-G970N (10):Samsung:SM-G970N=samsung/beyond0lteks/beyond0:10/QP1A.190711.020/G970NKSU4CTC9:user/release-keys__2020-04-01
-Samsung Galaxy S20 Ultra SM-G988B (10):Samsung:SM-G988B=samsung/z3sxxx/z3s:10/QP1A.190711.020/G988BXXU1ATCT:user/release-keys__2020-04-01
-Sony Xperia XA2 Ultra H3223 (9):Sony:H3223:=Sony/H3223/H3223:9/50.2.A.3.22/2120505936:user/release-keys__2019-11-01
-Xiaomi Mi A1 (7.1.2 & 8.0.0 & 8.1.0 & 9):Xiaomi:Mi A1=xiaomi/tissot/tissot_sprout:7.1.2/N2G47H/7.12.19:user/release-keys;xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/V.9.5.10.0.ODHMIFA:user/release-keys__2018-04-01;xiaomi/tissot/tissot_sprout:8.1.0/OPM1.171019.026/V9.6.8.0.ODHMIFE:user/release-keys__2018-11-01;xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.20.0.PDHMIXM:user/release-keys__2020-04-01
 "
 
 # Contributors:
-# @Displax, @wunerrahdev, @ipdev
+# 
