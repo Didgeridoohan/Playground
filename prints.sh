7608fe1b275da6eb8ae752e75919d45163facfe4
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-04-29 23:13:50 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,8 +8,19 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 5 (10)=OnePlus/OnePlus5/OnePlus5:10/QKQ1.191014.012/2004201751:user/release-keys__2020-02-05
+OnePlus 5T (10)=OnePlus/OnePlus5T/OnePlus5T:10/QKQ1.191014.012/2004201752:user/release-keys__2020-02-05
+OnePlus 6 (10)=OnePlus/OnePlus6/OnePlus6:10/QKQ1.190716.003/2004142132:user/release-keys__2020-04-01
+OnePlus 6T (10)=OnePlus/OnePlus6T/OnePlus6T:10/QKQ1.190716.003/2004142132:user/release-keys__2020-04-01
+OnePlus 8 IN2013 (10)=OnePlus/OnePlus8_EEA/OnePlus8:10/QKQ1.191222.002/2004210405:user/release-keys__2020-03-01
+OnePlus 8 IN2015 (10)=OnePlus/OnePlus8/OnePlus8:10/QKQ1.191222.002/2004210405:user/release-keys__2020-03-01
+OnePlus 8 India (10)=OnePlus/OnePlus8_IND/OnePlus8:10/QKQ1.191222.002/2004210400:user/release-keys__2020-03-01
+OnePlus 8 Pro IN2023 (10)=OnePlus/OnePlus8Pro/OnePlus8Pro:10/QKQ1.191222.002/2004210418:user/release-keys__2020-03-01
+OnePlus 8 Pro IN2025 (10)=OnePlus/OnePlus8Pro_EEA/OnePlus8Pro:10/QKQ1.191222.002/2004210410:user/release-keys__2020-03-01
+OnePlus 8 Pro India (10)=OnePlus/OnePlus8Pro_IND/OnePlus8Pro:10/QKQ1.191222.002/2004210408:user/release-keys__2020-03-01
+Sony Xperia M (4.3):Sony:C1905=Sony/C1905/C1905:4.3/15.4.A.1.9/eng.user.20140509.125022:user/release-keys
 Xiaomi Mi 8 (10):Xiaomi:MI 8=Xiaomi/dipper/dipper:10/QKQ1.190828.002/V11.0.3.0.QEAMIXM:user/release-keys__2020-01-01
 "
 
 # Contributors:
-# @s3v3ru5
+# @s3v3ru5, @pawloland, @Some_Random_Username
