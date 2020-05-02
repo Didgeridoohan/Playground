4000c36cc7f2a79bc20b2ca390de7f6b18d7a243
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-05-02 09:45:53 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,9 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-HTC Exodus 1 (9)=htc/exouhl_00617/htc_exouhl:9/PQ2A.190205.003/1109191.2:user/release-keys__2020-03-01
-Sony Xperia XZ1 Compact (9)=Sony/G8441/G8441:9/47.2.A.11.228/3311891731:user/release-keys__2019-09-01
 "
 
 # Contributors:
-# @jeanrivera, @russel5
+# 
