b2e532c8a8e3e984b62f8c3e0d1561edc55537c9
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-05-06 11:55:13 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,15 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Google Pixel 2 (10):Google:Pixel 2:=google/walleye/walleye:10/QQ2A.200501.001.B3/6396602:user/release-keys__2020-05-05
+Google Pixel 2 XL (10):Google:Pixel 2 XL:=google/taimen/taimen:10/QQ2A.200501.001.B3/6396602:user/release-keys__2020-05-05
+Google Pixel 3 (10):Google:Pixel 3:=google/blueline/blueline:10/QQ2A.200501.001.B2/6352890:user/release-keys__2020-05-05
+Google Pixel 3 XL (10):Google:Pixel 3 XL:=google/crosshatch/crosshatch:10/QQ2A.200501.001.B2/6352890:user/release-keys__2020-05-05
+Google Pixel 3a (10):Google:Pixel 3a:=google/sargo/sargo:10/QQ2A.200501.001.B2/6352890:user/release-keys__2020-05-05
+Google Pixel 3a XL (10):Google:Pixel 3a XL:=google/bonito/bonito:10/QQ2A.200501.001.B2/6352890:user/release-keys__2020-05-05
+Google Pixel 4 (10):Google:Pixel 4:=google/flame/flame:10/QQ2A.200501.001.B2/6352890:user/release-keys__2020-05-05
+Google Pixel 4 XL (10):Google:Pixel 4 XL:=google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys__2020-05-05
 "
 
 # Contributors:
-# 
+# @ipdev
