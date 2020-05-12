ad5e3334b75cf72c88322b8509520e5e4c82e2f3
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-05-12 20:18:53 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,10 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+POCO F2 Pro (10):POCO:POCO F2 Pro=POCO/lmi_eea/lmi:10/QKQ1.191117.002/V11.0.6.0.QJKEUXM:user/release-keys__2020-04-01
+Redmi K30 Pro Zoom Edition (10):Redmi:Redmi K30 Pro Zoom Edition=Redmi/lmipro/lmipro:10/QKQ1.191117.002/V11.0.16.0.QJKCNXM:user/release-keys__2020-04-01
+Xiaomi Mi A3 (10)=Xiaomi/laurel_sprout/laurel_sprout:10/QKQ1.190910.002/V11.0.15.0.QFQMIXM:user/release-keys__2020-05-05
 "
 
 # Contributors:
-# 
+# RealAkito @ Github, @Displax
