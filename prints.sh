dac7252f614e1a1f09c34b0bec199140c9b1de16
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-05-18 08:16:32 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,6 +8,11 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Redmi 6 (9):Xiaomi:Redmi 6:=xiaomi/cereus/cereus:9/PPR1.180610.011/V11.0.3.0.PCGMIXM:user/release-keys__2020-03-01
+Xiaomi Redmi 6A (9):Xiaomi:Redmi 6A:=xiaomi/cactus/cactus:9/PPR1.180610.011/V11.0.7.0.PCBMIXM:user/release-keys__2020-03-01
+Xiaomi Redmi 7 (9):Xiaomi:Redmi 7:=xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.5.0.PFLMIXM:user/release-keys__2020-03-01
+Xiaomi Redmi Note 7 (9):Xiaomi:Redmi Note 7:=xiaomi/lavender/lavender:9/PKQ1.180904.001/V11.0.5.0.PFGMIXM:user/release-keys__2020-01-05
+Xiaomi Redmi Note 9S (10):Xiaomi:Redmi Note 9S:=Redmi/curtana_global/curtana:10/QKQ1.191215.002/V11.0.7.0.QJWMIXM:user/release-keys__2020-04-01
 "
 
 # Contributors:
