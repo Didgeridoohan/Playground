4c10797d0b0ab7c4d1ac9b0013a7051d0c1b2825
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-05-20 21:21:18 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,6 +8,9 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 5 (10)=OnePlus/OnePlus5/OnePlus5:10/QKQ1.191014.012/2005112104:user/release-keys__2020-04-05
+OnePlus 5T (10)=OnePlus/OnePlus5T/OnePlus5T:10/QKQ1.191014.012/2005112111:user/release-keys__2020-04-05
+OnePlus 7 Pro Open Beta 14 (10)=OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/2005122138:user/release-keys__2020-05-01
 "
 
 # Contributors:
