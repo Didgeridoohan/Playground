6f1ff076bf34f3f9e5f48f2ae19b05a555c41e7c
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-05-20 12:53:31 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,15 +8,6 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-OnePlus 5 (10)=OnePlus/OnePlus5/OnePlus5:10/QKQ1.191014.012/2004231127:user/release-keys__2020-04-05
-OnePlus 5T (10)=OnePlus/OnePlus5T/OnePlus5T:10/QKQ1.191014.012/2004231356:user/release-keys__2020-04-05
-OnePlus 8 IN2011 (10)=OnePlus/OnePlus8_IND/OnePlus8:10/QKQ1.191222.002/2005090021:user/release-keys__2020-04-01
-OnePlus 8 IN2013 (10)=OnePlus/OnePlus8_EEA/OnePlus8:10/QKQ1.191222.002/2005090021:user/release-keys__2020-04-01
-OnePlus 8 IN2015 (10)=OnePlus/OnePlus8/OnePlus8:10/QKQ1.191222.002/2005090021:user/release-keys__2020-04-01
-OnePlus 8 Pro IN2021 (10)=OnePlus/OnePlus8Pro_IND/OnePlus8Pro:10/QKQ1.191222.002/2005100226:user/release-keys__2020-04-01
-OnePlus 8 Pro IN2023 (10)=OnePlus/OnePlus8Pro_EEA/OnePlus8Pro:10/QKQ1.191222.002/2005100226:user/release-keys__2020-04-01
-OnePlus 8 Pro IN2025 (10)=OnePlus/OnePlus8Pro/OnePlus8Pro:10/QKQ1.191222.002/2005100226:user/release-keys__2020-04-01
-Xiaomi Mi A1 (9)=xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.21.0.PDHMIXM:user/release-keys__2020-05-01
 "
 
 # Contributors:
