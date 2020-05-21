33be2d6a164e789a2a40f098ebf4be310527e4b8
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-05-21 20:41:27 +0200

    testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Redmi K30 Pro (10)=Redmi/lmi/lmi:10/QKQ1.191117.002/V11.0.19.0.QJKCNXM:user/release-keys__2020-04-01
 "
 
 # Contributors:
-# RealAkito @ Github, @Displax, @ipdev, @Some_Random_Username, @ayesse
+# RealAkito @ Github, @Displax, @ipdev, @Some_Random_Username, @ayesse, @Filo1995
