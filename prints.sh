a028f15210c4296b61bc4cf15cab7d42d03b71b1
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-05-22 17:15:28 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,9 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 7 Pro NR GM1920 (10)=OnePlus/OnePlus7ProNR_EEA/OnePlus7ProNR:10/QKQ1.190716.003/2005102305:user/release-keys__2020-05-01
+Xiaomi PocoPhone F1 (10)=Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.8.0.QEJMIXM:user/release-keys__2020-04-01
 "
 
 # Contributors:
-# 
+# @Sid220, @Some_Random_Username
