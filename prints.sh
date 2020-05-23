9fbb99c2802a802a38ada48e3c393b06acf39c81
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-05-23 07:15:22 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -11,7 +11,8 @@ PRINTSLIST="
 OnePlus 7 Pro NR GM1920 (10)=OnePlus/OnePlus7ProNR_EEA/OnePlus7ProNR:10/QKQ1.190716.003/2005102305:user/release-keys__2020-05-01
 Samsung Galaxy A5 2017 (8.0.0)=samsung/a5y17ltexx/a5y17lte:8.0.0/R16NW/A520FXXUECTDA:user/release-keys__2020-05-01
 Xiaomi PocoPhone F1 (10)=Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.8.0.QEJMIXM:user/release-keys__2020-04-01
+Xiaomi Redmi Note 8 (9)=xiaomi/ginkgo/ginkgo:9/PKQ1.190616.001/V11.0.8.0.PCOMIXM:user/release-keys__2020-03-01
 "
 
 # Contributors:
-# @Sid220, @Some_Random_Username, @swour
+# @Sid220, @Some_Random_Username, @swour, @amipel
