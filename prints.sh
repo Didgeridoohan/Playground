1667234b5cc216691273326f9c891a8599e3ae4f
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-05-25 22:42:43 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,6 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Motorola Moto G7 Power (9)=motorola/ocean/ocean-9/PPOS29.114-134-13/6abfe:user/release-keys__2020-04-01
 "
 
 # Contributors:
