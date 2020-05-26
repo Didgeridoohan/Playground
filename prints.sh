d5be3c8cfd49358376baf0f5d96b9133221e39bd
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-05-26 21:12:15 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,9 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Motorola Moto G7 Power (9)=motorola/ocean/ocean:9/PPOS29.114-134-13/6abfe:user/release-keys__2020-04-01
-Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.5.0.QDLMIXM:user/release-keys__2020-05-01
 "
 
 # Contributors:
-# @Sid220, @Some_Random_Username, @swour, @amipel
+# @Sid220, @Some_Random_Username, @swour, @amipel, @estofjlw, @Displax
