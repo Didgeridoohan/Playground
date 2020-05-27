b585e8437815033ff4b27e4b83c81d24ab52e9c3
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-05-27 08:11:40 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Lenovo Tab 4 10 Plus (7.1.1):Lenovo:Lenovo TB-X704L=Lenovo/LenovoTB-X704L/X704L:7.1.1/NMF26F/TB-X704L_S000056_181015_ROW:user/release-keys__2018-10-01
 "
 
 # Contributors:
-# @Sid220, @Some_Random_Username, @swour, @amipel, @estofjlw, @Displax
+# @Sid220, @Some_Random_Username, @swour, @amipel, @estofjlw, @Displax, @kontini
