da38749a1e0d553807b3e66dcdb6ba50159e8e23
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-05-28 22:46:11 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,9 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Lenovo Tab 4 10 Plus (7.1.1):Lenovo:Lenovo TB-X704F=Lenovo/LenovoTB-X704F/X704F:7.1.1/NMF26F/TB-X704F_S000056_181015_ROW:user/release-keys__2018-10-01
 Lenovo Tab 4 10 Plus (7.1.1):Lenovo:Lenovo TB-X704L=Lenovo/LenovoTB-X704L/X704L:7.1.1/NMF26F/TB-X704L_S000056_181015_ROW:user/release-keys__2018-10-01
+Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.10.0.QDIMIXM:user/release-keys__2020-05-05
 "
 
 # Contributors:
