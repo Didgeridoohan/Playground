fbb8ce604c6f6c5c51c078d967549ab0cc1cadc6
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-06-01 10:20:17 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,6 +8,18 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 5=OnePlus/OnePlus5/OnePlus5:10/QKQ1.191014.012/2005122320:user/release-keys__2020-04-05
+OnePlus 5T=OnePlus/OnePlus5T/OnePlus5T:10/QKQ1.191014.012/2005130017:user/release-keys__2020-04-05
+OnePlus 6=OnePlus/OnePlus6/OnePlus6:10/QKQ1.190716.003/2005052051:user/release-keys__2020-05-01
+OnePlus 6T=OnePlus/OnePlus6T/OnePlus6T:10/QKQ1.190716.003/2005052051:user/release-keys__2020-05-01
+OnePlus 7 GM1901 and GM1905=OnePlus/OnePlus7/OnePlus7:10/QKQ1.190716.003/2005132200:user/release-keys__2020-05-01
+OnePlus 7 Pro GM1911 and GM1917=OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/2005132200:user/release-keys__2020-05-01
+OnePlus 7 Pro NR GM1920=OnePlus/OnePlus7ProNR_EEA/OnePlus7ProNR:10/QKQ1.190716.003/2005102305:user/release-keys__2020-05-01
+OnePlus 7T HD1901 and HD1905=OnePlus/OnePlus7T/OnePlus7T:10/QKQ1.190716.003/2005192100:user/release-keys__2020-05-01
+OnePlus 7T Pro HD1911 and HD1917=OnePlus/OnePlus7TPro/OnePlus7TPro:10/QKQ1.190716.003/2005192100:user/release-keys__2020-05-01
+OnePlus 7T Pro NR HD1925=OnePlus/OnePlus7TProNR/OnePlus7TProNR:10/QKQ1.190716.003/2005062342:user/release-keys__2020-04-01
+OnePlus 8 Pro IN2021=OnePlus/OnePlus8Pro/OnePlus8Pro:10/QKQ1.191222.002/2005211243:user/release-keys__2020-04-01
+OnePlus 8 Pro IN2025=OnePlus/OnePlus8Pro_IND/OnePlus8Pro:10/QKQ1.191222.002/2005190021:user/release-keys__2020-04-01
 Poco X2 (10)=POCO/phoenixin/phoenixin:10/QKQ1.190825.002/V11.0.10.0.QGHINXM:user/release-keys__2020-04-01
 "
 
