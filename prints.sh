8f3ad803f1516f1799fb765b2f807143a6913ecf
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-06-01 19:53:35 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,6 +8,14 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Pixel 2=google/walleye/walleye:10/QQ3A.200605.001/6392402:user/release-keys__2020-06-05
+Pixel 2 XL=google/taimen/taimen:10/QQ3A.200605.001/6392402:user/release-keys__2020-06-05
+Pixel 3=google/blueline/blueline:10/QQ3A.200605.001/6392402:user/release-keys__2020-06-05
+Pixel 3 XL=google/crosshatch/crosshatch:10/QQ3A.200605.001/6392402:user/release-keys__2020-06-05
+Pixel 3a=google/sargo/sargo:10/QQ3A.200605.002/6416773:user/release-keys__2020-06-05
+Pixel 3a XL=google/bonito/bonito:10/QQ3A.200605.002/6416773:user/release-keys__2020-06-05
+Pixel 4=google/flame/flame:10/QQ3A.200605.001/6392402:user/release-keys__2020-06-05
+Pixel 4 XL=google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys__2020-06-05
 "
 
 # Contributors:
