88593c85675a82f6e4861177922393e541200360
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-06-09 23:01:49 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Redmi Note 8 Pro (10)=Redmi/begonia/begonia:10/QP1A.190711.020/20.6.4:user/release-keys__2020-05-01
 "
 
 # Contributors:
-# @Sid220, @Some_Random_Username, @swour, @amipel, @estofjlw, @Displax, @kontini, @73sydney, @Filo1995
+# @Sid220, @Some_Random_Username, @swour, @amipel, @estofjlw, @Displax, @kontini, @73sydney, @Filo1995, @dmonium
