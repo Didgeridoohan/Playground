e97d3750d8093b21dad77418e76760ef55b156ab
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-06-11 21:08:30 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,12 +8,6 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-OnePlus 8 IN2011=OnePlus/OnePlus8_IND/OnePlus8:10/QKQ1.191222.002/2005301331:user/release-keys__2020-05-01
-OnePlus 8 IN2013=OnePlus/OnePlus8_EEA/OnePlus8:10/QKQ1.191222.002/2005301331:user/release-keys__2020-05-01
-OnePlus 8 IN2015=OnePlus/OnePlus8/OnePlus8:10/QKQ1.191222.002/2005301331:user/release-keys__2020-05-01
-OnePlus 8 Pro IN2021=OnePlus/OnePlus8Pro_IND/OnePlus8Pro:10/QKQ1.191222.002/2005301332:user/release-keys__2020-05-01
-OnePlus 8 Pro IN2023=OnePlus/OnePlus8Pro_EEA/OnePlus8Pro:10/QKQ1.191222.002/2005301332:user/release-keys__2020-05-01
-OnePlus 8 Pro IN2025=OnePlus/OnePlus8Pro/OnePlus8Pro:10/QKQ1.191222.002/2005301332:user/release-keys__2020-05-01
 "
 
 # Contributors:
