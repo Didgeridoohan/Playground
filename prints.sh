96b8bceaa83b9869ac7cafd7a6c6bade9d49dfcc
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-06-12 21:06:02 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,10 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+LG K20 Plus LGMP260 (7.0)=MetroPCS/lv517_mpcs_us/lv517:7.0/NRD90U/1833409570571:user/release-keys
+LG K20 Plus LGTP260 (7.0)=lge/lv517_tmo_us/lv517:7.0/NRD90U/1809315248edb:user/release-keys
+Redmi Note 8 Pro (10)=Redmi/begonia/begonia:10/QP1A.190711.020/20.6.11:user/release-keys__2020-06-01
 "
 
 # Contributors:
-# 
+# @mario0318, @dmonium
