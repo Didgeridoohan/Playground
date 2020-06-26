f55a2bbeed466e7d4487482487e258e79a2586a2
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-06-26 19:06:17 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,10 +8,12 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 8 IN2017 (10)=OnePlus/OnePlus8TMO/OnePlus8TMO:10/QKQ1.191222.002/2005062340:user/release-keys__2020-04-01
+OnePlus 8 IN2019 (10)=OnePlus/OnePlus8VZW/OnePlus8VZW:10/QKQ1.191222.002/2004072220:user/release-keys__2020-02-01
 Xiaomi Mi 9 Lite (10)=Xiaomi/pyxis/pyxis:10/QKQ1.190828.002/V11.0.1.0.QFCMIXM:user/release-keys__2020-02-02
 Xiaomi Mi A1 (9)=xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.22.0.PDHMIXM:user/release-keys__2020-06-01
 Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.7.0.QDLMIXM:user/release-keys__2020-06-01
 "
 
 # Contributors:
-# @Displax
+# @Displax, @Some_Random_Username
