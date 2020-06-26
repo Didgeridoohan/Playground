cf7049a260cfbb08ce4d64496f91342ab9a9475f
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-06-26 15:42:27 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,10 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Mi 9 Lite (10)=Xiaomi/pyxis/pyxis:10/QKQ1.190828.002/V11.0.1.0.QFCMIXM:user/release-keys__2020-02-02
+Xiaomi Mi A1 (9)=xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.22.0.PDHMIXM:user/release-keys__2020-06-01
+Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.7.0.QDLMIXM:user/release-keys__2020-06-01
 "
 
 # Contributors:
-# 
+# @Displax
