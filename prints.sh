2f04ddca6e759298d30b1d01c4af7564a6576787
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-07-07 22:03:50 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,25 +8,24 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Pixel 2=google/walleye/walleye:10/QQ3A.200705.002/6506677:user/release-keys__2020-07-05
-Google Pixel 2 XL=google/taimen/taimen:10/QQ3A.200705.002/6506677:user/release-keys__2020-07-05
-Google Pixel 3=google/blueline/blueline:10/QQ3A.200705.002/6506677:user/release-keys__2020-07-05
-Google Pixel 3 XL=google/crosshatch/crosshatch:10/QQ3A.200705.002/6506677:user/release-keys__2020-07-05
-Google Pixel 3a=google/sargo/sargo:10/QQ3A.200705.002/6506677:user/release-keys__2020-07-05
-Google Pixel 3a XL=google/bonito/bonito:10/QQ3A.200705.002/6506677:user/release-keys__2020-07-05
-Google Pixel 4=google/flame/flame:10/QQ3A.200705.002/6506677:user/release-keys__2020-07-05
-Google Pixel 4 XL=google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys__2020-07-05
-Huawei P20 EML-L09 (10)=HUAWEI/EML-L09/HWEML:10/HUAWEIEML-L09/10.0.0.161C109:user/release-keys__2020-04-01
-OnePlus 8 IN2017 (10)=OnePlus/OnePlus8TMO/OnePlus8TMO:10/QKQ1.191222.002/2005062340:user/release-keys__2020-04-01
+Google Pixel 2=
+Google Pixel 2 XL=
+Google Pixel 3=
+Google Pixel 3 XL=
+Google Pixel 3a=
+Google Pixel 3a XL=
+Google Pixel 4=
+Google Pixel 4 XL=
+Huawei P20 EML-L09 (10)=
+OnePlus 8 IN2017 (10)=
 OnePlus 8 IN2019 (10)=OnePlus/OnePlus8VZW/OnePlus8VZW:10/QKQ1.191222.002/2004072220:user/release-keys__2020-02-01
-Redmi K30 Pro (10)=Redmi/lmi/lmi:10/QKQ1.191117.002/V12.0.1.0.QJKCNXM:user/release-keys__2020-06-01
+Redmi K30 Pro (10)=
 Samsung Galaxy A90 5G SM-A908B (9):samsung:SM-A908B=samsung/r3qxx/r3q:9/PPR1.180610.011/A908BXXU2ASK5:user/release-keys__2019-11-01
-Samsung Galaxy Tab A SM-T295 (9)=samsung/gtoxx/gto:9/PPR1.180610.011/T295XXS3ATB2:user/release-keys_2020-03-01
-Xiaomi Mi 9 Lite (10)=Xiaomi/pyxis/pyxis:10/QKQ1.190828.002/V11.0.1.0.QFCMIXM:user/release-keys__2020-02-02
-Xiaomi Mi A1 (9)=xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.22.0.PDHMIXM:user/release-keys__2020-06-01
-Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.11.0.QDIMIXM:user/release-keys__2020-06-01
-Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.7.0.QDLMIXM:user/release-keys__2020-06-01
-Xiaomi Mi A3 (10)=Xiaomi/laurel_sprout/laurel_sprout:10/QKQ1.190910.002/V11.0.16.0.QFQMIXM:user/release-keys__2020-06-05
+Samsung Galaxy Tab A SM-T295 (9)=samsung/gtoxx/gto:9/PPR1.180610.011/T295XXS3ATB2:user/release-keys__2020-03-01
+Xiaomi Mi A1 (9)=
+Xiaomi Mi A2 (10)=
+Xiaomi Mi A2 Lite (10)=
+Xiaomi Mi A3 (10)=
 "
 
 # Contributors:
