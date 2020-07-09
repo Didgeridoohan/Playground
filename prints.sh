161bcb5c038a82e8919758d3d081eafc5f8c4f92
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-07-09 19:32:52 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,8 +8,9 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Poco X2 (10)=POCO/phoenixin/phoenixin:10/QKQ1.190825.002/V11.0.11.0.QGHINXM:user/release-keys__2020-06-01
 Realme X2 Pro (10):realme:RMX1931=realme/RMX1931EEA/RMX1931L1:10/QKQ1.191021.002/1590460856:user/release-keys__2020-06-05
 "
 
 # Contributors:
-# @Displax, @Some_Random_Username, @Filo1995, @rdis, @ipdev, @LBA97
+# @Displax, @Some_Random_Username, @Filo1995, @rdis, @ipdev, @LBA97, @GtrCraft
