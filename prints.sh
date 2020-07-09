b5aa7580bf98b620fb661f725407c3739e44db99
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-07-09 11:58:19 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Realme X2 Pro ():realme:RMX1931=realme/RMX1931EEA/RMX1931L1:10/QKQ1.191021.002/1590460856:user/release-keys__2020-06-05
+Realme X2 Pro (10):realme:RMX1931=realme/RMX1931EEA/RMX1931L1:10/QKQ1.191021.002/1590460856:user/release-keys__2020-06-05
 "
 
 # Contributors:
