d9e1168c1cc5e50bc18ba426ecc326ddcd5c3ebb
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-07-09 11:52:12 +0200

    testing

--- prints.sh
+++ prints.sh
@@ -8,25 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Pixel 2=
-Google Pixel 2 XL=
-Google Pixel 3=
-Google Pixel 3 XL=
-Google Pixel 3a=
-Google Pixel 3a XL=
-Google Pixel 4=
-Google Pixel 4 XL=
-Huawei P20 EML-L09 (10)=
-OnePlus 8 IN2017 (10)=
-OnePlus 8 IN2019 (10)=OnePlus/OnePlus8VZW/OnePlus8VZW:10/QKQ1.191222.002/2004072220:user/release-keys__2020-02-01
-Redmi K30 Pro (10)=
-Samsung Galaxy A90 5G SM-A908B (9):samsung:SM-A908B=samsung/r3qxx/r3q:9/PPR1.180610.011/A908BXXU2ASK5:user/release-keys__2019-11-01
-Samsung Galaxy Tab A SM-T295 (9)=samsung/gtoxx/gto:9/PPR1.180610.011/T295XXS3ATB2:user/release-keys__2020-03-01
-Xiaomi Mi A1 (9)=
-Xiaomi Mi A2 (10)=
-Xiaomi Mi A2 Lite (10)=
-Xiaomi Mi A3 (10)=
+Realme X2 Pro ():realme:RMX1931=realme/RMX1931EEA/RMX1931L1:10/QKQ1.191021.002/1590460856:user/release-keys__2020-06-05
 "
 
 # Contributors:
-# @Displax, @Some_Random_Username, @Filo1995, @rdis, @ipdev
+# @Displax, @Some_Random_Username, @Filo1995, @rdis, @ipdev, @LBA97
