3d42112e02a299bd975e0e40be761eb1bf45f04b
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-07-12 13:01:43 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Redmi 8 (10)=Xiaomi/olive/olive:10/QKQ1.191014.001/V11.0.1.0.QCNCNXM:user/release-keys__2020-06-01
 "
 
 # Contributors:
-# 
+# @xuxu1
