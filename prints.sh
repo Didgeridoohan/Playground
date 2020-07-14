ba128c6530a835fad84625b2633a1175070395b8
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-07-14 20:07:56 +0200

    testing

--- prints.sh
+++ prints.sh
@@ -8,8 +8,15 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 8 IN2011 (10)=OnePlus/OnePlus8_IND/OnePlus8:10/QKQ1.191222.002/2006300108:user/release-keys__2020-06-01
+OnePlus 8 IN2013 (10)=OnePlus/OnePlus8_EEA/OnePlus8:10/QKQ1.191222.002/2006300106:user/release-keys__2020-06-01
+OnePlus 8 IN2015 (10)=OnePlus/OnePlus8/OnePlus8:10/QKQ1.191222.002/2006300109:user/release-keys__2020-06-01
+OnePlus 8 Pro IN2021 (10)=OnePlus/OnePlus8Pro_IND/OnePlus8Pro:10/QKQ1.191222.002/2007021926:user/release-keys__2020-06-01
+OnePlus 8 Pro IN2023 (10)=OnePlus/OnePlus8Pro_EEA/OnePlus8Pro:10/QKQ1.191222.002/2007021926:user/release-keys__2020-06-01
+OnePlus 8 Pro IN2025 (10)=OnePlus/OnePlus8Pro/OnePlus8Pro:10/QKQ1.191222.002/2007021926:user/release-keys__2020-06-01
+Xiaomi Redmi 6 Pro (9):Xiaomi:Redmi 6 Pro=xiaomi/sakura/sakura:9/PKQ1.180917.001/20.1.9:user/release-keys__2019-12-01
 Xiaomi Redmi 8 (10)=Xiaomi/olive/olive:10/QKQ1.191014.001/V11.0.1.0.QCNCNXM:user/release-keys__2020-06-01
 "
 
 # Contributors:
-# @xuxu1
+# @xuxu1, @Some_Random_Usernam, @Qi Xuan
