141fa367cd1c32a08ece7d2fff1d87653dfad514
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-07-18 20:06:10 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,15 +8,21 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Honor 6X BLN-L22 (8.0.0)=HONOR/BLN-L22/HWBLN-H:8.0.0/HONORBLN-L22/520(C675):user/release-keys__2018-11-05
+OnePlus 7 Pro NR Spr GM1925 (10)=OnePlus/OP7ProNRSpr/OP7ProNRSpr:10/QKQ1.190716.003/2006051914:user/release-keys__2020-06-01
 OnePlus 8 IN2011 (10)=OnePlus/OnePlus8_IND/OnePlus8:10/QKQ1.191222.002/2006300108:user/release-keys__2020-06-01
 OnePlus 8 IN2013 (10)=OnePlus/OnePlus8_EEA/OnePlus8:10/QKQ1.191222.002/2006300106:user/release-keys__2020-06-01
 OnePlus 8 IN2015 (10)=OnePlus/OnePlus8/OnePlus8:10/QKQ1.191222.002/2006300109:user/release-keys__2020-06-01
+OnePlus 8 IN2017 (10)=OnePlus/OnePlus8TMO/OnePlus8TMO:10/QKQ1.191222.002/2006102149:user/release-keys__2020-06-01
 OnePlus 8 Pro IN2021 (10)=OnePlus/OnePlus8Pro_IND/OnePlus8Pro:10/QKQ1.191222.002/2007021926:user/release-keys__2020-06-01
 OnePlus 8 Pro IN2023 (10)=OnePlus/OnePlus8Pro_EEA/OnePlus8Pro:10/QKQ1.191222.002/2007021926:user/release-keys__2020-06-01
 OnePlus 8 Pro IN2025 (10)=OnePlus/OnePlus8Pro/OnePlus8Pro:10/QKQ1.191222.002/2007021926:user/release-keys__2020-06-01
+Xiaomi Mi 9T Pro/K20 Pro India (10)=Xiaomi/raphaelin/raphaelin:10/QKQ1.190825.002/V12.0.2.0.QFKINXM:user/release-keys__2020-06-01
+Xiaomi Mi A1 (9)=xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.23.0.PDHMIXM:user/release-keys__2020-07-01
+Xiaomi Mi A3 (10)=Xiaomi/laurel_sprout/laurel_sprout:10/QKQ1.190910.002/V11.0.17.0.QFQMIXM:user/release-keys__2020-06-05
 Xiaomi Redmi 6 Pro (9):Xiaomi:Redmi 6 Pro=xiaomi/sakura/sakura:9/PKQ1.180917.001/20.1.9:user/release-keys__2019-12-01
 Xiaomi Redmi 8 (10)=Xiaomi/olive/olive:10/QKQ1.191014.001/V11.0.1.0.QCNCNXM:user/release-keys__2020-06-01
 "
 
 # Contributors:
-# @xuxu1, @Some_Random_Usernam, @Qi Xuan
+# @xuxu1, @Some_Random_Usernam, @Qi Xuan, @Displax, @mohitanchalia, @amrithaathmaram
