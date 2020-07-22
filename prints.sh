56d2a2eeec4c3601308c1763d605cf0b53c47b2d
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-07-22 13:41:20 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -14,6 +14,7 @@ OnePlus 7T HD1900 (10):OnePlus:HD1900=OnePlus/OnePlus7T_CH/OnePlus7T:10/QKQ1.190
 OnePlus 7 Pro HD1910 (10):OnePlus:HD1910=OnePlus/OnePlus7TPro_CH/OnePlus7TPro:10/QKQ1.190716.003/2005131900:user/release-keys__2020-05-01
 OnePlus 7 Pro NR GM1920 (10)=OnePlus/OnePlus7ProNR_EEA/OnePlus7ProNR:10/QKQ1.190716.003/2007081356:user/release-keys__2020-07-01
 OnePlus 8 IN2010 (10):OnePlus:IN2010=OnePlus/OnePlus8_CH/OnePlus8:10/QKQ1.191222.002/2006300106:user/release-keys/release-keys__2020-06-01
+OnePlus 8 2 (10)=OnePlus/OnePlus8_CH/OnePlus8:10/QKQ1.191222.002/2006300106:user/release-keys__2020-06-01
 OnePlus 8 Pro IN2020 (10):OnePlus:IN2020=OnePlus/OnePlus8Pro_CH/OnePlus8Pro:10/QKQ1.191222.002/2007021926:user/release-keys__2020-06-01
 "
 
