1f3b54e137859e32ff05588e3477635708ec355b
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-07-24 21:05:04 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,15 +8,6 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-OnePlus 7 GM1900 (10):OnePlus:GM1900=OnePlus/OnePlus7_CH/OnePlus7:10/QKQ1.190716.003/2005132200:user/release-keys__2020-05-01
-OnePlus 7 Pro GM1910 (10):OnePlus:GM1910=OnePlus/OnePlus7Pro_CH/OnePlus7Pro:10/QKQ1.190716.003/2005132200:user/release-keys__2020-05-01
-OnePlus 7T HD1900 (10):OnePlus:HD1900=OnePlus/OnePlus7T_CH/OnePlus7T:10/QKQ1.190716.003/2005131900:user/release-keys__2020-05-01
-OnePlus 7 Pro HD1910 (10):OnePlus:HD1910=OnePlus/OnePlus7TPro_CH/OnePlus7TPro:10/QKQ1.190716.003/2005131900:user/release-keys__2020-05-01
-OnePlus 7 Pro NR GM1920 (10)=OnePlus/OnePlus7ProNR_EEA/OnePlus7ProNR:10/QKQ1.190716.003/2007081356:user/release-keys__2020-07-01
-OnePlus 8 IN2010 (10):OnePlus:IN2010=OnePlus/OnePlus8_CH/OnePlus8:10/QKQ1.191222.002/2006300106:user/release-keys__2020-06-01
-OnePlus 8 Pro IN2020 (10):OnePlus:IN2020=OnePlus/OnePlus8Pro_CH/OnePlus8Pro:10/QKQ1.191222.002/2007021926:user/release-keys__2020-06-01
-Redmi Note 8 Pro (9):Redmi:Note 8 Pro=Redmi/begonia_ru/begonia:9/PPR1.180610.011/V11.0.3.0.PGGRUXM:user/release-keys__2019-11-01
-Xiaomi Mi A3 (10):Xiaomi:Mi A3=Xiaomi/laurel_sprout/laurel_sprout:10/QKQ1.190910.002/V11.0.19.0.QFQMIXM:user/release-keys__2020-07-05
 "
 
 # Contributors:
