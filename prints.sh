d77c9c709545cf07076d6e2381995596c8a58b52
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-07-26 03:32:38 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,15 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Asus ZenFone Max Pro M2 (9):asus:ASUS_X01BD=asus/WW_X01BD/ASUS_X01BD_1:9/PKQ1/16.2017.2007.094-20200705:user/release-keys__2020-07-05
+LG V30 LG-LS998 (8.0.0):LGE:LG-LS998=lge/joan_spr_us/joan:8.0.0/OPR1.170623.026/190181203d401:user/release-keys__2019-01-01
+Samsung Galaxy A3 2016 SM-A310F (7.0):samsung:SM-A310F=samsung/a3xeltexx/a3xelte:7.0/NRD90M/A310FXXU4CSB2:user/release-keys__2019-02-01
+Samsung Galaxy A3 2017 SM-A320FL (8.0.0):samsung:SM-A320FL=samsung/a3y17ltexc/a3y17lte:8.0.0/R16NW/A320FLXXU4CSD2:user/release-keys__2019-04-01
+Samsung Galaxy S10 SM-G973F (9):samsung:SM-G973F=samsung/beyond1ltexx/beyond1:9/PPR1.180610.011/FA90_G973FXXU1ASB2:user/release-keys__2019-01-01
+Samsung Galaxy Tab4 7.0 SM-T230NU (4.4.2):samsung:SM-T230NU=samsung/degaswifiue/degaswifiue:4.4.2/KOT49H/T230NUUEU0APA1:user/release-keys
+Samsung Galaxy Tab4 8.0 SM-T330NU (5.1.1):samsung:SM-T330NU=samsung/milletwifiue/milletwifiue:5.1.1/LMY47X/T330NUUES1BPL1:user/release-keys
+Samsung Galaxy Tab4 10.1 SM-T530NU (5.0.2):samsung:SM-T530NU=samsung/matissewifiue/matissewifiue:5.0.2/LRX22G/T530NUUEU1BQC1:user/release-keys
 "
 
 # Contributors:
-# @xuxu1, @Some_Random_Usernam, @Displax, @mohitanchalia, @amrithaathmaram, @mikko3024, @DreamFlasher, @timjosten
+# @xuxu1, @Some_Random_Usernam, @Displax, @mohitanchalia, @amrithaathmaram, @mikko3024, @DreamFlasher, @timjosten, @ipdev
