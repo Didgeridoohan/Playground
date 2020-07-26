fccceda765eeea160322beb9731cf8b3602a1f93
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-07-26 19:21:25 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Redmi Note 9 Pro (10):Xiaomi:Redmi Note 9 Pro=Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V11.0.4.0.QJZMIXM:user/release-keys__2020-06-01
 "
 
 # Contributors:
-# @xuxu1, @Some_Random_Usernam, @Displax, @mohitanchalia, @amrithaathmaram, @mikko3024, @DreamFlasher, @timjosten, @ipdev
+# @xuxu1, @Some_Random_Usernam, @Displax, @mohitanchalia, @amrithaathmaram, @mikko3024, @DreamFlasher, @timjosten, @ipdev, @rafaelbrunner
