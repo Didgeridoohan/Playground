61ce9181e5c1a764b27d150a54a2fb28b00f00dc
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-07-29 17:30:57 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -11,6 +11,7 @@ PRINTSLIST="
 Xiaomi Mi 10 Lite 5G (10):Xiaomi:M2002J9G=Xiaomi/monet_eea/monet:10/QKQ1.191222.002/V12.0.1.0.QJIEUXM:user/release-keys__2020-06-01
 Xiaomi Pocophone F1 (10)=Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.9.0.QEJMIXM:user/release-keys__2020-06-01
 Xiaomi Redmi 6 Pro (9):Xiaomi:Redmi 6 Pro=xiaomi/sakura/sakura:9/PKQ1.180917.001/20.1.9:user/release-keys__2019-12-01
+Xiaomi Redmi 6 Pro=xiaomi/sakura/sakura:9/PKQ1.180917.001/20.1.9:user/release-keys__2019-12-01
 "
 
 # Contributors:
