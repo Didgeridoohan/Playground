fafe5e7af8c7e02b110bd2de2146c0e72c21b15d
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-03 22:53:03 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -11,9 +11,17 @@ PRINTSLIST="
 Asus ZenFone Max M1 (ZB556KL) / ZenFone Max M1/M2/M3 (ZB555KL) (9):asus:ASUS_X00PD:=asus/WW_X00PD/ASUS_X00P_1:9/PPR1.180610.009/16.00.1906.411:user/release-keys__2019-06-05
 Asus ZenFone Max M1 (ZB556KL) / ZenFone Max M1/M2/M3 (ZB555KL) (10):asus:ASUS_X00PD:=asus/WW_X00PD/ASUS_X00P_1:10/QKQ1.191002.002/17.00.2003.23-20200401:user/release-keys__2020-03-05
 Asus ZenFone Max M1 (ZB556KL) / ZenFone Max M1/M2/M3 (ZB555KL) (8.0.0):asus:ASUS_X00PD:=asus/WW_X00PD/ASUS_X00P_1:8.0.0/OPR1.170623.032/15.00.2007.122:user/release-keys__2020-07-05
+Google Pixel 2 (10)=google/walleye/walleye:10/QQ3A.200805.001/6578210:user/release-keys__2020-08-05
+Google Pixel 2 XL (10)=google/taimen/taimen:10/QQ3A.200805.001/6578210:user/release-keys__2020-08-05
+Google Pixel 3 (10)=google/blueline/blueline:10/QQ3A.200805.001/6578210:user/release-keys__2020-08-05
+Google Pixel 3 XL (10)=google/crosshatch/crosshatch:10/QQ3A.200805.001/6578210:user/release-keys__2020-08-05
+Google Pixel 3a (10)=google/sargo/sargo:10/QQ3A.200805.001/6578210:user/release-keys__2020-08-05
+Google Pixel 3a XL (10)=google/bonito/bonito:10/QQ3A.200805.001/6578210:user/release-keys__2020-08-05
+Google Pixel 4 (10)=google/flame/flame:10/QQ3A.200805.001/6578210:user/release-keys__2020-08-05
+Google Pixel 4 XL (10)=google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys__2020-08-05
 Xiaomi Mi 10 Lite 5G (10):Xiaomi:M2002J9G=Xiaomi/monet_eea/monet:10/QKQ1.191222.002/V12.0.1.0.QJIEUXM:user/release-keys__2020-06-01
 Xiaomi Pocophone F1 (10)=Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.9.0.QEJMIXM:user/release-keys__2020-06-01
 "
 
 # Contributors:
-# @Sid220, @pepote666, @ipdev
+# @Sid220, @pepote666, @ipdev, @Displax
