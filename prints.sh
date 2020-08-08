1e59ed3ae9eab873dcc6a9e4183464af43591e2d
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-08 22:04:48 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,6 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Nokia 6.1 (10):Nokia:Nokia 6.1=Nokia/Plate2_00WW/PL2_sprout:10/QKQ1.190828.002/00WW_4_15E:user/release-keys__2020-06-01
 Samsung Galaxy S5 G900F (6.0.1)=samsung/kltexx/klte:6.0.1/MMB29M/G900FXXU1CRH1:user/release-keys
 "
 
