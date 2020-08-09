3ec959e8c6f17effccc73bb62f69ca9340e0c118
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-09 23:37:20 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -10,7 +10,8 @@ PRINTSTRANSF=1
 PRINTSLIST="
 Nokia 6.1 (10):Nokia:Nokia 6.1=Nokia/Plate2_00WW/PL2_sprout:10/QKQ1.190828.002/00WW_4_15E:user/release-keys__2020-06-01
 Samsung Galaxy S5 G900F (6.0.1)=samsung/kltexx/klte:6.0.1/MMB29M/G900FXXU1CRH1:user/release-keys
+Samsung Galaxy Tab S4 T830 (10)=samsung/gts4lwifixx/gts4lwifi:10/QP1A.190711.020/T830XXU4CTF5:user/release-keys__2020-06-01
 "
 
 # Contributors:
-# @Sid220, @pepote666, @ipdev, @Displax, @knojap
+# @Sid220, @pepote666, @ipdev, @Displax, @knojap, @enotar
