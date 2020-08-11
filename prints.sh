2481b09f60223e088a5a3794c1c02f5bd6cdb846
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-11 13:48:36 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,6 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Samsung Galaxy S10 SM-G973F (10):Samsung:SM-G973F=samsung/beyond1ltexx/beyond1:10/QP1A.190711.020/G973FXXS3BSL4:user/release-keys__2020-01-01
 "
 
 # Contributors:
