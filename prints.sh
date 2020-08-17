bcafd0449b3ed1f22ea999978f206bcc35ad89ee
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-17 07:50:32 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,10 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.12.0.QDIMIXM:user/release-keys__2020-07-05
+Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.9.0.QDLMIXM:user/release-keys__2020-07-01
+Xiaomi Mi A3 (10)=Xiaomi/laurel_sprout/laurel_sprout:10/QKQ1.190910.002/V11.0.20.0.QFQMIXM:user/release-keys__2020-08-05
 "
 
 # Contributors:
-# 
+# @Displax
