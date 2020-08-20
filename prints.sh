66e56aab126b1ff805aa02e747a3456d829818f5
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-20 09:46:16 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,12 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Poco X2 (10)=POCO/phoenixin/phoenixin:10/QKQ1.190825.002/V12.0.1.0.QGHINXM:user/release-keys__2020-07-01
-Sony Xperia 5 DSDS J9210 (10)=Sony/J9210_RU/J9210:10/55.1.A.9.52/055001A009005201481357772:user/release-keys__2020-07-01
-Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.12.0.QDIMIXM:user/release-keys__2020-07-05
-Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.9.0.QDLMIXM:user/release-keys__2020-07-01
-Xiaomi Mi A3 (10)=Xiaomi/laurel_sprout/laurel_sprout:10/QKQ1.190910.002/V11.0.20.0.QFQMIXM:user/release-keys__2020-08-05
 "
 
 # Contributors:
-# @Displax, @GtrCraft, @nos1609
+# 
