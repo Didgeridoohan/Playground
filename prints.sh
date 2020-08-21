dc29db7dd9ba559562b481fa5c88086cc9069830
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-22 01:04:21 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,9 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Huawei Mate 9 MHA-L29 (9):Huawei:MHA-L29=HUAWEI/MHA-L29/HWMHA:9/HUAWEIMHA-L29/9.1.0.275C432:user/release-keys__2020-05-01
+Xiaomi Mi 9T European (10)=Xiaomi/davinci_eea/davinci:10/QKQ1.190825.002/V12.0.2.0.QFJEUXM:user/release-keys__2020-07-01
 "
 
 # Contributors:
-# 
+# @zgfg
