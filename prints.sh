c16d0fc045bb8d09a39596d3f58177c7b9e79d83
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-23 13:27:50 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Huawei P9 EVA-AL10 (8.0.0):Huawei:EVA-AL10=HUAWEI/EVA-AL10/HWEVA:8.0.0/HUAWEIEVA-AL10/550(C00):user/release-keys__2020-07-01
 "
 
 # Contributors:
