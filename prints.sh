7978e2fbab6f2d82e955f6d1a8d1a0ed47ba08fb
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-24 07:00:22 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,8 +8,19 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Google Pixel 2 (11 11):Google:Pixel 2:=google/walleye/walleye:11/RPB3.200720.005/6705141:user/release-keys__2020-08-05
+Google Pixel 2 XL (11 11):Google:Pixel 2 XL:=google/taimen/taimen:11/RPB3.200720.005/6705141:user/release-keys__2020-08-05
+Google Pixel 3 (11 11):Google:Pixel 3:=google/blueline/blueline:11/RPB3.200720.005/6705141:user/release-keys__2020-08-05
+Google Pixel 3a (11 11):Google:Pixel 3a:=google/sargo/sargo:11/RPB3.200720.005/6705141:user/release-keys__2020-08-05
+Google Pixel 3a XL (11 11):Google:Pixel 3a XL:=google/bonito/bonito:11/RPB3.200720.005/6705141:user/release-keys__2020-08-05
+Google Pixel 3 XL (11 11):Google:Pixel 3 XL:=google/crosshatch/crosshatch:11/RPB3.200720.005/6705141:user/release-keys__2020-08-05
+Google Pixel 4 (11 11):Google:Pixel 4:=google/flame/flame:11/RPB3.200720.005/6705141:user/release-keys__2020-08-05
+Google Pixel 4 XL (11 11):Google:Pixel 4 XL:=google/coral/coral:11/RPB3.200720.005/6705141:user/release-keys__2020-08-05
 Huawei P9 EVA-AL10 (8.0.0):Huawei:EVA-AL10=HUAWEI/EVA-AL10/HWEVA:8.0.0/HUAWEIEVA-AL10/550(C00):user/release-keys__2020-07-01
+POCO F2 Pro (10):Xiaomi:POCO F2 Pro:=POCO/lmi_eea/lmi:10/QKQ1.191117.002/V12.0.2.0.QJKEUXM:user/release-keys__2020-07-01
+Redmi Note 9S (10):Xiaomi:Redmi Note 9S:=Redmi/curtana_global/curtana:10/QKQ1.191215.002/V11.0.10.0.QJWMIXM:user/release-keys__2020-07-01
+Samsung Galaxy A7 2018 SM-A750GN (9 & 10):samsung:SM-A750GN=samsung/a7y18ltedx/a7y18lte:9/PPR1.180610.011/A750GNDXU2BSD2:user/release-keys__2019-04-01;samsung/a7y18ltedx/a7y18lte:10/QP1A.190711.020/A750GNDXS6CTE1:user/release-keys__2020-06-01
 "
 
 # Contributors:
-# @zgfg
+# @zgfg, @ipdev, @Doligem
