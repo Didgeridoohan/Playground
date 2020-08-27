03686c3fcb93cd4190f9c2e7599ee613ad00d553
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-27 11:13:47 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,8 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Xiaomi MI 10 European (10):Xiaomi:Mi 10=Xiaomi/umi_eea/umi:10/QKQ1.191117.002/V11.0.10.0.QJBEUVF:user/release-keys__2020-07-01
 "
 
 # Contributors:
-# @zgfg, @ipdev, @Doligem, @Displax, @Some_Random_Username, @CoryCoolguy, @swour, @ReservedName, @unknown40404
+# 
