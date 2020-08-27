25f58d997232789162d403aa3ada6630e26f0557
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-27 20:00:19 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Pixel 2XL Test (9)=google/taimen/taimen-taimen:9/PQ3B.190801.002/5670241:user/release-keys__2019-08-05
+Google Pixel 2XL Test (9 & 10)=google/taimen/taimen-taimen:9/PQ3B.190801.002/5670241:user/release-keys__2019-08-05;google/taimen/taimen:10/QP1A.190711.020/5800535:user/release-keys__2019-09-05
 "
 
 # Contributors:
