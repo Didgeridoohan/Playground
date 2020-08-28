08ca396ea89843532bc1853e4775a091153397e8
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-28 09:40:54 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,6 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Huawei P9 Plus VIE-AL10 (8.0.0):Huawei:VIE-L29=Huawei/VIE-AL10/HWVIE:8.0.0/HUAWEIVIE-AL10/550(C00):user/release-keys__2020-07-01
 Huawei P9 Plus VIE-AL10 (8.0.0):Huawei:VIE-L29=HUAWEI/VIE-AL10/HWVIE:8.0.0/HUAWEIVIE-AL10/550(C00):user/release-keys__2020-07-01
 "
 
