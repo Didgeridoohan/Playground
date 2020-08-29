4566f4b3bc10fd2ac95499960bd20d3c998748d1
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-29 07:10:33 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -9,7 +9,10 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 Huawei P9 Plus VIE-AL10 (8.0.0):Huawei:VIE-L29=HUAWEI/VIE-AL10/HWVIE:8.0.0/HUAWEIVIE-AL10/550(C00):user/release-keys__2020-07-01
+Motorola Moto G7 (10):Motorola:moto g(7)=motorola/river/river:10/QPU30.52-16-2/be43c:user/release-keys__2020-04-01
+Motorola Moto G7 Power (10):Motorola:moto g(7) power=motorola/ocean/ocean:10/QPO30.52-29/c0490:user/release-keys__2020-04-01
+Motorola Moto G7 Play (10):Motorola:moto g(7) channel=motorola/channel/channel:10/QPY30.52-22/355d1:user/release-keys__2020-04-01
 "
 
 # Contributors:
-# @zgfg
+# @zgfg, @SyberHexen
