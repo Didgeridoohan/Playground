20ea5e6e85da32676ebacc6d7d87c9ec957e46c6
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2020-08-30 20:11:43 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -10,9 +10,11 @@ PRINTSTRANSF=1
 PRINTSLIST="
 Huawei P9 Plus VIE-AL10 (8.0.0):Huawei:VIE-L29=HUAWEI/VIE-AL10/HWVIE:8.0.0/HUAWEIVIE-AL10/550(C00):user/release-keys__2020-07-01
 Motorola Moto G7 (10):Motorola:moto g(7)=motorola/river/river:10/QPU30.52-16-2/be43c:user/release-keys__2020-04-01
+Motorola Moto G7 (10):Motorola:moto g(7)=motorola/river_retail/river:10/QPUS30.52-16-2-5/58144:user/release-keys__2020-08-01
 Motorola Moto G7 Power (10):Motorola:moto g(7) power=motorola/ocean/ocean:10/QPO30.52-29/c0490:user/release-keys__2020-04-01
 Motorola Moto G7 Play (10):Motorola:moto g(7) channel=motorola/channel/channel:10/QPY30.52-22/355d1:user/release-keys__2020-04-01
+Xiaomi Mi 10 Pro (10)=Xiaomi/cmi/cmi:10/QKQ1.191117.002/V12.0.4.0.QJACNXM:user/release-keys__2020-07-01
 "
 
 # Contributors:
-# @zgfg, @SyberHexen
+# @zgfg, @SyberHexen, @JarlPenguin, @smohanv
