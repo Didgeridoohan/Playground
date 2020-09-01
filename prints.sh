b2d324e4c29d0d6595344e87afc4bfcbeb21d297
Didgeridoohan <github@heptadecagon.one>
2020-09-01 10:01:30 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,15 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Huawei P9 Plus VIE-AL10 (8.0.0):Huawei:VIE-L29=HUAWEI/VIE-AL10/HWVIE:8.0.0/HUAWEIVIE-AL10/550(C00):user/release-keys__2020-07-01
-Motorola Moto G7 XT1962-1 (10):Motorola:moto g(7)=motorola/river/river:10/QPU30.52-16-2/be43c:user/release-keys__2020-04-01
-Motorola Moto G7 XT1962-5 (10):Motorola:moto g(7)=motorola/river_retru_n/river_n:10/QPUS30.52-16-2-5/58144:user/release-keys__2020-08-01
-Motorola Moto G7 Power (10):Motorola:moto g(7) power=motorola/ocean/ocean:10/QPO30.52-29/c0490:user/release-keys__2020-04-01
-Motorola Moto G7 Play (10):Motorola:moto g(7) channel=motorola/channel/channel:10/QPY30.52-22/355d1:user/release-keys__2020-04-01
-Redmi Note 9 Pro (10)=Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V11.0.5.0.QJZMIXM:user/release-keys__2020-08-01
-Samsung Galaxy tab A 10.1 2019 SM-T510 (10)=samsung/gta3xlwifieea/gta3xlwifi:10/QP1A.190711.020/T510XXU3BTFN:user/release-keys__2020-07-01
-Xiaomi Mi 10 Pro (10)=Xiaomi/cmi/cmi:10/QKQ1.191117.002/V12.0.4.0.QJACNXM:user/release-keys__2020-07-01
 "
 
 # Contributors:
-# @zgfg, @SyberHexen, @JarlPenguin, @smohanv, @klausxda, @Kraznov
+# 
