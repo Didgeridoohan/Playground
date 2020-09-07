71d364bba1840c9f361ac5a7f97486173d437288
Didgeridoohan <github@heptadecagon.one>
2020-09-07 20:51:28 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,9 +8,14 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 8 IN2013 (10): OnePlus/OnePlus8_EEA/OnePlus8:10/QKQ1.191222.002/2008150504:user/release-keys__2020-08-01
+OnePlus 8 Pro IN2023 (10): OnePlus/OnePlus8Pro_EEA/OnePlus8Pro:10/QKQ1.191222.002/2008150503:user/release-keys__2020-08-01
+OnePlus Nord AC2001 (10): OnePlus/Nord_IND/Nord:10/QKQ1.200412.002/2009010217:user/release-keys__2020-07-01
+OnePlus Nord European AC2003 (10): OnePlus/Nord_EEA/Nord:10/QKQ1.200412.002/2009010216:user/release-keys__2020-07-01
+OnePlus Nord Global AC2003 (10): OnePlus/Nord/Nord:10/QKQ1.200412.002/2009010217:user/release-keys__2020-07-01
 Samsung Galaxy J7 Neo (8.1.0)=samsung/j7velteub/j7velte:8.1.0/M1AJQ/J701MUBU6BSD2:user/release-keys__2019-04-01
 Samsung Galaxy Tab S5e SM-T720 (10)=samsung/gts4lvwifixx/gts4lvwifi:10/QP1A.190711.020/T720XXU1BTG3:user/release-keys__2020-06-01
 "
 
 # Contributors:
-# @lawrencee, @OhioYJ
+# @lawrencee, @OhioYJ, @Some_Random_Username
