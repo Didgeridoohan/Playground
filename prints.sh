8cafebe8ece01c98fe3efb520423b7d513be03ac
Didgeridoohan <github@heptadecagon.one>
2020-09-07 18:10:51 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -9,7 +9,8 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 Samsung Galaxy J7 Neo (8.1.0)=samsung/j7velteub/j7velte:8.1.0/M1AJQ/J701MUBU6BSD2:user/release-keys__2019-04-01
+Samsung Galaxy Tab S5e SM-T720 (10)=samsung/gts4lvwifixx/gts4lvwifi:10/QP1A.190711.020/T720XXU1BTG3:user/release-keys__2020-06-01
 "
 
 # Contributors:
-# 
+# @lawrencee, @OhioYJ
