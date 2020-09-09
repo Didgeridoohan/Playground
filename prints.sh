b55fe744e8446224cc5d90ba65d29cabf71bdf1f
Didgeridoohan <github@heptadecagon.one>
2020-09-09 04:35:22 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,9 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 8 IN2015 (11)=OnePlus/OnePlus8/OnePlus8:11/RP1A.201005.001/2009051300:user/release-keys__2020-09-01
+OnePlus 8 Pro IN2025 (11)=OnePlus/OnePlus8Pro/OnePlus8Pro:11/RP1A.201005.001/2009051300:user/release-keys__2020-09-01
 "
 
 # Contributors:
-# 
+# @Some_Random_Username
