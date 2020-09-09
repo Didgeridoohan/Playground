9b53f74469c03409755797d6225c166963d1965e
Didgeridoohan <github@heptadecagon.one>
2020-09-09 19:45:48 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -10,7 +10,8 @@ PRINTSTRANSF=1
 PRINTSLIST="
 OnePlus 8 IN2015 (11)=OnePlus/OnePlus8/OnePlus8:11/RP1A.201005.001/2009051300:user/release-keys__2020-09-01
 OnePlus 8 Pro IN2025 (11)=OnePlus/OnePlus8Pro/OnePlus8Pro:11/RP1A.201005.001/2009051300:user/release-keys__2020-09-01
+Xiaomi Pocophone F1 (10)=Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.1.0.QEJMIXM:user/release-keys__2020-08-01
 "
 
 # Contributors:
-# @Some_Random_Username
+# @Some_Random_Username, @Sid220
