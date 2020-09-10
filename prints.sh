de6d12e9399e5ffcdf77718bf687681cadc61884
Didgeridoohan <github@heptadecagon.one>
2020-09-10 10:19:41 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,6 +8,11 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Google Pixel 2 (11)=google/walleye/walleye:11/RP1A.200720.009/6720564:user/release-keys__2020-09-05
+Google Pixel 2 XL (11)=google/taimen/taimen:11/RP1A.200720.009/6720564:user/release-keys__2020-09-05
+Google Pixel 4 (11)=google/flame/flame:11/RP1A.200720.009/6720564:user/release-keys__2020-09-05
+Google Pixel 4 XL (11)=google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys__2020-09-05
+Google Pixel 4a (11)=google/sunfish/sunfish:11/RP1A.200720.011/6746289:user/release-keys__2020-09-05
 OnePlus 8 IN2015 (11)=OnePlus/OnePlus8/OnePlus8:11/RP1A.201005.001/2009051300:user/release-keys__2020-09-01
 OnePlus 8 Pro IN2025 (11)=OnePlus/OnePlus8Pro/OnePlus8Pro:11/RP1A.201005.001/2009051300:user/release-keys__2020-09-01
 Xiaomi Pocophone F1 (10)=Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.1.0.QEJMIXM:user/release-keys__2020-08-01
