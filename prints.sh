4e2a415123fe35d423f3099a33e50cccf47fa7ce
Didgeridoohan <github@heptadecagon.one>
2020-09-11 22:11:20 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,9 +8,6 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Sony Xperia 10 II Dual XQ-AU52 (10)=Sony/XQ-AU52_EEA/XQ-AU52:10/59.0.A.6.24/059000A006002402956232951:user/release-keys__2020-07-01
-Xiaomi Mi 10 Global (11)=Xiaomi/umi/umi:11/RKQ1.200710.002/V12.1.3.0.RJBCNXM:user/release-keys__2020-09-01
-Xiaomi Mi 10 Pro (11)=Xiaomi/cmi/cmi:11/RKQ1.200710.002/V12.1.2.0.RJACNXM:user/release-keys__2020-09-01
 "
 
 # Contributors:
