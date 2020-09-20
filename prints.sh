9386d0164d43130eaa7303b1cde96028362073b3
Didgeridoohan <github@heptadecagon.one>
2020-09-20 20:39:00 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -10,7 +10,8 @@ PRINTSTRANSF=1
 PRINTSLIST="
 Asus ZenFone Max Pro M1 ASUS_X00TD (9)=asus/WW_X00TD/ASUS_X00T_3:9/PKQ1/16.2017.2009.087-20200826:user/release-keys__2020-09-05
 OnePlus 8 IN2015 Visible (10):OnePlus:IN2015=OnePlus/OnePlus8Visible/OnePlus8Visible:10/QKQ1.191222.002/2007221621:user/release-keys__2020-06-01
+Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.11.0.QDLMIXM:user/release-keys__2020-09-01
 "
 
 # Contributors:
-# @kk9999gada, @Some_Random_Username
+# @kk9999gada, @Some_Random_Username, @Displax
