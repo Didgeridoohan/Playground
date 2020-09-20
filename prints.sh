7e8c56e74e65880aaaa4433e1aa3ee39c64fbd05
Didgeridoohan <github@heptadecagon.one>
2020-09-20 19:33:08 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -9,7 +9,8 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 Asus ZenFone Max Pro M1 ASUS_X00TD (9)=asus/WW_X00TD/ASUS_X00T_3:9/PKQ1/16.2017.2009.087-20200826:user/release-keys__2020-09-05
+OnePlus 8 IN2015 Visible (10):OnePlus:IN2015=OnePlus/OnePlus8Visible/OnePlus8Visible:10/QKQ1.191222.002/2007221621:user/release-keys__2020-06-01
 "
 
 # Contributors:
-# @kk9999gada
+# @kk9999gada, @Some_Random_Username
