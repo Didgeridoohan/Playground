0e668c69acf49afe5b5710bd9b10e2ca35e5c49d
Didgeridoohan <github@heptadecagon.one>
2020-09-21 00:53:37 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,11 +8,17 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Asus ZenFone 3 Ultra (ZU680KL) (7.0):asus:ASUS_A001=asus/WW_Phone/ASUS_A001:7.0/NRD90M/14.1010.1804.75-20180612:user/release-keys__2018-04-01
+Asus ZenFone 7 / 7 Pro (ZS670KS/ZS671KS) (10):asus:ASUS_I002D=asus/WW_I002D/ASUS_I002D:10/QKQ1.200127.002/29.12.18.14_20200902:user/release-keys__2020-07-05
+Asus ZenFone 5 (ZE620KL) (9):asus:ASUS_X00QD=asus/WW_X00QD/ASUS_X00QD:9/PPR1.180610.009/16.0615.2006.131-0:user/release-keys__2020-06-01
+Asus ZenFone 4 (ZE554KL) (8.0.0):asus:ASUS_Z01KD=asus/WW_Z01KD/ASUS_Z01KD_1:8.0.0/OPR1.170623.032/15.0610.1909.39-0:user/release-keys__2019-09-01
+Asus ZenFone 4 (ZE554KL) (9):asus:ASUS_Z01KD=asus/WW_Z01KD/ASUS_Z01KD_1:9/PPR1.180610.009/16.0620.1912.23-0:user/release-keys__2019-12-01
 Asus ZenFone Max Pro M1 ASUS_X00TD (9)=asus/WW_X00TD/ASUS_X00T_3:9/PKQ1/16.2017.2009.087-20200826:user/release-keys__2020-09-05
 OnePlus 8 IN2015 Visible (10):OnePlus:IN2015=OnePlus/OnePlus8Visible/OnePlus8Visible:10/QKQ1.191222.002/2007221621:user/release-keys__2020-06-01
+POCO F2 Pro (10):Xiaomi:POCO F2 Pro=POCO/lmi_global/lmi:10/QKQ1.191117.002/V12.0.3.0.QJKMIXM:user/release-keys__2020-07-01
 Samsung Galaxy Tab S4 SM-T835=samsung/gts4lltexx/gts4llte:10/QP1A.190711.020/T835XXU4CTF5:user/release-keys__2020-06-01
 Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.11.0.QDLMIXM:user/release-keys__2020-09-01
 "
 
 # Contributors:
-# @kk9999gada, @Some_Random_Username, @Displax
+# @kk9999gada, @Some_Random_Username, @Displax, @ipdev
