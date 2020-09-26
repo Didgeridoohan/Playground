8bc5d798139a0782511faf5af0cdde2a9ff22615
Didgeridoohan <github@heptadecagon.one>
2020-09-26 08:29:54 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,8 +8,9 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+POCO x3 (10)=POCO/surya_global/surya:10/QKQ1.200512.002/V12.0.2.0.QJGMIXM:user/release-keys__2020-09-01
 Samsung Galaxy Note 20 Ultra SM-N986U (10):Samsung:SM-N986U=samsung/c2qsqw/c2q:10/QP1A.190711.020/N986USQU1ATH3:user/release-keys__2020-08-01
 "
 
 # Contributors:
-# @TheUndertaker21
+# @TheUndertaker21, @Feeleepino
