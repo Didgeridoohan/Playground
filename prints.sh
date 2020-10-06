ed8af38c8c83bd1ea22fecb15ee34601ba7b04c6
Didgeridoohan <github@heptadecagon.one>
2020-10-06 22:15:20 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,8 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Xiaomi Redmi Note 8 (10)=xiaomi/ginkgo/ginkgo:10/QKQ1.200114.002/20.8.27:user/release-keys__2020-09-01
 "
 
 # Contributors:
-# @geekopa
+# 
