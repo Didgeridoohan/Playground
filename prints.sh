3e1fd97682cdcf74fb95683bc25259aa904d4232
Didgeridoohan <github@heptadecagon.one>
2020-10-08 10:16:23 +0200

    Testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,10 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 8T European (11)=OnePlus/OnePlus8T_EEA/OnePlus8T:11/RP1A.201005.001/2009210039:user/release-keys__2020-09-01
+Redmi Note 8 Pro (9)=Redmi/begonia/begonia:9/PPR1.180610.011/V10.4.1.0.PGGMIXM:user/release-keys__2019-09-01
+Xiaomi Redmi Note 8 (10)=xiaomi/ginkgo/ginkgo:10/QKQ1.200114.002/20.6.17:user/release-keys/1592537410__2020-05-01
 "
 
 # Contributors:
-# 
+# @geekopa, @Some_Random_Username
