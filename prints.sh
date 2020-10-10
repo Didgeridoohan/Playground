601fc4acd035dbb0b51becee80ecd39bc1a4d331
Didgeridoohan <github@heptadecagon.one>
2020-10-10 06:46:25 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -9,9 +9,13 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 OnePlus 8T European (11)=OnePlus/OnePlus8T_EEA/OnePlus8T:11/RP1A.201005.001/2009210039:user/release-keys__2020-09-01
+POCO F1 (10):Xiaomi:POCOPHONE F1=Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.2.0.QEJMIXM:user/release-keys__2020-09-01
+POCO F2 Pro European (10):POCO:POCO F2 Pro=POCO/lmi_eea/lmi:10/QKQ1.191117.002/V12.0.3.0.QJKEUXM:user/release-keys__2020-09-01
 Redmi Note 8 Pro (9)=Redmi/begonia/begonia:9/PPR1.180610.011/V10.4.1.0.PGGMIXM:user/release-keys__2019-09-01
+Redmi Note 8 Pro (10):Redmi:Redmi Note 8 Pro=Redmi/begonia/begonia:10/QP1A.190711.020/V12.0.3.0.QGGMIXM:user/release-keys__2020-08-01
+Xiaomi Redmi Note 8 (9):Xiaomi:Redmi Note 8=xiaomi/ginkgo/ginkgo:9/PKQ1.190616.001/V11.0.11.0.PCOMIXM:user/release-keys__2020-09-01
 Xiaomi Redmi Note 8 (10)=xiaomi/ginkgo/ginkgo:10/QKQ1.200114.002/20.6.17:user/release-keys/1592537410__2020-05-01
 "
 
 # Contributors:
-# @geekopa, @Some_Random_Username
+# @geekopa, @Some_Random_Username, @ipdev
