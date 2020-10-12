5a19d02b857edad47d0b311ec7f6df4b9ef54371
Didgeridoohan <github@heptadecagon.one>
2020-10-12 18:49:27 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -14,7 +14,8 @@ OnePlus 8 IN2015 (11)=OnePlus/OnePlus8/OnePlus8:11/RP1A.201005.001/2010031600:us
 OnePlus 8 Pro IN2021 (11)=OnePlus/OnePlus8Pro_IND/OnePlus8Pro:11/RP1A.201005.001/2009301800:user/release-keys__2020-09-01
 OnePlus 8 Pro IN2023 (11)=OnePlus/OnePlus8Pro_EEA/OnePlus8Pro:11/RP1A.201005.001/2009301800:user/release-keys__2020-09-01
 OnePlus 8 Pro IN2025 (11)=OnePlus/OnePlus8Pro/OnePlus8Pro:11/RP1A.201005.001/2010031600:user/release-keys__2020-09-01
+Xiaomi Mi 9T Global (10):Xiaomi:Mi 9T=Xiaomi/davinci/davinci:10/QKQ1.190825.002/V12.0.3.0.QFJMIXM:user/release-keys__2020-08-01
 "
 
 # Contributors:
-# @geekopa, @Some_Random_Username, @ipdev
+# @geekopa, @Some_Random_Username, @ipdev, @EmperorEye1993 (GitHub)
