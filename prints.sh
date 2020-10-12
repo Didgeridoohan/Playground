fc29635f403cbc36464db1ccf0263f981c222683
Didgeridoohan <github@heptadecagon.one>
2020-10-12 06:31:59 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,12 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+OnePlus 8 IN2011 (11)=OnePlus/OnePlus8_IND/OnePlus8:11/RP1A.201005.001/2009301800:user/release-keys__2020-09-01
+OnePlus 8 IN2013 (11)=OnePlus/OnePlus8_EEA/OnePlus8:11/RP1A.201005.001/2009301800:user/release-keys__2020-09-01
+OnePlus 8 IN2015 (11)=OnePlus/OnePlus8/OnePlus8:11/RP1A.201005.001/2010031600:user/release-keys__2020-09-01
+OnePlus 8 Pro IN2021 (11)=OnePlus/OnePlus8Pro_IND/OnePlus8Pro:11/RP1A.201005.001/2009301800:user/release-keys__2020-09-01
+OnePlus 8 Pro IN2023 (11)=OnePlus/OnePlus8Pro_EEA/OnePlus8Pro:11/RP1A.201005.001/2009301800:user/release-keys__2020-09-01
+OnePlus 8 Pro IN2025 (11)=OnePlus/OnePlus8Pro/OnePlus8Pro:11/RP1A.201005.001/2010031600:user/release-keys__2020-09-01
 "
 
 # Contributors:
