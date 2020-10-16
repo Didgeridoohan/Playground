e827a687c7440dffb0337f245b973076167d7075
Didgeridoohan <github@heptadecagon.one>
2020-10-16 21:07:09 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,11 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-OnePlus 6 (10)=OnePlus/OnePlus6/OnePlus6:10/QKQ1.190716.003/2010042216:user/release-keys__2020-09-01
-OnePlus 6T (10)=OnePlus/OnePlus6T/OnePlus6T:10/QKQ1.190716.003/2010042216:user/release-keys__2020-09-01
-Xiaomi Mi 10 Lite 5G Global (10):Xiaomi:M2002J9G=Xiaomi/monet_global/monet:10/QKQ1.191222.002/V12.0.1.0.QJIMIXM:user/release-keys__2020-08-01
-Xiaomi Mi 10 Ultra (10):Xiaomi:Mi 10 Ultra=Xiaomi/cas/cas:10/QKQ1.200419.002/V12.0.10.0.QJJCNXM:user/release-keys__2020-08-01
 "
 
 # Contributors:
-# @geekopa, @Some_Random_Username, @ipdev, @Fangshing87 (Github)
+# @geekopa, @Some_Random_Username, @ipdev, @Fangshing87
