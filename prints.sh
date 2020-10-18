3931c83612b052925d2526e66e646ebfc523a2a8
Didgeridoohan <github@heptadecagon.one>
2020-10-18 19:32:34 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,9 +8,6 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-OnePlus 8T India KB2001 (11):OnePlus:KB2001=OnePlus/OnePlus8T_IND/OnePlus8T:11/RP1A.201005.001/2009181137:user/release-keys__2020-09-01
-OnePlus 8T KB2005 (11):OnePlus:KB2005=OnePlus/OnePlus8T/OnePlus8T:11/RP1A.201005.001/2009181139:user/release-keys__2020-09-01
-OnePlus 8T T-Mobile KB2007 (11):OnePlus:KB2007=OnePlus/OnePlus8TTMO/OnePlus8TTMO:11/RP1A.201005.001/2009240808:user/release-keys__2020-09-01
 "
 
 # Contributors:
