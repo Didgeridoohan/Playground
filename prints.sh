bdd2d3794f36febe3c5776170a7de80a88619b47
Didgeridoohan <github@heptadecagon.one>
2020-10-19 21:26:41 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,11 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Nvidia Shield TV 2015 (8.0.0 & 9):NVIDIA:SHIELD Android TV=NVIDIA/foster_e/foster:8.0.0/OPR6.170623.010/3507953_1441.7411:user/release-keys__2018-12-05;NVIDIA/foster_e/foster:9/PPR1.180610.011/4079208_2235.1395:user/release-keys__2020-07-05
+Nvidia Shield TV 2015 Pro (8.0.0 & 9):NVIDIA:SHIELD Android TV=NVIDIA/foster_e_hdd/foster:8.0.0/OPR6.170623.010/3507953_1441.7411:user/release-keys__2018-12-05;NVIDIA/foster_e_hdd/foster:9/PPR1.180610.011/4079208_2235.1395:user/release-keys__2020-07-05
+Nvidia Shield TV 2017 (8.0.0 & 9):NVIDIA:SHIELD Android TV=NVIDIA/darcy/darcy:8.0.0/OPR6.170623.010/3507953_1441.7411:user/release-keys__2018-12-05;NVIDIA/darcy/darcy:9/PPR1.180610.011/4079208_2235.1395:user/release-keys__2020-07-05
+Nvidia Shield TV 2019 (9):NVIDIA:SHIELD Android TV=NVIDIA/sif/sif:9/PPR1.180610.011/4079180_2235.1395:user/release-keys__2020-07-05
+Nvidia Shield TV 2019 Pro (9):NVIDIA:SHIELD Android TV=NVIDIA/mdarcy/mdarcy:9/PPR1.180610.011/4079208_2235.1395:user/release-keys__2020-07-05
 Xiaomi Mi 9T Global (10)=Xiaomi/davinci/davinci:10/QKQ1.190825.002/V12.0.5.0.QFJMIXM:user/release-keys__2020-10-01
 Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.12.0.QDLMIXM:user/release-keys__2020-10-01
 "
