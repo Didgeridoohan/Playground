9feca80643414249082a929ff2586fa3b237199c
Didgeridoohan <github@heptadecagon.one>
2020-10-25 08:27:32 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -13,9 +13,6 @@ Nvidia Shield TV 2015 Pro (8.0.0 & 9):NVIDIA:SHIELD Android TV=NVIDIA/foster_e_h
 Nvidia Shield TV 2017 (8.0.0 & 9):NVIDIA:SHIELD Android TV=NVIDIA/darcy/darcy:8.0.0/OPR6.170623.010/3507953_1441.7411:user/release-keys__2018-12-05;NVIDIA/darcy/darcy:9/PPR1.180610.011/4079208_2235.1395:user/release-keys__2020-07-05
 Nvidia Shield TV 2019 (9):NVIDIA:SHIELD Android TV=NVIDIA/sif/sif:9/PPR1.180610.011/4079180_2235.1395:user/release-keys__2020-07-05
 Nvidia Shield TV 2019 Pro (9):NVIDIA:SHIELD Android TV=NVIDIA/mdarcy/mdarcy:9/PPR1.180610.011/4079208_2235.1395:user/release-keys__2020-07-05
-Xiaomi Mi 8 UD/Pro China (8.1.0 & 9 & 10):Xiaomi:Mi 8 UD=Xiaomi/equuleus/equuleus:8.1.0/OPM1.190828.002/V10.0.2.0.OECCNFH:user/release-keys;Xiaomi/equuleus/equuleus:9/PKQ1.190828.002/V10.2.1.0.PECCNFK:user/release-keys;Xiaomi/equuleus/equuleus:10/QKQ1.190828.002/V12.0.2.0.QECCNXM:user/release-keys__2020-08-01
-Xiaomi Mi 8 UD/Pro Global (8.1.0 & 9 & 10):Xiaomi:Mi 8 UD=Xiaomi/equuleus/equuleus:8.1.0/OPM1.190828.002/V10.0.1.0.OECMIFH:user/release-keys;Xiaomi/equuleus/equuleus:9/PKQ1.190828.002/V10.2.1.0.PECMIXM:user/release-keys;Xiaomi/equuleus/equuleus:10/QKQ1.190828.002/V11.0.11.0.QECMIXM:user/release-keys__2020-08-01
-Xiaomi Mi 8 UD/Pro Russia (9 & 10):Xiaomi:Mi 8 UD=Xiaomi/equuleus/equuleus:9/PKQ1.190828.002/V10.2.2.0.PECRUXM:user/release-keys;Xiaomi/equuleus/equuleus:10/QKQ1.190828.002/V11.0.4.0.QECRUXM:user/release-keys__2020-08-01
 Xiaomi Mi 9T Global (10)=Xiaomi/davinci/davinci:10/QKQ1.190825.002/V12.0.5.0.QFJMIXM:user/release-keys__2020-10-01
 Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.12.0.QDLMIXM:user/release-keys__2020-10-01
 "
