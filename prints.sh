881e94d013c2c596ec606578e0faf271404fe086
Didgeridoohan <github@heptadecagon.one>
2020-10-25 12:27:13 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -13,10 +13,11 @@ Nvidia Shield TV 2015 Pro (8.0.0 & 9):NVIDIA:SHIELD Android TV=NVIDIA/foster_e_h
 Nvidia Shield TV 2017 (8.0.0 & 9):NVIDIA:SHIELD Android TV=NVIDIA/darcy/darcy:8.0.0/OPR6.170623.010/3507953_1441.7411:user/release-keys__2018-12-05;NVIDIA/darcy/darcy:9/PPR1.180610.011/4079208_2235.1395:user/release-keys__2020-07-05
 Nvidia Shield TV 2019 (9):NVIDIA:SHIELD Android TV=NVIDIA/sif/sif:9/PPR1.180610.011/4079180_2235.1395:user/release-keys__2020-07-05
 Nvidia Shield TV 2019 Pro (9):NVIDIA:SHIELD Android TV=NVIDIA/mdarcy/mdarcy:9/PPR1.180610.011/4079208_2235.1395:user/release-keys__2020-07-05
+Samsung Galaxy Note 20 Ultra 5G SM-N9860 (10):Samsung:SM-N9860=samsung/c2qzhx/c2q:10/QP1A.190711.020/N9860ZSU1ATJ6:user/release-keys__2020-09-01
 Xiaomi Mi 9T Global (10)=Xiaomi/davinci/davinci:10/QKQ1.190825.002/V12.0.5.0.QFJMIXM:user/release-keys__2020-10-01
 Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.15.0.QDIMIXM:user/release-keys__2020-10-01
 Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.12.0.QDLMIXM:user/release-keys__2020-10-01
 "
 
 # Contributors:
-# @geekopa, @Some_Random_Username, @ipdev, @Fangshing87, @zgfg, @malowz, @Displax
+# @geekopa, @Some_Random_Username, @ipdev, @Fangshing87, @zgfg, @malowz, @Displax, @dron39
