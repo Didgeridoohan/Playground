95db9a822a4c65a044281f751e7d05a520e532f3
Didgeridoohan <github@heptadecagon.one>
2020-10-25 11:35:19 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -14,8 +14,9 @@ Nvidia Shield TV 2017 (8.0.0 & 9):NVIDIA:SHIELD Android TV=NVIDIA/darcy/darcy:8.
 Nvidia Shield TV 2019 (9):NVIDIA:SHIELD Android TV=NVIDIA/sif/sif:9/PPR1.180610.011/4079180_2235.1395:user/release-keys__2020-07-05
 Nvidia Shield TV 2019 Pro (9):NVIDIA:SHIELD Android TV=NVIDIA/mdarcy/mdarcy:9/PPR1.180610.011/4079208_2235.1395:user/release-keys__2020-07-05
 Xiaomi Mi 9T Global (10)=Xiaomi/davinci/davinci:10/QKQ1.190825.002/V12.0.5.0.QFJMIXM:user/release-keys__2020-10-01
+Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.15.0.QDIMIXM:user/release-keys__2020-10-01
 Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.12.0.QDLMIXM:user/release-keys__2020-10-01
 "
 
 # Contributors:
-# @geekopa, @Some_Random_Username, @ipdev, @Fangshing87, @zgfg, @malowz
+# @geekopa, @Some_Random_Username, @ipdev, @Fangshing87, @zgfg, @malowz, @Displax
