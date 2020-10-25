99368bde82f9be07ebb0f401bc916e0a5f0d6dca
Didgeridoohan <github@heptadecagon.one>
2020-10-25 20:08:19 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Mi 9T Europe (10)=Xiaomi/davinci_eea/davinci:10/QKQ1.190825.002/V12.0.3.0.QFJEUXM:user/release-keys__2020-10-01
 "
 
 # Contributors:
-# @geekopa, @Some_Random_Username, @ipdev, @Fangshing87, @zgfg, @malowz, @Displax, @dron39, @dynux90
+# @geekopa, @Some_Random_Username, @ipdev, @Fangshing87, @zgfg, @malowz, @Displax, @dron39, @dynux90, @zgfg
