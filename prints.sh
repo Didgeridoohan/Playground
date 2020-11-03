06d325c6677da3b716229233bb4293c09c5c2fac
Didgeridoohan <github@heptadecagon.one>
2020-11-03 09:44:14 +0100

    testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,16 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Google Pixel 3=google/blueline/blueline:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
+Google Pixel 3 XL=google/crosshatch/crosshatch:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
+Google Pixel 3a=google/sargo/sargo:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
+Google Pixel 3a XL=google/bonito/bonito:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
+Google Pixel 4=google/flame/flame:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
+Google Pixel 4 XL=google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
+Google Pixel 4a=google/sunfish/sunfish:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
+Google Pixel 4a (5G)=google/bramble/bramble:11/RD1A.201105.003.C1/6886399:user/release-keys__2020-11-05
+Google Pixel 5=google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys__2020-11-05
 "
 
 # Contributors:
-# 
+# @Displax
