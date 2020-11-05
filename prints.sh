f5772ffd2c590d2eecfb637c4b8fcbb6e9a039d6
Didgeridoohan <github@heptadecagon.one>
2020-11-05 23:24:10 +0100

    Testing

--- prints.sh
+++ prints.sh
@@ -17,8 +17,10 @@ Google Pixel 4 XL=google/coral/coral:11/RP1A.201105.002/6869500:user/release-key
 Google Pixel 4a=google/sunfish/sunfish:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
 Google Pixel 4a (5G)=google/bramble/bramble:11/RD1A.201105.003.C1/6886399:user/release-keys__2020-11-05
 Google Pixel 5=google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys__2020-11-05
-Samsung A50S SM-A507FN (10):Samsung:SM-A507FN=samsung/a50sxx/a50s:10/QP1A.190711.020/A507FNXXS5BTI1:user/release-keys__2020-09-01
+Motorola Moto G7 Play T-Mobile (10)=motorola/channel_t/channel:10/QPYS30.85-18-3/60cdb:user/release-keys__2020-08-01
+Samsung Galaxy A5 2017 (8.0.0)=samsung/a5y17ltexx/a5y17lte:8.0.0/R16NW/A520FXXUGCTI9:user/release-keys__2020-09-01
+Samsung Galaxy A50S SM-A507FN (10):Samsung:SM-A507FN=samsung/a50sxx/a50s:10/QP1A.190711.020/A507FNXXS5BTI1:user/release-keys__2020-09-01
 "
 
 # Contributors:
-# @Displax, @rheznendra.p
+# @Displax, @rheznendra.p, @swour, @AL2009man
