e0607eaba16915fa3f31eb9ce9ec203397adbde4
Didgeridoohan <github@heptadecagon.one>
2020-11-09 08:29:30 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -20,7 +20,8 @@ Google Pixel 5=google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-k
 Motorola Moto G7 Play T-Mobile (10)=motorola/channel_t/channel:10/QPYS30.85-18-3/60cdb:user/release-keys__2020-08-01
 Samsung Galaxy A5 2017 (8.0.0)=samsung/a5y17ltexx/a5y17lte:8.0.0/R16NW/A520FXXUGCTI9:user/release-keys__2020-09-01
 Samsung Galaxy A50S SM-A507FN (10):Samsung:SM-A507FN=samsung/a50sxx/a50s:10/QP1A.190711.020/A507FNXXS5BTI1:user/release-keys__2020-09-01
+Xiaomi Mi 10T Pro Europe (10):Xiaomi:M2007J3SG=Redmi/apollo_eea/apollo:10/QKQ1.200419.002/V12.0.10.0.QJDEUXM:user/release-keys__2020-09-01
 "
 
 # Contributors:
-# @Displax, @rheznendra.p, @swour, @AL2009man
+# @Displax, @rheznendra.p, @swour, @AL2009man, @BlueMan_86
