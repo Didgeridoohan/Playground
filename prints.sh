206f01240d8643fa62d258ec31db53afb2acc0f4
Didgeridoohan <github@heptadecagon.one>
2020-11-10 09:59:19 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -9,18 +9,18 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Pixel 3=google/blueline/blueline:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
-Google Pixel 3 XL=google/crosshatch/crosshatch:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
-Google Pixel 3a=google/sargo/sargo:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
-Google Pixel 3a XL=google/bonito/bonito:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
-Google Pixel 4=google/flame/flame:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
-Google Pixel 4 XL=google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
-Google Pixel 4a=google/sunfish/sunfish:11/RP1A.201105.002/6869500:user/release-keys__2020-11-05
+Google Pixel 3=
+Google Pixel 3 XL=
+Google Pixel 3a=
+Google Pixel 3a XL=
+Google Pixel 4=
+Google Pixel 4 XL=
+Google Pixel 4a=
 Google Pixel 4a (5G)=google/bramble/bramble:11/RD1A.201105.003.C1/6886399:user/release-keys__2020-11-05
 Google Pixel 5=google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys__2020-11-05
 Motorola Moto G7 Play T-Mobile (10)=motorola/channel_t/channel:10/QPYS30.85-18-3/60cdb:user/release-keys__2020-08-01
 Redmi K30S Ultra (10):Redmi:M2007J3SC=Redmi/apollo/apollo:10/QKQ1.200419.002/V12.0.3.0.QJDCNXM:user/release-keys__2020-09-01
-Samsung Galaxy A5 2017 (8.0.0)=samsung/a5y17ltexx/a5y17lte:8.0.0/R16NW/A520FXXUGCTI9:user/release-keys__2020-09-01
+Samsung Galaxy A5 2017 (8.0.0)=
 Samsung Galaxy A50S SM-A507FN (10):Samsung:SM-A507FN=samsung/a50sxx/a50s:10/QP1A.190711.020/A507FNXXS5BTI1:user/release-keys__2020-09-01
 Xiaomi Mi 10T Europe (10):Xiaomi:M2007J3SY=Xiaomi/apollo_eea/apollo:10/QKQ1.200419.002/V12.0.4.0.QJDEUXM:user/release-keys__2020-09-01
 Xiaomi Mi 10T Pro Europe (10):Xiaomi:M2007J3SG=Xiaomi/apollopro_eea/apollo:10/QKQ1.200419.002/V12.0.4.0.QJDEUXM:user/release-keys__2020-09-01
