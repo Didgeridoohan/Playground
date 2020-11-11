10ad5312a5386e1955d7f4c44c74aeddfe48301c
Didgeridoohan <github@heptadecagon.one>
2020-11-11 12:06:35 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -9,8 +9,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-POCO X2 (10)=POCO/phoenixin/phoenixin:10/QKQ1.190825.002/V12.0.2.0.QGHINXM:user/release-keys__2020-10-01
 "
 
 # Contributors:
-# @GtrCraft
+# 
