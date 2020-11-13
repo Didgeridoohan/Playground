2a909cb69fe6f435f9cf84641617ffcf4ccbc14d
Didgeridoohan <github@heptadecagon.one>
2020-11-13 17:02:19 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -9,7 +9,11 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Redmi K30S Ultra (10)=Redmi/apollo_eea/apollo:10/QKQ1.200419.002/V12.0.10.0.QJDEUXM:user/release-keys__2020-09-01
+Samsung Galaxy A7 2018 SM-A750GN (10)=samsung/a7y18ltedx/a7y18lte:10/QP1A.190711.020/A750GNDXS6CTJ2:user/release-keys__2020-10-01
+Xiaomi Mi 10T (10)=Xiaomi/apollo_eea/apollo:10/QKQ1.200419.002/V12.0.10.0.QJDEUXM:user/release-keys__2020-09-01
+Xiaomi Mi 10T Pro (10)=Xiaomi/apollopro_eea/apollo:10/QKQ1.200419.002/V12.0.10.0.QJDEUXM:user/release-keys__2020-09-01
 "
 
 # Contributors:
-# 
+# @K-218, @BlueMan_86
