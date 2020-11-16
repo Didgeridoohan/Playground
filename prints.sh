1c1d92b4359c80bcc329684e3e073ef06a352b5f
Didgeridoohan <github@heptadecagon.one>
2020-11-16 14:41:42 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -10,7 +10,8 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 OnePlus 8T China (11):OnePlus:KB2000=OnePlus/OnePlus8T_CH/OnePlus8T:11/RP1A.201005.001/2011100046:user/release-keys__2020-10-01
-"
+Samsung Galaxy Note 20 Ultra 5G SM-N986B (10):samsung:SM-N986B=samsung/c2sxxx/c2s:10/QP1A.190711.020/N986BXXU1ATJ1:user/release-keys__2020-10-01
+Samsung Galaxy S20 FE 5G (10):samsung:SM-G781B=samsung/z3sxxx/z3s:10/QP1A.190711.020/G988BXXU1ATCT:user/release-keys__2020-10-01"
 
 # Contributors:
-# @red_lnn
+# @red_lnn, @Johnny Eng, @dron39
