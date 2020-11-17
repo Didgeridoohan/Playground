d102ae29c6ba02b9c0084087eaa16f2692cc85a6
Didgeridoohan <github@heptadecagon.one>
2020-11-17 08:38:18 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -12,6 +12,8 @@ PRINTSLIST="
 OnePlus 8T China (11):OnePlus:KB2000=OnePlus/OnePlus8T_CH/OnePlus8T:11/RP1A.201005.001/2011100046:user/release-keys__2020-10-01
 Samsung Galaxy Note 20 Ultra 5G SM-N986B (10):samsung:SM-N986B=samsung/c2sxxx/c2s:10/QP1A.190711.020/N986BXXU1ATJ1:user/release-keys__2020-10-01
 Samsung Galaxy S20 FE 5G (10):samsung:SM-G781B=samsung/r8qxxx/r8q:10/QP1A.190711.020/G781BXXU1ATJ5:user/release-keys__2020-10-01
+Xiaomi Mi 10 China (10)=Xiaomi/umi/umi:10/QKQ1.191117.002/V12.0.8.0.QJBCNXM:user/release-keys__2020-07-01
+Xiaomi Mi 10 China (11)=Xiaomi/umi/umi:11/RKQ1.200710.002/V12.1.3.0.RJBCNXM:user/release-keys__2020-09-01
 "
 
 # Contributors:
