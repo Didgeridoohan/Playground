0ade95fc7a276106fc53552cd66c38b8400483a9
Didgeridoohan <github@heptadecagon.one>
2020-12-02 22:15:31 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+POCO X3 NFC Europe (10)=POCO/surya_eea/surya:10/QKQ1.200512.002/V12.0.4.0.QJGEUXM:user/release-keys__2020-10-01
 "
 
 # Contributors:
-# 
+# @GtrCraft
