e49b827a544eaa841a24b7be2732ce800c1010ca
Didgeridoohan <github@heptadecagon.one>
2020-12-03 08:16:37 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -9,7 +9,8 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 POCO X3 NFC Europe (10)=POCO/surya_eea/surya:10/QKQ1.200512.002/V12.0.4.0.QJGEUXM:user/release-keys__2020-10-01
+Samsung Galaxy Tab E 9.6 SM-T560 (4.4.4):Samsung:SM-T560=samsung/gtelwifixx/gtelwifi:4.4.4/KTU84P/T560XXU0APL1:user/release-keys
 "
 
 # Contributors:
-# @GtrCraft
+# @GtrCraft, @carlos974
