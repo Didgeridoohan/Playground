a505db8e1c064d717bfe6ecda63396574a42bf2c
Didgeridoohan <github@heptadecagon.one>
2020-12-11 08:54:34 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,22 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-ASUS ZENFONE 7 Pro (10):asus:ASUS_I002D=asus/EU_I002D/ASUS_I002D:10/QKQ1.200127.002/29.14.53.5_20201111:user/release-keys__2020-10-05
-Google Pixel 2 (11)=google/walleye/walleye:11/RP1A.201005.004.A1/6934943:user/release-keys__2020-10-05
-Google Pixel 2 XL (11)=google/taimen/taimen:11/RP1A.201005.004.A1/6934943:user/release-keys__2020-10-05 
-Google Pixel 3 (11)=google/blueline/blueline:11/RQ1A.201205.003/6906706:user/release-keys__2020-12-05
-Google Pixel 3 XL (11)=google/crosshatch/crosshatch:11/RQ1A.201205.003/6906706:user/release-keys__2020-12-05
-Google Pixel 3a (11)=google/sargo/sargo:11/RQ1A.201205.003/6906706:user/release-keys__2020-12-05
-Google Pixel 3a XL (11)=google/bonito/bonito:11/RQ1A.201205.003/6906706:user/release-keys__2020-12-05
-Google Pixel 4 (11)=google/flame/flame:11/RQ1A.201205.008/6943376:user/release-keys__2020-12-05
-Google Pixel 4 XL (11)=google/coral/coral:11/RQ1A.201205.008/6943376:user/release-keys__2020-12-05
-Google Pixel 4a (11)=google/sunfish/sunfish:11/RQ1A.201205.008/6943376:user/release-keys__2020-12-05
-Google Pixel 4a 5G (11)=google/bramble/bramble:11/RQ1A.201205.011/6966805:user/release-keys__2020-12-05
-Google Pixel 5 (11)=google/redfin/redfin:11/RQ1A.201205.011/6966805:user/release-keys__2020-12-05
-POCO X3 NFC Europe (10)=POCO/surya_eea/surya:10/QKQ1.200512.002/V12.0.4.0.QJGEUXM:user/release-keys__2020-10-01
-Samsung Galaxy Tab E 9.6 SM-T560 (4.4.4):Samsung:SM-T560=samsung/gtelwifixx/gtelwifi:4.4.4/KTU84P/T560XXU0APL1:user/release-keys
-Xiaomi Mi A3 Global (10)=Xiaomi/laurel_sprout/laurel_sprout:10/QKQ1.190910.002/V11.0.23.0.QFQMIXM:user/release-keys__2020-11-05
 "
 
 # Contributors:
-# @GtrCraft, @carlos974, @IanWorthington, @garylawwd, @Displax
+# 
