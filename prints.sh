5479d5674e86cbd6ebb314c0ed4ac88fd6e5a3da
Didgeridoohan <github@heptadecagon.one>
2021-01-03 22:58:30 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -10,7 +10,6 @@ PRINTSTRANSF=1
 PRINTSLIST="
 Meizu 17 (10):meizu:meizu 17=meizu/meizu_17_N_CN/meizu17:10/QKQ1.200223.002/1599445800:user/release-keys__2020-07-01
 Redmi Note 8 Pro Europe (10)=Redmi/begonia_eea/begonia:10/QP1A.190711.020/V12.0.3.0.QGGEUXM:user/release-keys__2020-11-01
-Samsung Galaxy A6 (10)=samsung/a6ltexx/a6lte:10/QP1A.190711.020/A600FNXXS7CTI1:user/release-keys__2020-09-01
 Xiaomi MI 9 (11)=Xiaomi/cepheus/cepheus:11/RKQ1.200826.002/20.12.28:user/release-keys__2020-12-01
 Xiaomi Mi 10T Europe (10)=Xiaomi/apollo_eea/apollo:10/QKQ1.200419.002/V12.0.13.0.QJDEUXM:user/release-keys__2020-11-01
 Xiaomi Mi 10T Pro Europe (10)=Xiaomi/apollopro_eea/apollo:10/QKQ1.200419.002/V12.0.13.0.QJDEUXM:user/release-keys__2020-11-01
