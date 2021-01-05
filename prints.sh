d02492fe81ce3e2582e32ef9e1a4a408561ff422
Didgeridoohan <github@heptadecagon.one>
2021-01-05 11:53:05 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -10,6 +10,7 @@ PRINTSTRANSF=1
 PRINTSLIST="
 Meizu 17 (10):meizu:meizu 17=meizu/meizu_17_N_CN/meizu17:10/QKQ1.200223.002/1599445800:user/release-keys__2020-07-01
 Redmi Note 8 Pro Europe (10)=Redmi/begonia_eea/begonia:10/QP1A.190711.020/V12.0.3.0.QGGEUXM:user/release-keys__2020-11-01
+Samsung Galaxy S10e SM-G970F (10):samsung:SM-G970F=samsung/beyond0ltexx/beyond0:10/QP1A.190711.020/G970FXXS9DTK9:user/release-keys__2020-12-01
 Xiaomi MI 9 (11)=Xiaomi/cepheus/cepheus:11/RKQ1.200826.002/20.12.28:user/release-keys__2020-12-01
 Xiaomi Mi 10T Europe (10)=Xiaomi/apollo_eea/apollo:10/QKQ1.200419.002/V12.0.13.0.QJDEUXM:user/release-keys__2020-11-01
 Xiaomi Mi 10T Pro Europe (10)=Xiaomi/apollopro_eea/apollo:10/QKQ1.200419.002/V12.0.13.0.QJDEUXM:user/release-keys__2020-11-01
@@ -17,4 +18,4 @@ Xiaomi Redmi Note 8 (10)=xiaomi/ginkgo/ginkgo:10/QKQ1.200114.002/V12.0.1.0.QCOMI
 "
 
 # Contributors:
-# @emanresu2, @BlueMan_86, @sh3rry on GitHub, @Mr54233 on GitHub, @FivEawE
+# @emanresu2, @BlueMan_86, @sh3rry on GitHub, @Mr54233 on GitHub, @FivEawE, @oursgentil
