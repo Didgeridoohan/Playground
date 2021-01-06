057bb76a55146fa93e89a8335b1d2e2db843a941
Didgeridoohan <github@heptadecagon.one>
2021-01-06 10:02:36 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -9,6 +9,8 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 Meizu 17 (10):meizu:meizu 17=meizu/meizu_17_N_CN/meizu17:10/QKQ1.200223.002/1599445800:user/release-keys__2020-07-01
+POCO X3 Europe (10)=POCO/surya_eea/surya:10/QKQ1.200512.002/V12.0.7.0.QJGEUXM:user/release-keys__2020-11-01
+POCO X3 Global (10)=POCO/surya_global/surya:10/QKQ1.200512.002/V12.0.7.0.QJGMIXM:user/release-keys__2020-11-01
 Redmi Note 8 Pro Europe (10)=Redmi/begonia_eea/begonia:10/QP1A.190711.020/V12.0.3.0.QGGEUXM:user/release-keys__2020-11-01
 Samsung Galaxy S10e SM-G970F (10):samsung:SM-G970F=samsung/beyond0ltexx/beyond0:10/QP1A.190711.020/G970FXXS9DTK9:user/release-keys__2020-12-01
 Xiaomi MI 9 (11)=Xiaomi/cepheus/cepheus:11/RKQ1.200826.002/20.12.28:user/release-keys__2020-12-01
@@ -18,4 +20,4 @@ Xiaomi Redmi Note 8 (10)=xiaomi/ginkgo/ginkgo:10/QKQ1.200114.002/V12.0.1.0.QCOMI
 "
 
 # Contributors:
-# @emanresu2, @BlueMan_86, @sh3rry on GitHub, @Mr54233 on GitHub, @FivEawE, @oursgentil
+# @emanresu2, @BlueMan_86, @sh3rry on GitHub, @Mr54233 on GitHub, @FivEawE, @oursgentil, @GtrCraft
