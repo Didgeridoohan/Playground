a4506eb8f270b05dbcb72d8dfa3b082d698bad5c
Didgeridoohan <github@heptadecagon.one>
2021-01-12 22:13:47 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Fairphone 3/3 Plus (10):Fairphone:FP3=Fairphone/FP3/FP3:10/8901.3.A.0054.20200929/09290922:user/release-keys__2020-10-05
 "
 
 # Contributors:
-# 
+# @tsteeples
