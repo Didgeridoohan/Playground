f4a0d042310fac62e72c0ddb8530ee0c826529f7
Didgeridoohan <github@heptadecagon.one>
2021-01-16 16:55:12 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,9 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Mi 10 Lite (10)=Xiaomi/gauguin_eea/gauguin:10/QKQ1.200628.002/V12.0.8.0.QJSEUXM:user/release-keys__
+Xiaomi Pocophone F1 (10)=Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys__2020-12-01
 "
 
 # Contributors:
-# 
+# @Sid220, @der_Kief
