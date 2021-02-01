fd084c4fede389f54fc736ba01b036c857182b14
Didgeridoohan <github@heptadecagon.one>
2021-02-01 18:23:51 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -11,7 +11,8 @@ PRINTSLIST="
 Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.20.0.QDIMIXM:user/release-keys__2021-01-01
 Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.17.0.QDLMIXM:user/release-keys__2021-01-01
 Xiaomi Mi A3 (11)=Xiaomi/laurel_sprout/laurel_sprout:11/RKQ1.200903.002/V12.0.4.0.RFQMIXM:user/release-keys__2021-01-05
+Xiaomi Redmi Note 8 Pro Europe (10)=xiaomi/willow_eea/willow:10/QKQ1.200114.002/V12.0.1.0.QCXEUXM:user/release-keys__2020-10-01
 "
 
 # Contributors:
-# @Displax
+# @Displax, @olivercervera
