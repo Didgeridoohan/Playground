a08055b0cbdf98104771bc63e4d6bbfa72462715
Didgeridoohan <github@heptadecagon.one>
2021-02-02 08:47:36 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -17,7 +17,6 @@ Google Pixel 4 XL=google/coral/coral:11/RQ1A.210205.004/7038034:user/release-key
 Google Pixel 4a=google/sunfish/sunfish:11/RQ1A.210205.004/7038034:user/release-keys__2021-02-05
 Google Pixel 4a (5G)=google/bramble/bramble:11/RQ1A.210205.004/7038034:user/release-keys__2021-02-05
 Google Pixel 5=google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys__2021-02-05
-Motorola Moto Z3 Play (9)=motorola/beckham/beckham:9/PPWS29.131-27-1-27/34b6d:user/release-keys__2018-08-05
 Samsung Galaxy A7 2018 SM-A750GN (10)=samsung/a7y18ltedx/a7y18lte:10/QP1A.190711.020/A750GNDXU6CTK1:user/release-keys__2020-12-01
 Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.20.0.QDIMIXM:user/release-keys__2021-01-01
 Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.17.0.QDLMIXM:user/release-keys__2021-01-01
@@ -26,4 +25,4 @@ Xiaomi Redmi Note 8 Pro Europe (10)=xiaomi/willow_eea/willow:10/QKQ1.200114.002/
 "
 
 # Contributors:
-# @Displax, @olivercervera, @K-218, @YamazakiRobert
+# @Displax, @olivercervera, @K-218
