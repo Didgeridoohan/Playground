0cc5939bfa2d3eef09d24684698816c1b3a91f28
Didgeridoohan <github@heptadecagon.one>
2021-02-02 08:15:40 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,17 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Google Pixel 3=google/blueline/blueline:11/RQ1A.210205.004/7038034:user/release-keys__2021-02-05
+Google Pixel 3 XL=google/crosshatch/crosshatch:11/RQ1A.210205.004/7038034:user/release-keys__2021-02-05
+Google Pixel 3a=google/sargo/sargo:11/RQ1A.210205.004/7038034:user/release-keys__2021-02-05
+Google Pixel 3a XL=google/bonito/bonito:11/RQ1A.210205.004/7038034:user/release-keys__2021-02-05
+Google Pixel 4=google/flame/flame:11/RQ1A.210205.004/7038034:user/release-keys__2021-02-05
+Google Pixel 4 XL=google/coral/coral:11/RQ1A.210205.004/7038034:user/release-keys__2021-02-05
+Google Pixel 4a=google/sunfish/sunfish:11/RQ1A.210205.004/7038034:user/release-keys__2021-02-05
+Google Pixel 4a (5G)=google/bramble/bramble:11/RQ1A.210205.004/7038034:user/release-keys__2021-02-05
+Google Pixel 5=google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys__2021-02-05
+Motorola Moto Z3 Play (9)=motorola/beckham/beckham:9/PPWS29.131-27-1-27/34b6d:user/release-keys__2018-08-05
+Samsung Galaxy A7 2018 SM-A750GN (10)=samsung/a7y18ltedx/a7y18lte:10/QP1A.190711.020/A750GNDXU6CTK1:user/release-keys__2020-12-01
 Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.20.0.QDIMIXM:user/release-keys__2021-01-01
 Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.17.0.QDLMIXM:user/release-keys__2021-01-01
 Xiaomi Mi A3 (11)=Xiaomi/laurel_sprout/laurel_sprout:11/RKQ1.200903.002/V12.0.4.0.RFQMIXM:user/release-keys__2021-01-05
@@ -15,4 +26,4 @@ Xiaomi Redmi Note 8 Pro Europe (10)=xiaomi/willow_eea/willow:10/QKQ1.200114.002/
 "
 
 # Contributors:
-# @Displax, @olivercervera
+# @Displax, @olivercervera, @K-218, @YamazakiRobert
