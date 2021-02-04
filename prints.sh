25cceb0f3010ff4eeb528d4fbd20eea468428935
Didgeridoohan <github@heptadecagon.one>
2021-02-04 09:40:10 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,9 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Mi 8 Pro Global (10)=Xiaomi/equuleus/equuleus:10/QKQ1.190828.002/V11.0.5.0.QECMIXM:user/release-keys__2020-04-01
+Xiaomi Mi 8 Pro Russia (10)=Xiaomi/equuleus_ru/equuleus:10/QKQ1.190828.002/V11.0.3.0.QECRUXM:user/release-keys__2020-04-01
 "
 
 # Contributors:
-# 
+# @gallgamer
