4d6d6bc24eb0662a841f1fc81f213c09618c035b
Didgeridoohan <github@heptadecagon.one>
2021-02-07 14:40:08 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,10 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-OPPO A53s (10):OPPO:CPH2135=OPPO/CPH2135EEA/OP4EFDL1:10/QKQ1.200614.002/1605790610:user/release-keys__2020-11-05
-Xiaomi Mi 8 Pro Global (10):Xiaomi:Mi 8 Pro=Xiaomi/equuleus/equuleus:10/QKQ1.190828.002/V11.0.5.0.QECMIXM:user/release-keys__2020-04-01
-Xiaomi Mi 8 Pro Russia (10):Xiaomi:Mi 8 Pro=Xiaomi/equuleus_ru/equuleus:10/QKQ1.190828.002/V11.0.3.0.QECRUXM:user/release-keys__2020-04-01
 "
 
 # Contributors:
-# @gallgamer, @amplatfus
+# 
