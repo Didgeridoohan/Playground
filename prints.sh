6784c6d6538e686a54fa596f6e9fd25d78828afe
Didgeridoohan <github@heptadecagon.one>
2021-02-22 14:24:27 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,13 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Samsung Galaxy S9 SM-G960F (10):samsung:SM-G960F=samsung/starltexx/starlte:10/QP1A.190711.020/G960FXXSDFTL1:user/release-keys__2021-01-01
-Sony Xperia 10 (10):Sony:I4193=Sony/I4193/I4193:10/53.1.A.3.85/053001A003008502135191194:user/release-keys__2020-11-01
-Xiaomi Mi 9T Pro/Redmi K20 Pro Europe (10)=Xiaomi/raphael_eea/raphael:10/QKQ1.190825.002/V12.0.4.0.QFKEUXM:user/release-keys__2021-01-01
-Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.18.0.QDLMIXM:user/release-keys__2021-02-01
-Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.21.0.QDIMIXM:user/release-keys__2021-02-01
-Xiaomi Mi A3 (11)=Xiaomi/laurel_sprout/laurel_sprout:11/RKQ1.200903.002/V12.0.6.0.RFQMIXM:user/release-keys__2021-02-05
 "
 
 # Contributors:
-# @Displax, @jarton, @Marocco2 on GitHub, @Xanth0k1d
+# 
