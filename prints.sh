0c0c77578c5ed001f5cc010e1edc66ea6e9fbb91
Didgeridoohan <github@heptadecagon.one>
2021-02-28 06:59:19 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -13,6 +13,7 @@ Nvidia Shield TV 2015 Pro (9)=NVIDIA/foster_e_hdd/foster:9/PPR1.180610.011/40792
 Nvidia Shield TV 2017 (9)=NVIDIA/darcy/darcy:9/PPR1.180610.011/4079208_2513.0256:user/release-keys__2020-12-05
 Nvidia Shield TV 2019 (9)=NVIDIA/sif/sif:9/PPR1.180610.011/4079180_2513.0256:user/release-keys__2020-12-05
 Nvidia Shield TV 2019 Pro (9)=NVIDIA/mdarcy/mdarcy:9/PPR1.180610.011/4079208_2513.0256:user/release-keys__2020-12-05
+Xiaomi Mi A3 Europe (9 & 10 & 11):Xiaomi:Mi A3=Xiaomi/laurel_sprout_eea/laurel_sprout:9/PKQ1.190416.001/V10.3.17.0.PFQEUXM:user/release-keys__2020-03-05;Xiaomi/laurel_sprout_eea/laurel_sprout:10/QKQ1.190910.002/V11.0.6.0.QFQEUXM:user/release-keys__2020-08-05;Xiaomi/laurel_sprout_eea/laurel_sprout:11/RKQ1.200903.002/V12.0.3.0.RFQEUXM:user/release-keys__2021-01-05
 "
 
 # Contributors:
