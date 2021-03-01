4a81bf1816f4a58de683ab3d46a17ed15259a197
Didgeridoohan <github@heptadecagon.one>
2021-03-01 22:57:10 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -13,8 +13,9 @@ Nvidia Shield TV 2015 Pro (9)=NVIDIA/foster_e_hdd/foster:9/PPR1.180610.011/40792
 Nvidia Shield TV 2017 (9)=NVIDIA/darcy/darcy:9/PPR1.180610.011/4079208_2513.0256:user/release-keys__2020-12-05
 Nvidia Shield TV 2019 (9)=NVIDIA/sif/sif:9/PPR1.180610.011/4079180_2513.0256:user/release-keys__2020-12-05
 Nvidia Shield TV 2019 Pro (9)=NVIDIA/mdarcy/mdarcy:9/PPR1.180610.011/4079208_2513.0256:user/release-keys__2020-12-05
+Redmi Note 8 Pro Europe (10)=Redmi/begonia_eea/begonia:10/QP1A.190711.020/V12.0.4.0.QGGEUXM:user/release-keys__2021-02-01
 Xiaomi Redmi 9 China (10):Xiaomi:M2004J19AG=Redmi/lancelot/lancelot:10/QP1A.190711.020/V12.0.6.0.QJCCNXM:user/release-keys__2020-11-05
 "
 
 # Contributors:
-# @Displax, @marat2509
+# @Displax, @marat2509, @FivEawE
