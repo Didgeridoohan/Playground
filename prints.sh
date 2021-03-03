a0505f559cc9b18c704b634e5c7edf0ea0cc21fd
Didgeridoohan <github@heptadecagon.one>
2021-03-03 23:12:17 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -10,7 +10,7 @@ PRINTSTRANSF=1
 PRINTSLIST="
 Nvidia Shield Tablet LTE (7.0):NVIDIA:SHIELD Tablet=nvidia/wx_un_do/shieldtablet:7.0/NRD90M/1928188_1065.2559:user/release-keys
 Nvidia Shield Tablet WiFi (7.0):NVIDIA:SHIELD Tablet=nvidia/wx_na_wf/shieldtablet:7.0/NRD90M/1928188_1065.2559:user/release-keys
-Razer Phone 2 (9)=razer/cheryl2/aura:9/P-SMR6-RC001-RZR-201022/3201:user/release-keys_2020-11-05
+Razer Phone 2 (9)=razer/cheryl2/aura:9/P-SMR7-RC003-RZR-210107/3225:user/release-keys__2021-02-05
 Samsung Galaxy A5 2016 SM-A510F (7.0):samsung:SM-A510F=samsung/a5xeltexx/a5xelte:7.0/NRD90M/A510FXXS8CSF3:user/release-keys__2019-07-01
 "
 
