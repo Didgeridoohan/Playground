38087ce21c17cb54e50fa9657deceb066b90b2eb
Didgeridoohan <github@heptadecagon.one>
2021-03-03 06:45:00 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,10 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Nvidia Shield Tablet LTE (7.0):NVIDIA:SHIELD Tablet=nvidia/wx_un_do/shieldtablet:7.0/NRD90M/1928188_1065.2559:user/release-keys
+Nvidia Shield Tablet WiFi (7.0):NVIDIA:SHIELD Tablet=nvidia/wx_na_wf/shieldtablet:7.0/NRD90M/1928188_1065.2559:user/release-keys
+Razer Phone 2 (9)=razer/cheryl2/aura:9/P-SMR6-RC001-RZR-201022/3201:user/release-keys_2020-11-05
 "
 
 # Contributors:
-# 
+# @Displax
