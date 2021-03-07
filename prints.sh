a630723e69ab8a868338f711f408e749ef3d6a2f
Didgeridoohan <github@heptadecagon.one>
2021-03-07 07:04:11 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -13,8 +13,9 @@ Nvidia Shield Tablet LTE (7.0):NVIDIA:SHIELD Tablet=nvidia/wx_un_do/shieldtablet
 Nvidia Shield Tablet WiFi (7.0):NVIDIA:SHIELD Tablet=nvidia/wx_na_wf/shieldtablet:7.0/NRD90M/1928188_1065.2559:user/release-keys
 Razer Phone 2 (9)=razer/cheryl2/aura:9/P-SMR7-RC003-RZR-210107/3225:user/release-keys__2021-02-05
 Samsung Galaxy A5 2016 SM-A510F (7.0):samsung:SM-A510F=samsung/a5xeltexx/a5xelte:7.0/NRD90M/A510FXXS8CSF3:user/release-keys__2019-07-01
+Samsung Galaxy A7 2018 SM-A750GN (10)=samsung/a7y18ltedx/a7y18lte:10/QP1A.190711.020/A750GNDXU6CUA1:user/release-keys__2021-01-01
 Xiaomi Mi 11 (11)=Xiaomi/venus/venus:11/RKQ1.200928.002/21.3.1:user/release-keys__2021-02-01
 "
 
 # Contributors:
-# @Displax, @glothatik, @T1tino, @johnyu2003
+# @Displax, @glothatik, @T1tino, @johnyu2003, @K-218
