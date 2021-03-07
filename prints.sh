445bed15247ae3fb994d62d6d9018c2036bcb135
Didgeridoohan <github@heptadecagon.one>
2021-03-07 20:43:33 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,14 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-LG K20 Plus (7.0)=MetroPCS/lv517_mpcs_us/lv517:7.0/NRD90U/1833409570571:user/release-keys__2018-12-01
-Nvidia Shield Tablet LTE (7.0):NVIDIA:SHIELD Tablet=nvidia/wx_un_do/shieldtablet:7.0/NRD90M/1928188_1065.2559:user/release-keys
-Nvidia Shield Tablet WiFi (7.0):NVIDIA:SHIELD Tablet=nvidia/wx_na_wf/shieldtablet:7.0/NRD90M/1928188_1065.2559:user/release-keys
-Razer Phone 2 (9)=razer/cheryl2/aura:9/P-SMR7-RC003-RZR-210107/3225:user/release-keys__2021-02-05
-Samsung Galaxy A5 2016 SM-A510F (7.0):samsung:SM-A510F=samsung/a5xeltexx/a5xelte:7.0/NRD90M/A510FXXS8CSF3:user/release-keys__2019-07-01
-Samsung Galaxy A7 2018 SM-A750GN (10)=samsung/a7y18ltedx/a7y18lte:10/QP1A.190711.020/A750GNDXU6CUA1:user/release-keys__2021-01-01
-Xiaomi Mi 11 (11):Xiaomi:M2011K2C=ro.odm.build.fingerprint=Xiaomi/venus/venus:11/RKQ1.200928.002/V12.0.12.0.RKBCNXM:user/release-keys__2020-12-01
 "
 
 # Contributors:
-# @Displax, @glothatik, @T1tino, @johnyu2003, @K-218
+# 
