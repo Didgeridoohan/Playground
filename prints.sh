a4f2f1fee220c6d8dd1de5dc1deaeccac1fdadcb
Didgeridoohan <github@heptadecagon.one>
2021-03-07 21:59:34 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,8 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Xiaomi Mi 11 (11)=Xiaomi/venus/venus:11/RKQ1.200928.002/V12.0.22.0.RKBCNXM:user/release-keys__2021-01-01
 "
 
 # Contributors:
-# @johnyu2003
+# 
