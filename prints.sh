5094becb40d25724aba5c230679ee9e725f23dbe
Didgeridoohan <github@heptadecagon.one>
2021-03-10 11:03:29 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Motorola Moto G6 XT1925-10 (9):Motorola:moto g(6)=motorola/ali/ali:9/PPSS29.55-37-7-6/16c5a7:user/release-keys__2020-02-01
+Motorola Moto G6 (9):Motorola:moto g(6)=motorola/ali/ali:9/PPSS29.55-37-7-6/16c5a7:user/release-keys__2020-02-01
 Redmi Note 9 Pro Max India (10):Redmi:Redmi Note 9 Pro Max=Redmi/excalibur_in/excalibur:10/QKQ1.191215.002/V12.0.3.0.QJXINXM:user/release-keys__2021-02-01
 "
 
