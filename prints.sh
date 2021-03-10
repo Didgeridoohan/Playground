901d219a74e390ff648e12fd681a956c7a0e854c
Didgeridoohan <github@heptadecagon.one>
2021-03-10 07:05:01 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Motorola Moto G6 XT1925-10 (9):Motorola:moto g(6)=motorola/ali/ali:9/PPSS29.55-37-7-6/16c5a7:user/release-keys__2020-02-01
 "
 
 # Contributors:
-# 
+# @Ema_Prowler
