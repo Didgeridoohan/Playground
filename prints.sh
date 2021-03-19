f0e605d58342022ba50bd8316b499f989788b98d
Didgeridoohan <github@heptadecagon.one>
2021-03-19 21:16:03 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,48 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Motorola Moto G6 (9):Motorola:moto g(6)=
-OnePlus 6 (10)=
-OnePlus 6T (10)=
-OnePlus 7 (10)=
-OnePlus 7 (10)=
-OnePlus 7 Pro (10)=
-OnePlus 7 Pro (10)=
-OnePlus 7 Pro NR (10)=
-OnePlus 7T (10)=
-OnePlus 7T (10)=
-OnePlus 7T Pro (10)=
-OnePlus 7T Pro (10)=
-OnePlus 8 (11)=
-OnePlus 8 (11)=
-OnePlus 8 (11)=
-OnePlus 8 (11)=
-OnePlus 8 Pro (11)=
-OnePlus 8 Pro (11)=
-OnePlus 8 Pro (11)=
-OnePlus 8T (11)=
-OnePlus 8T (11)=
-OnePlus 8T (11)=
-OnePlus 8T (11)=
-OnePlus Nord (10)=
-OnePlus Nord (11)=
-OnePlus Nord (10)=
-OnePlus Nord (11)=
-OnePlus Nord (10)=
-OnePlus Nord (11)=
-OnePlus N10 (10)=
-OnePlus N10 (10)=
-OnePlus N10 (10)=OnePlus/OnePlusN10METRO/OnePlusN10METRO:10/QKQ1.200830.002/2012181933:user/release-keys__2020-12-05
-OnePlus N10 (10)=OnePlus/OnePlusN10TMO/OnePlusN10TMO:10/QKQ1.200830.002/2012181933:user/release-keys__2020-12-05
-OnePlus N100 (10)=
-OnePlus N100 (10)=
-OnePlus N100 (10)=OnePlus/OnePlusN100METRO/OnePlusN100METRO:10/QKQ1.200719.002/2012212113:user/release-keys__2020-12-05
-OnePlus N100 (10)=OnePlus/OnePlusN100TMO/OnePlusN100TMO:10/QKQ1.200719.002/2012212112:user/release-keys__2020-12-05
-Redmi Note 9 Pro Max India (10):Redmi:Redmi Note 9 Pro Max=Redmi/excalibur_in/excalibur:10/QKQ1.191215.002/V12.0.3.0.QJXINXM:user/release-keys__2021-02-01
-Samsung Galaxy Tab 7 WiFi SM-T970 (11):Samsung:SM-T970=samsung/gts7xlwifixx/gts7xlwifi:11/RP1A.200720.012/T970XXU1BUAA:user/release-keys__2021-01-01
-Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.22.0.QDIMIXM:user/release-keys__2021-03-01
-Xiaomi Mi A3 (11)=Xiaomi/laurel_sprout/laurel_sprout:11/RKQ1.200903.002/V12.0.7.0.RFQMIXM:user/release-keys__2021-03-05
 "
 
 # Contributors:
-# @Ema_Prowler, @DevRiturajKumar on GitHub, @Some_Random_Username, @a5782033 on GitHub, @Displax
+# 
