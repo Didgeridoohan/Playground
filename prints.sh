39d9a14ed68b34c3432640e55a276cb5cd4064b7
Didgeridoohan <github@heptadecagon.one>
2021-03-21 20:43:22 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,14 +8,22 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Samsung Galaxy S10e - SM-G970F (11)=samsung/beyond0lteeea/beyond0:11/RP1A.200720.012/G970FXXU9FUBD:user/release-keys_2021_03_01
-Samsung Galaxy S10e - SM-G970N (11)=samsung/beyond0lteeea/beyond0:11/RP1A.200720.012/G970NKSU5FUBD:user/release-keys_2021_03_01
-Samsung Galaxy S10 - SM-G973F (11)=samsung/beyond1lteeea/beyond1:11/RP1A.200720.012/G973FXXU9FUBD:user/release-keys_2021_03_01
-Samsung Galaxy S10 - SM-G973N (11)=samsung/beyond1lteeea/beyond1:11/RP1A.200720.012/G973NKSU5FUBD:user/release-keys_2021_03_01
-Samsung Galaxy S10 Plus - SM-G975F (11)=samsung/beyond2lteeea/beyond2:11/RP1A.200720.012/G975FXXU9FUBD:user/release-keys_2021_03_01
-Samsung Galaxy S10 Plus - SM-G975N (11)=samsung/beyond2lteeea/beyond2:11/RP1A.200720.012/G975NKSU5FUBD:user/release-keys_2021_03_01
-Samsung Galaxy S10 5G - SM-G977B (11)=samsung/beyondxeea/beyondx:11/RP1A.200720.012/G977BXXU6FUBD:user/release-keys_2021_03_01
-Samsung Galaxy S10 5G - SM-G977N (11)=samsung/beyondxeea/beyondx:11/RP1A.200720.012/G977NKSU4FUBD:user/release-keys_2021_03_01
+Samsung Galaxy S10e - SM-G970F (11)=samsung/beyond0ltexx/beyond0:11/RP1A.200720.012/G970FXXU9FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10e - SM-G970F (11)=samsung/beyond0lteeea/beyond0:11/RP1A.200720.012/G970FXXU9FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10e - SM-G970N (11)=samsung/beyond0lteks/beyond0:11/RP1A.200720.012/G970NKSU5FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10e - SM-G970N (11)=samsung/beyond0lteeea/beyond0:11/RP1A.200720.012/G970NKSU5FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10 - SM-G973F (11)=samsung/beyond1ltexx/beyond1:11/RP1A.200720.012/G973FXXU9FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10 - SM-G973F (11)=samsung/beyond1lteeea/beyond1:11/RP1A.200720.012/G973FXXU9FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10 - SM-G973N (11)=samsung/beyond1lteks/beyond1:11/RP1A.200720.012/G973NKSU5FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10 - SM-G973N (11)=samsung/beyond1lteeea/beyond1:11/RP1A.200720.012/G973NKSU5FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10 Plus - SM-G975F (11)=samsung/beyond2ltexx/beyond2:11/RP1A.200720.012/G975FXXU9FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10 Plus - SM-G975F (11)=samsung/beyond2lteeea/beyond2:11/RP1A.200720.012/G975FXXU9FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10 Plus - SM-G975N (11)=samsung/beyond2lteks/beyond2:11/RP1A.200720.012/G975NKSU5FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10 Plus - SM-G975N (11)=samsung/beyond2lteeea/beyond2:11/RP1A.200720.012/G975NKSU5FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10 5G - SM-G977B (11)=samsung/beyondxxx/beyondx:11/RP1A.200720.012/G977BXXU6FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10 5G - SM-G977B (11)=samsung/beyondxeea/beyondx:11/RP1A.200720.012/G977BXXU6FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10 5G - SM-G977N (11)=samsung/beyondxks/beyondx:11/RP1A.200720.012/G977NKSU4FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10 5G - SM-G977N (11)=samsung/beyondxeea/beyondx:11/RP1A.200720.012/G977NKSU4FUBD:user/release-keys__2021_03_01
 "
 
 # Contributors:
