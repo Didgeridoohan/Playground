3fcce490e749a4d521fc2a0fecfb74411dca2827
Didgeridoohan <github@heptadecagon.one>
2021-03-21 14:29:37 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Redmi 6 Pro (9)=xiaomi/sakura/sakura:9/PKQ1.180917.001/V12.0.1.0.PDICNXM:user/release-keys__2020-07-05
 "
 
 # Contributors:
-# 
+# @OFShuvo
