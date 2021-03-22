4d3b9291ee71829736191e3405f15ae36aa948aa
Didgeridoohan <github@heptadecagon.one>
2021-03-22 09:37:16 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,10 +8,11 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Samsung Galaxy S10e - SM-G970F (11)=samsung/beyond0ltexx/beyond0:11/RP1A.200720.012/G970FXXU9FUBD:user/release-keys__2021_03_05
-Samsung Galaxy S10e - SM-G970F (11)=samsung/beyond0lteeea/beyond0:11/RP1A.200720.012/G970FXXU9FUBD:user/release-keys__2021-03-05
+Samsung Galaxy S10 - SM-G973F (11)=samsung/beyond1lteeea/beyond1:11/RP1A.200720.012/G973FXXU9FUBD:user/release-keys__2021_03_01
+Samsung Galaxy S10 Plus SM-G975F (11)=samsung/beyond2lteeea/beyond2:11/RP1A.200720.012/G975FXXU9FUBD:user/release-keys__2021-03-01
+Samsung Galaxy S10e - SM-G970F (11)=samsung/beyond0lteeea/beyond0:11/RP1A.200720.012/G970FXXU9FUBD:user/release-keys__2021-03-01
 Xiaomi Redmi 6 Pro (9)=xiaomi/sakura/sakura:9/PKQ1.180917.001/V12.0.1.0.PDICNXM:user/release-keys__2020-07-01
 "
 
 # Contributors:
-# @OFShuvo
+# @OFShuvo, @DjLeleX
