b14d8577304480bf4322950880b9d2f787daa342
Didgeridoohan <github@heptadecagon.one>
2021-03-22 11:55:32 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Blu Vivo XI (8.1.0)=BLU/BLU_VIVO_XI/V0330WW:8.1.0/O11019/1543905757:user/release-keys__2018-12-05
+BLU Vivo XI (8.1.0):BLU:BLU_VIVO_XI=BLU/BLU_VIVO_XI/V0330WW:8.1.0/O11019/1543905757:user/release-keys__2018-12-05
 Samsung Galaxy S10 - SM-G973F (11)=samsung/beyond1lteeea/beyond1:11/RP1A.200720.012/G973FXXU9FUBD:user/release-keys__2021_03_01
 Samsung Galaxy S10 Plus SM-G975F (11)=samsung/beyond2lteeea/beyond2:11/RP1A.200720.012/G975FXXU9FUBD:user/release-keys__2021-03-01
 Samsung Galaxy S10e - SM-G970F (11)=samsung/beyond0lteeea/beyond0:11/RP1A.200720.012/G970FXXU9FUBD:user/release-keys__2021-03-01
