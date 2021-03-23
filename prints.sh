1b5a859bc7852e7e7438938b58fe7e5df460f233
Didgeridoohan <github@heptadecagon.one>
2021-03-23 15:57:40 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -12,8 +12,9 @@ BLU Vivo XI (8.1.0):BLU:BLU_VIVO_XI=BLU/BLU_VIVO_XI/V0330WW:8.1.0/O11019/1543905
 Samsung Galaxy S10 - SM-G973F (11)=samsung/beyond1lteeea/beyond1:11/RP1A.200720.012/G973FXXU9FUBD:user/release-keys__2021_03_01
 Samsung Galaxy S10 Plus SM-G975F (11)=samsung/beyond2lteeea/beyond2:11/RP1A.200720.012/G975FXXU9FUBD:user/release-keys__2021-03-01
 Samsung Galaxy S10e - SM-G970F (11)=samsung/beyond0lteeea/beyond0:11/RP1A.200720.012/G970FXXU9FUBD:user/release-keys__2021-03-01
+Xiaomi Mi 9 China (9 & 10):Xiaomi:MI 9=Xiaomi/cepheus/cepheus:9/PKQ1.181121.001/V10.2.35.0.PFACNXM:user/release-keys__2018-08-05;Xiaomi/cepheus/cepheus:10/QKQ1.190825.002/V12.0.6.0.QFACNXM:user/release-keys__2020-11-01
 Xiaomi Redmi 6 Pro (9)=xiaomi/sakura/sakura:9/PKQ1.180917.001/V12.0.1.0.PDICNXM:user/release-keys__2020-07-01
 "
 
 # Contributors:
-# @OFShuvo, @DjLeleX, @ineedroot69
+# @OFShuvo, @DjLeleX, @ineedroot69, @Howard20181 at Github
