c8d95ac7840b2f9e29c0777fce50bdc085b63b60
Didgeridoohan <github@heptadecagon.one>
2021-04-03 23:48:40 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,25 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-BLU Vivo XI (8.1.0):BLU:BLU_VIVO_XI=BLU/BLU_VIVO_XI/V0330WW:8.1.0/O11019/1543905757:user/release-keys__2018-12-05
-Motorola Moto Z2 Force T-Mobile (8.0.0):Motorola:Moto Z2=motorola/nash_tmo_c/nash:8.0.0/OCXS27.109-51-14-7/12:user/release-keys__2019-07-01
-Redmi 9T Global (10):Redmi:M2010J19SI=Redmi/lime_global/lime:10/QKQ1.200830.002/V12.0.7.0.QJQMIXM:user/release-keys__2021-03-01
-Samsung Galaxy S10 SM-G973F (11):Samsung:SM-G973F=samsung/beyond1ltexx/beyond1:11/RP1A.200720.012/G973FXXU9FUBD:user/release-keys__2021-03-01
-Samsung Galaxy S10 SM-G973F Europe (11):Samsung:SM-G973F=samsung/beyond1lteeea/beyond1:11/RP1A.200720.012/G973FXXU9FUBD:user/release-keys__2021-03-01
-Samsung Galaxy S10 SM-G973N (11):Samsung:SM-G973N=samsung/beyond1lteks/beyond1:11/RP1A.200720.012/G973NKSU5FUBD:user/release-keys__2021-03-01
-Samsung Galaxy S10 5G SM-G977B (11):Samsung:SM-G977B=samsung/beyondxxx/beyondx:11/RP1A.200720.012/G977BXXU6FUBD:user/release-keys__2021-03-01
-Samsung Galaxy S10 5G SM-G977N (11):Samsung:SM-G977N=samsung/beyondxks/beyondx:11/RP1A.200720.012/G977NKSU4FUBD:user/release-keys__2021-03-01
-Samsung Galaxy S10 Plus SM-G975F (11):Samsung:SM-G975F=samsung/beyond2ltexx/beyond2:11/RP1A.200720.012/G975FXXU9FUBD:user/release-keys__2021-03-01
-Samsung Galaxy S10 Plus SM-G975F Europe (11):Samsung:SM-G975F=samsung/beyond2lteeea/beyond2:11/RP1A.200720.012/G975FXXU9FUBD:user/release-keys__2021-03-01
-Samsung Galaxy S10 Plus SM-G975N (11):Samsung:SM-G975N=samsung/beyond2lteks/beyond2:11/RP1A.200720.012/G975NKSU5FUBD:user/release-keys__2021-03-01
-Samsung Galaxy S10e SM-G970F (11):Samsung:SM-G970F=samsung/beyond0ltexx/beyond0:11/RP1A.200720.012/G970FXXU9FUBD:user/release-keys__2021-03-01
-Samsung Galaxy S10e SM-G970F Europe (11):Samsung:SM-G970F=samsung/beyond0lteeea/beyond0:11/RP1A.200720.012/G970FXXU9FUBD:user/release-keys__2021-03-01
-Samsung Galaxy S10e SM-G970N (11):Samsung:SM-G970N=samsung/beyond0lteks/beyond0:11/RP1A.200720.012/G970NKSU5FUBD:user/release-keys__2021-03-01
-Xiaomi Mi 9 China (10):Xiaomi:MI 9=Xiaomi/cepheus/cepheus:10/QKQ1.190825.002/V12.0.6.0.QFACNXM:user/release-keys__2020-11-01
-Xiaomi Mi A3 Europe (11)=Xiaomi/laurel_sprout_eea/laurel_sprout:11/RKQ1.200903.002/V12.0.3.0.RFQEUXM:user/release-keys__2021-01-05
-Xiaomi Redmi 6 Pro China (9):Xiaomi:Redmi 6 Pro=xiaomi/sakura/sakura:9/PKQ1.180917.001/V12.0.1.0.PDICNXM:user/release-keys__2020-07-01
-Xiaomi Redmi 6 Pro India (9):Xiaomi:Redmi 6 Pro=xiaomi/sakura_india/sakura_india:9/PKQ1.180917.001/V12.0.4.0.PDMMIXM:user/release-keys__2020-09-01
 "
 
 # Contributors:
-# @OFShuvo, @DjLeleX, @ineedroot69, @Howard20181 at Github, @XirXes, @ipdev, @firetablethelp
+# 
