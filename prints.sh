caefe4b5e69c85586cf37ebd40a7ea586200caa0
Didgeridoohan <github@heptadecagon.one>
2021-04-03 09:51:34 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -9,12 +9,16 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 BLU Vivo XI (8.1.0):BLU:BLU_VIVO_XI=BLU/BLU_VIVO_XI/V0330WW:8.1.0/O11019/1543905757:user/release-keys__2018-12-05
+Motorola Noto Z2 Force (8.0.0)=motorola/nash_tmo_c/nash:8.0.0/OCXS27.109-51-14-7/12:user/release-keys__2019-07-01
+Redmi 9T (10)=Redmi/lime_global/lime:10/QKQ1.200830.002/V12.0.7.0.QJQMIXM:user/release-keys__2021-03-01
 Samsung Galaxy S10 - SM-G973F (11)=samsung/beyond1lteeea/beyond1:11/RP1A.200720.012/G973FXXU9FUBD:user/release-keys__2021_03_01
 Samsung Galaxy S10 Plus SM-G975F (11)=samsung/beyond2lteeea/beyond2:11/RP1A.200720.012/G975FXXU9FUBD:user/release-keys__2021-03-01
 Samsung Galaxy S10e - SM-G970F (11)=samsung/beyond0lteeea/beyond0:11/RP1A.200720.012/G970FXXU9FUBD:user/release-keys__2021-03-01
 Xiaomi Mi 9 China (9 & 10):Xiaomi:MI 9=Xiaomi/cepheus/cepheus:9/PKQ1.181121.001/V10.2.35.0.PFACNXM:user/release-keys__2018-08-05;Xiaomi/cepheus/cepheus:10/QKQ1.190825.002/V12.0.6.0.QFACNXM:user/release-keys__2020-11-01
-Xiaomi Redmi 6 Pro (9)=xiaomi/sakura/sakura:9/PKQ1.180917.001/V12.0.1.0.PDICNXM:user/release-keys__2020-07-01
+Xiaomi Mi A3 Europe (11)=Xiaomi/laurel_sprout_eea/laurel_sprout:11/RKQ1.200903.002/V12.0.3.0.RFQEUXM:user/release-keys__2021-01-05
+Xiaomi Redmi 6 Pro China (9)=xiaomi/sakura/sakura:9/PKQ1.180917.001/V12.0.1.0.PDICNXM:user/release-keys__2020-07-01
+Xiaomi Redmi 6 Pro India (9):Xiaomi:Redmi 6 Pro=xiaomi/sakura_india/sakura_india:9/PKQ1.180917.001/V12.0.4.0.PDMMIXM:user/release-keys__2020-09-01
 "
 
 # Contributors:
-# @OFShuvo, @DjLeleX, @ineedroot69, @Howard20181 at Github
+# @OFShuvo, @DjLeleX, @ineedroot69, @Howard20181 at Github, @XirXes, @ipdev, @firetablethelp
