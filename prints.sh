9aabd3f6c65bb1f9a2d79f5f283905785c353334
Didgeridoohan <github@heptadecagon.one>
2021-04-04 09:15:20 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Mi 10 Ultra (11)=Xiaomi/cas/cas:11/RKQ1.200826.002/V12.5.1.0.RJJCNXM:user/release-keys__2021-03-01
 "
 
 # Contributors:
-# 
+# @2B_4G10
