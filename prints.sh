0f1e8b7dff6d3cf32d64538c45ef3c8387e54259
Didgeridoohan <github@heptadecagon.one>
2021-04-07 21:04:31 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,9 +8,21 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+POCO X3 Pro Global (11):POCO:M2102J20SG=POCO/vayu_global/vayu:11/RKQ1.200826.002/V12.0.4.0.RJUMIXM:user/release-keys__2021-03-01
+Samsung Galaxy S10 SM-G973F (11)=samsung/beyond1ltexx/beyond1:11/RP1A.200720.012/G973FXXU9FUCD:user/release-keys__2021-04-01
+Samsung Galaxy S10 Europe SM-G973F (11)=samsung/beyond1lteeea/beyond1:11/RP1A.200720.012/G973FXXU9FUCD:user/release-keys__2021-04-01
+Samsung Galaxy S10 SM-G973N (11)=samsung/beyond1lteks/beyond1:11/RP1A.200720.012/G973NKSU5FUCD:user/release-keys__2021-04-01
+Samsung Galaxy S10 5G SM-G977B (11)=samsung/beyondxxx/beyondx:11/RP1A.200720.012/G977BXXU6FUCD:user/release-keys__2021-04-01
+Samsung Galaxy S10 5G SM-G977N (11)=samsung/beyondxks/beyondx:11/RP1A.200720.012/G977NKSU4FUCD:user/release-keys__2021-04-01
+Samsung Galaxy S10 Plus SM-G975F (11)=samsung/beyond2ltexx/beyond2:11/RP1A.200720.012/G975FXXU9FUCD:user/release-keys__2021-04-01
+Samsung Galaxy S10 Plus Europe SM-G975F (11)=samsung/beyond2lteeea/beyond2:11/RP1A.200720.012/G975FXXU9FUCD:user/release-keys__2021-04-01
+Samsung Galaxy S10 Plus SM-G975N (11)=samsung/beyond2lteks/beyond2:11/RP1A.200720.012/G975NKSU5FUCD:user/release-keys__2021-04-01
+Samsung Galaxy S10e SM-G970F (11)=samsung/beyond0ltexx/beyond0:11/RP1A.200720.012/G970FXXU9FUCD:user/release-keys__2021-04-01
+Samsung Galaxy S10e Europe SM-G970F (11)=samsung/beyond0lteeea/beyond0:11/RP1A.200720.012/G970FXXU9FUCD:user/release-keys__2021-04-01
+Samsung Galaxy S10e SM-G970N (11)=samsung/beyond0lteks/beyond0:11/RP1A.200720.012/G970NKSU5FUCD:user/release-keys__2021-04-01
 Xiaomi Mi 10 Ultra China (11)=Xiaomi/cas/cas:11/RKQ1.200826.002/V12.5.1.0.RJJCNXM:user/release-keys__2021-03-01
 Xiaomi Mi 10T Global (11)=Xiaomi/apollo_global/apollo_global:11/RKQ1.200826.002/V12.1.1.0.RJDMIXM:user/release-keys__2021-02-01
 "
 
 # Contributors:
-# @2B_4G10, @Sherbi
+# @2B_4G10, @Sherbi, @DjLeleX, @TheReduxPL
