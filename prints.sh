797d1669ecd52db70187ba7b782867757612d15b
Didgeridoohan <github@heptadecagon.one>
2021-04-11 18:02:30 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,8 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Mi 10S China (11)=Xiaomi/thyme/thyme:11/RKQ1.200826.002/V12.0.10.0.RGACNXM:user/release-keys__2021-03-01
 "
 
 # Contributors:
-# 
+# @liyiecho
