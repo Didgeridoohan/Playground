1f37d152af38e15a8d046f938c0c3b7ab4cd13d7
Didgeridoohan <github@heptadecagon.one>
2021-05-02 00:19:33 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,11 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Huawei P10 Lite (8.0.0)=HUAWEI/WAS-LX1A/HWWAS-H:8.0.0/HUAWEIWAS-LX1A/376(C02):user/release-keys__2019-03-01
-Motorola moto g stylus (10):motorola:moto g stylus=motorola/sofiap_retail/sofiap:10/QPRS30.80-58-7-5-1-2/1ec95d:user/release-keys__2021-01-01
-Samsung Galaxy Tab S5e (11):samsung:SM-T720=samsung/gts4lvwifixx/gts4lvwifi:11/RP1A.200720.012/T720XXU2DUD3:user/release-keys__2021_03_01
-Xiaomi Mi 10S China (11)=Xiaomi/thyme/thyme:11/RKQ1.200826.002/V12.0.10.0.RGACNXM:user/release-keys__2021-03-01
 "
 
 # Contributors:
-# @liyiecho, @ipdev, @korabio, @OhioYJ
+# 
