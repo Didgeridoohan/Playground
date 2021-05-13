1a758b52d644acf568effb2717f823672406628d
Didgeridoohan <github@heptadecagon.one>
2021-05-13 12:29:36 +0200

    testing

--- prints.sh
+++ prints.sh
@@ -8,7 +8,9 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Asus ROG Phone 5 ZS673KS (10):Asus:ZS673KS=asus/WW_I005D/ASUS_I005_1:11/RKQ1.201022.002/18.0840.2103.26-0:user/release-keys__2021-03-05
+Xiaomi Redmi Note 6 Pro (9)=xiaomi/tulip/tulip:9/PKQ1.180904.001/V12.0.1.0.PEKMIXM:user/release-keys__2020-11-05
 "
 
 # Contributors:
-# 
+# @qtr1st on Github, @Svg.Sakib
