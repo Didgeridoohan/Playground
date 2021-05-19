f162f4afb9ef25bd28cf40e2c8b5f82e4f893747
Didgeridoohan <github@heptadecagon.one>
2021-05-19 10:48:17 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -10,9 +10,8 @@ PRINTSTRANSF=1
 PRINTSLIST="
 Asus ROG Phone 5 ZS673KS (10):Asus:ZS673KS=asus/WW_I005D/ASUS_I005_1:11/RKQ1.201022.002/18.0840.2103.26-0:user/release-keys__2021-03-05
 POCO X3 NFC Europe (11)=POCO/surya_eea/surya:11/RKQ1.200826.002/V12.0.7.0.RJGEUXM:user/release-keys__2021-04-01
-POCO X3 Pro Global (11)=POCO/vayu_global/vayu:11/RKQ.200826.002/V12.0.6.0.RJUMIXM:user/release-keys__2021-04-01
 Xiaomi Redmi Note 6 Pro (9)=xiaomi/tulip/tulip:9/PKQ1.180904.001/V12.0.1.0.PEKMIXM:user/release-keys__2020-11-05
 "
 
 # Contributors:
-# @qtr1st on Github, @Svg.Sakib, @Peshyy, @AusVGM
+# @qtr1st on Github, @Svg.Sakib, @Peshyy
