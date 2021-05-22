ba7ef403238e652783a2f872cf0404f6b8c2eb76
Didgeridoohan <github@heptadecagon.one>
2021-05-22 16:18:47 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -11,7 +11,7 @@ PRINTSLIST="
 Asus ROG Phone 5 ZS673KS (10):Asus:ZS673KS=asus/WW_I005D/ASUS_I005_1:11/RKQ1.201022.002/18.0840.2103.26-0:user/release-keys__2021-03-05
 Huawei P20 Lite Single SIM (9):HUAWEI:ANE-LX1=HUAWEI/ANE-LX1/HWANE:9/HUAWEIANE-L01/9.1.0.368C432:user/release-keys__2020-08-01
 POCO X3 NFC Europe (11)=POCO/surya_eea/surya:11/RKQ1.200826.002/V12.0.7.0.RJGEUXM:user/release-keys__2021-04-01
-Samsung Tab S5e SM-T720 (11):samsung:SM-T720=samsung/gts4lvwifixx/gts4lvwifi:11/RP1A.200720.012/T720XXU2DUD3:user/release-keys__2021-03-01
+Samsung Galaxy Tab S5e SM-T720 (11):samsung:SM-T720=samsung/gts4lvwifixx/gts4lvwifi:11/RP1A.200720.012/T720XXU2DUD3:user/release-keys__2021-03-01
 Xiaomi Redmi Note 6 Pro (9)=xiaomi/tulip/tulip:9/PKQ1.180904.001/V12.0.1.0.PEKMIXM:user/release-keys__2020-11-05
 "
 
