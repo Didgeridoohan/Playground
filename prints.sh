ef8f5c68143b32b22224ddbba1117f1a5cd73568
Didgeridoohan <github@heptadecagon.one>
2021-05-22 23:12:44 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,19 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Asus ROG Phone 5 ZS673KS (10):Asus:ZS673KS=asus/WW_I005D/ASUS_I005_1:11/RKQ1.201022.002/18.0840.2103.26-0:user/release-keys__2021-03-05
-Huawei P20 Lite Single SIM (9):HUAWEI:ANE-LX1=HUAWEI/ANE-LX1/HWANE:9/HUAWEIANE-L01/9.1.0.368C432:user/release-keys__2020-08-01
-POCO X3 NFC Europe (11)=POCO/surya_eea/surya:11/RKQ1.200826.002/V12.0.7.0.RJGEUXM:user/release-keys__2021-04-01
-Samsung Galaxy Tab S5e SM-T720 (11):samsung:SM-T720=samsung/gts4lvwifixx/gts4lvwifi:11/RP1A.200720.012/T720XXU2DUD3:user/release-keys__2021-03-01
-Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.23.0.QDIMIXM:user/release-keys__2021-04-01
-Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.19.0.QDLMIXM:user/release-keys__2021-05-01
-Xiaomi Mi A3 Global (11)=Xiaomi/laurel_sprout/laurel_sprout:11/RKQ1.200903.002/V12.0.9.0.RFQMIXM:user/release-keys__2021-05-05
-Xiaomi Mi A3 Europe (11)=Xiaomi/laurel_sprout_eea/laurel_sprout:11/RKQ1.200903.002/V12.0.7.0.RFQEUXM:user/release-keys__2021-05-05
-Xiaomi Mi A3 Europe (10)=Xiaomi/laurel_sprout_eea/laurel_sprout:10/QKQ1.190910.002/V11.0.13.0.QFQEUXM:user/release-keys__2020-12-05
-Xiaomi Mi A3 Europe (9)=Xiaomi/laurel_sprout_eea/laurel_sprout:9/PKQ1.190416.001/V10.3.17.0.PFQEUXM:user/release-keys__2020-03-05
-
-Xiaomi Redmi Note 6 Pro (9)=xiaomi/tulip/tulip:9/PKQ1.180904.001/V12.0.1.0.PEKMIXM:user/release-keys__2020-11-05
 "
 
 # Contributors:
-# @qtr1st on Github, @Svg.Sakib, @Peshyy, @Frax3r, @OhioYJ, @Displax
+# 
