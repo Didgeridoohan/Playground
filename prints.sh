8ca2ca000dde5e7ef3aa5e61230f3cdbac2ab915
Didgeridoohan <github@heptadecagon.one>
2021-06-07 11:01:30 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -9,7 +9,6 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 OnePlus 7T India (11)=OnePlus/OnePlus7T/OnePlus7T:11/RKQ1.201022.002/2105071638:user/release-keys__2021-05-01
-POCO X3 NFC Global (10)=POCO/surya_global/surya:10/QKQ1.200512.002/V12.0.9.0JGMIXM:user/release-keys__2021-05-01
 Samsung Galaxy S21 Samsung SM-G991B (11):Samsung:SMG991B=samsung/o1sxxx/o1s:11/RP1A.200720.012/G991BXXU3AUE1:user/release-keys__2021-05-01
 Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.26.0.QDIMIXM:user/release-keys__2021-05-01
 "
