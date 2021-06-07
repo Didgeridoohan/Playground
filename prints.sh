305668e8f635040ffe891b8043e43ce6debb4047
Didgeridoohan <github@heptadecagon.one>
2021-06-07 21:30:20 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,10 +8,11 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+LG K20 V (7.0)=lge/lv517_vzw/lv517:7.0/NRD90U/1906714433ce5:user/release-keys__2019-03-01
 OnePlus 7T India (11)=OnePlus/OnePlus7T/OnePlus7T:11/RKQ1.201022.002/2105071638:user/release-keys__2021-05-01
 Samsung Galaxy S21 Samsung SM-G991B (11):Samsung:SMG991B=samsung/o1sxxx/o1s:11/RP1A.200720.012/G991BXXU3AUE1:user/release-keys__2021-05-01
 Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.26.0.QDIMIXM:user/release-keys__2021-05-01
 "
 
 # Contributors:
-# @CHT45, @Displax, @NOS_bzone, @Kapiljhajhria
+# @CHT45, @Displax, @NOS_bzone, @Kapiljhajhria, @mario0318
