2320bb27f0ddf4de9e5ec433c4fd699872ae4541
Didgeridoohan <github@heptadecagon.one>
2021-06-07 23:15:31 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,15 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Google Pixel 3 (11)=google/blueline/blueline:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
+Google Pixel 3 XL (11)=google/crosshatch/crosshatch:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
+Google Pixel 3a (11)=google/sargo/sargo:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
+Google Pixel 3a XL (11)=google/bonito/bonito:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
+Google Pixel 4 (11)=google/flame/flame:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
+Google Pixel 4 XL (11)=google/coral/coral:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
+Google Pixel 4a (11)=google/sunfish/sunfish:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
+Google Pixel 4a 5G (11)=google/bramble/bramble:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
+Google Pixel 5 (11)=google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
 LG K20 V (7.0)=lge/lv517_vzw/lv517:7.0/NRD90U/1906714433ce5:user/release-keys__2019-03-01
 OnePlus 7T India (11)=OnePlus/OnePlus7T/OnePlus7T:11/RKQ1.201022.002/2105071638:user/release-keys__2021-05-01
 Samsung Galaxy S21 Samsung SM-G991B (11):Samsung:SMG991B=samsung/o1sxxx/o1s:11/RP1A.200720.012/G991BXXU3AUE1:user/release-keys__2021-05-01
