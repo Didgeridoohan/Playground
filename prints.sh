683cb5c7ab0b1191707c72ca6db559e7285f1007
Didgeridoohan <github@heptadecagon.one>
2021-06-21 00:49:41 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,22 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Pixel 3 (11)=google/blueline/blueline:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
-Google Pixel 3 XL (11)=google/crosshatch/crosshatch:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
-Google Pixel 3a (11)=google/sargo/sargo:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
-Google Pixel 3a XL (11)=google/bonito/bonito:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
-Google Pixel 4 (11)=google/flame/flame:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
-Google Pixel 4 XL (11)=google/coral/coral:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
-Google Pixel 4a (11)=google/sunfish/sunfish:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
-Google Pixel 4a 5G (11)=google/bramble/bramble:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
-Google Pixel 5 (11)=google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys__2021-06-05
-LG K20 V (7.0)=lge/lv517_vzw/lv517:7.0/NRD90U/1906714433ce5:user/release-keys__2019-03-01
-OnePlus 7T India (11)=OnePlus/OnePlus7T/OnePlus7T:11/RKQ1.201022.002/2105071638:user/release-keys__2021-05-01
-Redmi Note 9 Pro India (10)=Redmi/joyeuse_id/joyeuse:10/QKQ1.191215.002/V12.0.5.0.QJZIDXM:user/release-keys__2021-03-01
-Samsung Galaxy S21 Samsung SM-G991B (11):Samsung:SMG991B=samsung/o1sxxx/o1s:11/RP1A.200720.012/G991BXXU3AUE1:user/release-keys__2021-05-01
-Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.26.0.QDIMIXM:user/release-keys__2021-05-01
-Xiaomi Mi Mix 3 5G (9)=Xiaomi/andromeda_eea/andromeda:9/PKQ1.190321.001/V12.0.7.0.PEMEUXM:user/release-keys__2021-05-01
 "
 
 # Contributors:
-# @CHT45, @Displax, @NOS_bzone, @Kapiljhajhria, @mario0318, @nomeyamo, @game5413
+# 
