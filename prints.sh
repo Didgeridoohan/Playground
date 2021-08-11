08f0a74dd1fae9f98bd73efad75adee7a6d6f230
Didgeridoohan <github@heptadecagon.one>
2021-08-11 11:04:57 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,30 +8,10 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Pixel 3 (11)=google/blueline/blueline:11/RQ3A.210805.001.A1/7474174:user/release-keys__2021-08-05
-Google Pixel 3 XL (11)=google/crosshatch/crosshatch:11/RQ3A.210805.001.A1/7474174:user/release-keys__2021-08-05
-Google Pixel 3a (11)=google/sargo/sargo:11/RQ3A.210805.001.A1/7474174:user/release-keys__2021-08-05
-Google Pixel 3a XL (11)=google/bonito/bonito:11/RQ3A.210805.001.A1/7474174:user/release-keys__2021-08-05
-Google Pixel 4 (11)=google/flame/flame:11/RQ3A.210805.001.A1/7474174:user/release-keys__2021-08-05
-Google Pixel 4 XL (11)=google/coral/coral:11/RQ3A.210805.001.A1/7474174:user/release-keys__2021-08-05
-Google Pixel 4a (11)=google/sunfish/sunfish:11/RQ3A.210805.001.A1/7474174:user/release-keys__2021-08-05
-Google Pixel 4a 5G (11)=google/bramble/bramble:11/RQ3A.210805.001.A1/7474174:user/release-keys__2021-08-05
-Google Pixel 5 (11)=google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys__2021-08-05
-Motorola Moto E 2020 (10):Motorola:moto e=motorola/ginna_retail/ginna:10/QPG30.82-141/84c8d:user/release-keys__2021-02-01
-Motorola Moto G7 XT1962-1 (10):Motorola:moto g(7)=motorola/river_retail/river:10/QPUS30.52-33-11/df129:user/release-keys__2021-02-01
-Motorola Moto G7 Power (10):Motorola:moto g(7) power=motorola/ocean_retail/ocean:10/QPOS30.52-29-11/72afa:user/release-keys__2020-12-01
-Motorola Moto G7 Play (10):Motorola:moto g(7) play=motorola/channel_retail/channel:10/QPY30.85-18/6d15b:user/release-keys__2020-06-01
-Nvidia Shield TV 2019 (9)=NVIDIA/sif/sif:9/PPR1.180610.011/4079180_2740.7538:user/release-keys__2021-04-05
-POCO F3 Global (11)=POCO/alioth_global/alioth:11/RKQ1.200826.002/V12.5.1.0.RKHMIXM:user/release-keys__2021-03-01
-Samsung S10 Lite SM-G770F (11):Samsung:SM-G770F=samsung/r5qnsxx/r5q:11/RP1A.200720.012/G770FXXS4EUF6:user/release-keys__2021-07-01
-Xiaomi Mi A2 (10)=xiaomi/jasmine/jasmine_sprout:10/QKQ1.190910.002/V11.0.28.0.QDIMIXM:user/release-keys__2021-07-01
-Xiaomi Mi A2 Lite (10)=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.21.0.QDLMIXM:user/release-keys__2021-07-01
-Xiaomi Mi A3 Europe (11)=Xiaomi/laurel_sprout_eea/laurel_sprout:11/RKQ1.200903.002/V12.0.10.0.RFQEUXM:user/release-keys__2021-07-05
-Xiaomi Mi A3 Global (11)=Xiaomi/laurel_sprout/laurel_sprout:11/RKQ1.200903.002/V12.0.12.0.RFQMIXM:user/release-keys__2021-07-05
 "
 
 # Contributors:
-# @Some_Random_Username, @Dhaval Bhramkhatri, @Last_SiN, @Displax, @GtrCraft, @BlackSoulxxx, @rdis, @yazakachka, @tepsys, @DjLeleX, @ipdev, @xuxu1, @spacedingo (Github), @Howard20181 (Github), @SyberHexen (Github), @CHT45
+# 
 
 # Failed prints:
 
