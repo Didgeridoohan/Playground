341b10ed6f1121c81f45b9aa612b1e80a2e82703
Didgeridoohan <github@heptadecagon.one>
2021-08-12 09:43:09 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,10 +8,11 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Mi 8 Explorer (10):Xiaomi:Mi 8 Explorer=Xiaomi/ursa/ursa:10/QKQ1.190828.002/V12.5.1.0.QEHCNXM:user/release-keys__2021-06-07
 "
 
 # Contributors:
-# 
+# @Last_SiN
 
 # Failed prints:
 
