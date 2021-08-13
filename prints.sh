ad241612848c89fc6cfb41ea9d52726f5afa81ca
Didgeridoohan <github@heptadecagon.one>
2021-08-13 11:16:38 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,12 +8,13 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+POCO F3 Indoniesia (11)=POCO/alioth_id/alioth:11/RKQ1.200826.002/V12.5.2.0.RKHIDXM:user/release-keys__2021-06-01
 Redmi K40 China (11)=Redmi/alioth/alioth:11/RKQ1.200826.002/V12.5.5.0.RKHCNXM:user/release-keys__2021-07-01
 Xiaomi Mi 8 Explorer (10):Xiaomi:Mi 8 Explorer=Xiaomi/ursa/ursa:10/QKQ1.190828.002/V12.5.1.0.QEHCNXM:user/release-keys__2020-12-01
 "
 
 # Contributors:
-# 
+# @NOS_bzone, @Last_SiN
 
 # Failed prints:
 
