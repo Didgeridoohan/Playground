45087626c10bd235be4c0f3e8abf18a31874fa94
Didgeridoohan <github@heptadecagon.one>
2021-08-13 11:01:08 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -9,13 +9,10 @@ PRINTSTRANSF=1
 # Certified fingerprints
 PRINTSLIST="
 Redmi K40 China (11)=Redmi/alioth/alioth:11/RKQ1.200826.002/V12.5.5.0.RKHCNXM:user/release-keys__2021-07-01
-Xiaomi Mi 8 Explorer (10):Xiaomi:Mi 8 Explorer=Xiaomi/ursa/ursa:10/QKQ1.190828.002/V12.5.1.0.QEHCNXM:user/release-keys__2021-06-01
-Xiaomi Mi 11X India (11)=Redmi/aliothin/alioth:11/RKQ1.200826.002/V12.5.2.0.RKHINXM:user/release-keys__2021-05-01
-
 "
 
 # Contributors:
-# @Last_SiN
+# 
 
 # Failed prints:
 
