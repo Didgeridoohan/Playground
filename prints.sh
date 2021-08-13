943e3d49408d6f68ef9b4529c2adc577de62ebd5
Didgeridoohan <github@heptadecagon.one>
2021-08-13 10:17:28 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,16 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+POCO F3 Europe (11)=Redmi/alioth_eea/alioth:11/RKQ1.200826.002/V12.5.3.0.RKHEUXM:user/release-keys__2021-06-01
+POCO F3 Global (11)=Redmi/alioth_global/alioth:11/RKQ1.200826.002/V12.5.2.0.RKHMIXM:user/release-keys__2021-05-01
+POCO F3 Indoniesia (11)=Redmi/alioth_id/alioth:11/RKQ1.200826.002/V12.5.2.0.RKHIDXM:user/release-keys__2021-06-01
+POCO F3 Russia (11)=Redmi/alioth_ru/alioth:11/RKQ1.200826.002/V12.5.3.0.RKHRUXM:user/release-keys__2021-06-01
+POCO F3 Taiwan (11)=Redmi/alioth_global/alioth:11/RKQ1.200826.002/V12.5.2.0.RKHTWXM:user/release-keys__2021-06-01
+POCO F3 Turkey (11)=Redmi/alioth_tr/alioth:11/RKQ1.200826.002/V12.5.2.0.RKHTRXM:user/release-keys__2021-06-01
+Redmi K40 China (11)=Redmi/alioth/alioth:11/RKQ1.200826.002/V12.5.5.0.RKHCNXM:user/release-keys__2021-07-01
 Xiaomi Mi 8 Explorer (10):Xiaomi:Mi 8 Explorer=Xiaomi/ursa/ursa:10/QKQ1.190828.002/V12.5.1.0.QEHCNXM:user/release-keys__2021-06-07
+Xiaomi Mi 11X India (11)=Redmi/aliothin/alioth:11/RKQ1.200826.002/V12.5.2.0.RKHINXM:user/release-keys__2021-05-01
+
 "
 
 # Contributors:
