abda667dd0fd091dae9fdaaa2607305373086674
Didgeridoohan <github@heptadecagon.one>
2021-08-14 16:20:37 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,14 +8,6 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-POCO F3 Europe (11)=POCO/alioth_eea/alioth:11/RKQ1.200826.002/V12.5.3.0.RKHEUXM:user/release-keys__2021-06-01
-POCO F3 Global (11)=POCO/alioth_global/alioth:11/RKQ1.200826.002/V12.5.2.0.RKHMIXM:user/release-keys__2021-05-01
-POCO F3 Indoniesia (11)=POCO/alioth_id/alioth:11/RKQ1.200826.002/V12.5.2.0.RKHIDXM:user/release-keys__2021-06-01
-POCO F3 Russia (11)=POCO/alioth_ru/alioth:11/RKQ1.200826.002/V12.5.3.0.RKHRUXM:user/release-keys__2021-06-01
-POCO F3 Taiwan (11)=POCO/alioth_global/alioth:11/RKQ1.200826.002/V12.5.2.0.RKHTWXM:user/release-keys__2021-06-01
-POCO F3 Turkey (11)=POCO/alioth_tr/alioth:11/RKQ1.200826.002/V12.5.2.0.RKHTRXM:user/release-keys__2021-06-01
-Redmi K40 China (11):Xiaomi:M2012K11AC=Redmi/alioth/alioth:11/RKQ1.200826.002/V12.5.5.0.RKHCNXM:user/release-keys__2021-07-01
-Xiaomi Mi 8 Explorer (10):Xiaomi:Mi 8 Explorer=Xiaomi/ursa/ursa:10/QKQ1.190828.002/V12.5.1.0.QEHCNXM:user/release-keys__2020-12-01
 "
 
 # Contributors:
@@ -23,4 +15,3 @@ Xiaomi Mi 8 Explorer (10):Xiaomi:Mi 8 Explorer=Xiaomi/ursa/ursa:10/QKQ1.190828.0
 
 # Failed prints:
 
-# https://forum.xda-developers.com/t/miui-updates-tracker-poco-f3-mi-11x-redmi-k40-alioth.4263137/
