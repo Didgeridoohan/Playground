49f308e178a21139a7cbaac547624469d2fd2217
Didgeridoohan <github@heptadecagon.one>
2021-09-05 15:53:13 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,6 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Xiaomi Redmi Note 9 Pro Russia (11):Xiaomi:Redmi Note 9 Pro=Redmi/joyeuse_ru/joyeuse:11/RKQ1.200826.002/V12.5.1.0.RJZRUXM:user/release-keys_2021-08-05
 "
 
 # Contributors:
