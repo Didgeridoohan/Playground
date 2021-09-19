46aacb4c4c8715c40b08beeb98513e43815c92ff
Didgeridoohan <github@heptadecagon.one>
2021-09-19 12:48:23 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Asus Smartphone for Snapdragon Insiders ASUS_I007D (11):asus:ASUS_I007D:asus/WW_I007D/ASUS_I007_1:11/RKQ1.201112.002/18.1030.2107.138-0:user/release-keys__2021-07-01
+Asus Smartphone for Snapdragon Insiders ASUS_I007D (11):asus:ASUS_I007D=asus/WW_I007D/ASUS_I007_1:11/RKQ1.201112.002/18.1030.2107.138-0:user/release-keys__2021-07-01
 Motorola Moto G8 Plus (10):motorola:moto g8 plus=motorola/doha/doha:10/QPIS30.28-Q3-26-4-1-6/9976a:user/release-keys__2021-08-01
 Samsung Galaxy J6 SM-J600GD (10):samsung:SM-J600GD=samsung/j6ltedx/j6lte:10/QP1A.190711.020/J600GDXUACUB3:user/release-keys__2021-02-01
 Xiaomi Mi 10 Lite 5G Global (11)=Xiaomi/monet_global/monet:11/RKQ1.200826.002/V12.5.3.0.RJITWXM:user/release-keys__2021-07-01
