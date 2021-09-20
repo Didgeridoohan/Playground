3dd13b16082903eadb049a6711dfc1df6034c21e
Didgeridoohan <github@heptadecagon.one>
2021-09-20 23:39:23 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,7 +8,7 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-OnePlus N100 (11)=OnePlus/OnePlusN100/OnePlusN100:11/RKQ1.201217.002/2108191027:user/release-keys__2021-08-01
+OnePlus N100 (11)=
 "
 
 # Contributors:
