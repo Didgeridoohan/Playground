8a09e6ae0cf651ebe7b575b1d7eb3b9ff0de9997
Didgeridoohan <github@heptadecagon.one>
2021-09-20 19:52:56 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -11,8 +11,6 @@ PRINTSLIST="
 "
 
 # Contributors:
-# @Displax, @pepote666, @NOS_bzone
+
 
 # Failed prints:
-# Motorola Moto G8 Plus (10):motorola:moto g8 plus=motorola/doha/doha:10/QPIS30.28-Q3-26-4-1-6/9976a:user/release-keys__2021-08-01, @gfrank227
-# Samsung Galaxy J6 SM-J600GD (10):samsung:SM-J600GD=samsung/j6ltedx/j6lte:10/QP1A.190711.020/J600GDXUACUB3:user/release-keys__2021-02-01, @Chicada
