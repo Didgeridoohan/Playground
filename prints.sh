3b3aee0b2ae6cf56502cc927db810367deef3d5a
Didgeridoohan <github@heptadecagon.one>
2021-09-23 11:51:59 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,10 +8,10 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-OnePlus N100 (11)=
+Pioneer SDA-8TAB (9):worldchip:SDA-8TAB=Pioneer/SDA_8TAB_XEEU5_EEA/SDA_8TAB:9/PPR1.181005.003/20200715-141328:user/release-keys__2020-07-05
 "
 
 # Contributors:
-
+@Lefteris Fragiadakis (Frago)
 
 # Failed prints:
