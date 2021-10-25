c8583f8ae13257c20639c1175af9d3e0afc40a27
Didgeridoohan <github@heptadecagon.one>
2021-10-25 08:46:40 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,43 +8,10 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Pixel 3 (11)=
-Google Pixel (11) 3 XL (11)=
-Google Pixel 3a (11)=
-Google Pixel 3a XL (11)=
-Google Pixel 4 (11)=
-Google Pixel 4 XL (11)=
-Google Pixel 4a (11)=
-Google Pixel 4a 5G (11)=
-Google Pixel 5 (11)=
-Google Pixel 5a (11)=
-Google Pixel 3 (12)=
-Google Pixel 3 XL (12)=
-Google Pixel 3a (12)=
-Google Pixel 3a XL (12)=
-Google Pixel 4 (12)=
-Google Pixel 4 XL (12)=
-Google Pixel 4a (12)=
-Google Pixel 4a 5G (12)=
-Google Pixel 5 (12)=
-Google Pixel 5a (12)=
-LG G8 ThinQ (11):LGE:LM-G850l=lge/mh2lm/mh2lm:11/RKQ1.210420.001/211980139c7f7:user/release-keys__2021-07-01
-Nokia 6.1 Plus (10)=
-OnePlus 6T (11)=
-OnePlus 9 (12)=
-OnePlus 9 India (12)=
-OnePlus 9 Pro (12)=
-OnePlus 9 Pro India (12)=
-Samsung Galaxy S20 FE SM-G780F (11):Samsung:SM-G780F=samsung/r8sxx/r8s:11/RP1A.200720.012/G780FXXU5CUH1:user/release-keys__2021-08-01
-Xiaomi Mi 9T Europe (11)=
-Xiaomi Mi 9T Pro Global (11)=
-Xiaomi Redmi 7 Europe (10):Xiaomi:Redmi 7=
-Xiaomi Redmi K20 Europe (11)=Xiaomi/davinci_eea/davinci:11/RKQ1.200826.002/V12.1.1.0.RFJEUXM:user/release-keys__2021-08-01
-Xiaomi Redmi K20 Pro China (11)=
 "
 
 # Contributors:
-# @liwenjie119 on Github, @Some_Random_Username, @Displax, @zgfg, @marciniuk on GitHub, @cocka11 on GitHub, @vijaykanth, @ipdev
+# 
 
 # Failed prints:
 # 
