e5379cb0df893882e7626067e8b1e1d84aacd5a9
Didgeridoohan <github@heptadecagon.one>
2021-10-29 09:30:46 +0200

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,12 +8,10 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
-Google Pixel 6 (12):Google:Pixel 6=google/oriole/oriole:12/SD1A.210817.015.A4/7697517:user/release-keys__2021-10-05
-Google Pixel 6 Pro (12):Google:Pixel 6 Pro=google/raven/raven:12/SD1A.210817.015.A4/7697517:user/release-keys__2021-10-05
 "
 
 # Contributors:
-# @Displax
+# 
 
 # Failed prints:
 # 
