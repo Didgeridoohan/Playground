cdef1a765b3588c652809cf2d3fcd5eb11a23fb8
Didgeridoohan <github@heptadecagon.one>
2021-11-03 17:42:28 +0100

    Update prints.sh

--- prints.sh
+++ prints.sh
@@ -8,10 +8,13 @@ PRINTSTRANSF=1
 
 # Certified fingerprints
 PRINTSLIST="
+Samsung Galaxy A52 4G (11):Samsung:SM-A525F=samsung/a52qnsxx/a52q:11/RP1A.200720.012/A525FXXU2AUF3:user/release-keys__2021-06-01
+Xiaomi Mi 6X (9):Xiaomi:Mi 6X=xiaomi/wayne/wayne:9/PKQ1.180904.001/V12.0.2.0.PDCCNXM:user/release-keys__2020-11-05
+Xiaomi Redmi K30Pro (11):Xiaomi:Redmi K30 Pro=Redmi/lmi/lmi:11/RKQ1.200826.002/V12.5.5.0.RJKCNXM:user/release-keys__2021-08-01
 "
 
 # Contributors:
-# 
+# @Simon1511, @Filo1995, @TPelangi
 
 # Failed prints:
 # 
